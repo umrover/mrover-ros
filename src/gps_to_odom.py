17544e9cd57b02dd1689ca45cd3e3a80ea122bed
ashwin <ashwingupta2000@gmail.com>
2022-04-03 14:38:57 -0400

    gps to odom

---
+++ src/gps_to_odom.py
@@ -0,0 +1,16 @@
+
+import rospy
+import smach_ros
+import smach
+from sensor_msgs.msg import NavSatFix
+
+def gps_callback(data):
+    print(data)
+
+def main():
+    rospy.init_node("gps_to_odom")
+    rospy.Subscriber("/gps/fix", NavSatFix, gps_callback)
+
+    rospy.spin()
+ 
+main()
