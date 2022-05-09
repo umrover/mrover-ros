e243b30b522f319818956bda12dd91518319658c
qhdwight <qhdwight@gmail.com>
2022-05-08 22:13:35 -0700

    Format

--- src/navigation.py
+++ src/navigation.py
@@ -2,6 +2,7 @@ import rospy
 import smach_ros
 import smach
 
+
 class DriveState(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=["driving", "drive_done"])
@@ -11,11 +12,11 @@ class DriveState(smach.State):
 
         return "driving"
 
+
 class TurnState(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=["turning", "turn_done"])
         pass
-    
 
     def execute(self, userdata):
         return "turning"
@@ -23,15 +24,16 @@ class TurnState(smach.State):
 
 def main():
     rospy.init_node("nav")
- 
+
     state_machine = smach.StateMachine(outcomes=['done'])
     sis = smach_ros.IntrospectionServer('server_name', state_machine, '/SM_ROOT')
     sis.start()
     with state_machine:
         state_machine.add("DriveState", DriveState(), transitions={"driving": "DriveState", "drive_done": "TurnState"})
-        state_machine.add("TurnState", TurnState(), transitions={"turning": "TurnState", "turn_done":"DriveState"})
-    
+        state_machine.add("TurnState", TurnState(), transitions={"turning": "TurnState", "turn_done": "DriveState"})
+
     outcome = state_machine.execute()
 
 
-main()
+if __name__ == '__main__':
+    main()
