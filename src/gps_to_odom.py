6df083f7315ce193562e752898d2a321798575be
qhdwight <qhdwight@gmail.com>
2022-05-08 22:13:35 -0700

    Format

--- src/gps_to_odom.py
+++ src/gps_to_odom.py
@@ -6,13 +6,16 @@ from sensor_msgs.msg import NavSatFix
 
 EARTH_RADIUS = 10
 
+
 def gps_callback(data):
     print(data)
+
 
 def main():
     rospy.init_node("gps_to_odom")
     rospy.Subscriber("/gps/fix", NavSatFix, gps_callback)
 
     rospy.spin()
- 
+
+
 main()
