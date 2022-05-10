3ba60fde2d249a3f2861d07e79a63ea3e1d421eb
qhdwight <qhdwight@gmail.com>
2022-05-10 01:00:12 -0700

    Localization lineralization working, added publish debug course

--- src/navigation/navigation.py
+++ src/navigation/navigation.py
@@ -1,9 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+import threading
 
 import rospy
 import smach
 import smach_ros
-
 from common import DoneState, Context
 from tag import SingleTagState
 from waypoint import WaypointState
@@ -12,12 +12,15 @@ from waypoint import WaypointState
 class Navigation:
     state_machine: smach.StateMachine
     context: Context
+    thread: threading.Thread
+    sis: smach_ros.IntrospectionServer
 
     def __init__(self):
         # rospy.Subscriber('/fiducial_vertices', FiducialArray, self.tag_callback)
         self.state_machine = smach.StateMachine(outcomes=['done'])
         self.context = Context()
-        smach_ros.IntrospectionServer('server_name', self.state_machine, '/SM_ROOT').start()
+        self.sis = smach_ros.IntrospectionServer('server_name', self.state_machine, '/SM_ROOT')
+        self.sis.start()
         with self.state_machine:
             self.state_machine.add(
                 'WaypointState', WaypointState(self.context),
@@ -38,10 +41,19 @@ class Navigation:
                 transitions={'done': 'DoneState'}
             )
 
-    def run(self):
-        return self.state_machine.execute()
+    def start(self):
+        self.thread = threading.Thread(target=self.state_machine.execute)
+        self.thread.start()
+
+    def stop(self):
+        self.state_machine.request_preempt()
+        self.sis.stop()
+        self.thread.join()
 
 
 if __name__ == '__main__':
     rospy.init_node('navigation')
-    Navigation().run()
+    navigation = Navigation()
+    navigation.start()
+    rospy.spin()
+    navigation.stop()
