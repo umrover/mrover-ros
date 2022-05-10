a10f0e95712fe1010a356915c509f265b9fcba6e
qhdwight <qhdwight@gmail.com>
2022-05-09 21:55:12 -0700

    Add tag and drive state

--- src/nav.py
+++ src/nav.py
@@ -3,23 +3,30 @@ import smach_ros
 import smach
 
 from geometry_msgs.msg import TwistStamped
-from fiducial_msgs.msg import FiducialArray
 
 
-class DriveState(smach.State):
+class WaypointState(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=["driving", "drive_done"])
+        smach.State.__init__(self, outcomes=["waypoint", "waypoint_done"])
 
     def execute(self, userdata):
-        return "driving"
+        return "waypoint"
 
 
-class TurnState(smach.State):
+class SingleTagState(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=["turning", "turn_done"])
+        smach.State.__init__(self, outcomes=["single_tag", "single_tag_done"])
 
     def execute(self, userdata):
-        return "turning"
+        return "single_tag"
+
+
+class DoneState(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, outcomes=["done"])
+
+    def execute(self, userdata):
+        return "done"
 
 
 class Navigation:
@@ -28,22 +35,22 @@ class Navigation:
 
     def __init__(self):
         self.vel_cmd_publisher = rospy.Publisher('cmd_vel', TwistStamped, queue_size=1)
-        rospy.Subscriber('/fiducial_vertices', FiducialArray, self.tag_callback)
+        # rospy.Subscriber('/fiducial_vertices', FiducialArray, self.tag_callback)
         self.state_machine = smach.StateMachine(outcomes=['done'])
         sis = smach_ros.IntrospectionServer('server_name', self.state_machine, '/SM_ROOT')
         sis.start()
         with self.state_machine:
-            self.state_machine.add("DriveState", DriveState(), transitions={
-                "driving": "DriveState",
-                "drive_done": "TurnState"
+            self.state_machine.add("WaypointState", WaypointState(), transitions={
+                "waypoint": "WaypointState",
+                "waypoint_done": "DoneState"
             })
-            self.state_machine.add("TurnState", TurnState(), transitions={
-                "turning": "TurnState",
-                "turn_done": "DriveState"
+            self.state_machine.add("SingleTagState", SingleTagState(), transitions={
+                "single_tag": "SingleTagState",
+                "single_tag_done": "DoneState"
             })
 
-    def tag_callback(self, data):
-        print(data)
+    # def tag_callback(self, data):
+    #     print(data)
 
     def run(self):
         return self.state_machine.execute()
