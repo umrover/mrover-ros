9549a211fbfa92492b5262d51ad77157f2fac34f
qhdwight <qhdwight@gmail.com>
2022-05-09 23:56:11 -0700

    Remove marker scripts, add inits, fix some python build sys stuff

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['marker_generation'],
-    package_dir={'': 'scripts'}
+    packages=['localization', 'util'],
+    package_dir={'': 'src'}
 )
 
 setup(**d)
