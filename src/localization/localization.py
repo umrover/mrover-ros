89104856dfbf3716ab1fba59b5fceed9d7bc0b50
qhdwight <qhdwight@gmail.com>
2022-05-10 03:14:31 -0700

    PID on angle 0 I guess LOL

--- src/localization/localization.py
+++ src/localization/localization.py
@@ -3,7 +3,8 @@
 import rospy
 import tf
 # from geodesy.utm import fromLatLong
-from sensor_msgs.msg import NavSatFix
+from geometry_msgs.msg import Quaternion
+from sensor_msgs.msg import NavSatFix, Imu
 from util.tf_utils import gps_to_world
 
 SIM_SAT_FIX = NavSatFix(latitude=42.199999689512715, longitude=-83.69999929025072, altitude=0.4652098556018964)
@@ -15,8 +16,11 @@ def main():
     ref_gps_point: NavSatFix = rospy.get_param('ref_gps_point', SIM_SAT_FIX)
     tf_broadcaster = tf.TransformBroadcaster()
 
+    orientation = Quaternion(0, 0, 0, 1)
+
     def gps_callback(gps: NavSatFix):
         stamped_cartesian_transform = gps_to_world(gps, ref_gps_point, 'rover', 'base_link')
+        stamped_cartesian_transform.transform.rotation = orientation
         tf_broadcaster.sendTransformMessage(stamped_cartesian_transform)
         # utm = fromLatLong(gps.latitude, gps.longitude, gps.altitude)
         # tf_broadcaster.sendTransform(
@@ -27,6 +31,11 @@ def main():
         #     'base_link'
         # )
 
+    def imu_callback(imu: Imu):
+        nonlocal orientation
+        orientation = imu.orientation
+
+    rospy.Subscriber('/imu', Imu, imu_callback)
     rospy.Subscriber("/gps/fix", NavSatFix, gps_callback)
 
     rospy.spin()
--- src/navigation/waypoint.py
+++ src/navigation/waypoint.py
@@ -1,6 +1,12 @@
+import math
+
+import numpy as np
+
 import rospy
+import tf
 from common import Context, BaseState
 from geometry_msgs.msg import Twist
+from tf.transformations import euler_from_quaternion
 
 
 class WaypointState(BaseState):
@@ -13,9 +19,19 @@ class WaypointState(BaseState):
         )
 
     def evaluate(self, ud):
-        next_waypoint = self.transform('course')
-        if next_waypoint:
+        try:
+            pos, rot = self.context.tf_listener.lookupTransform('course', 'rover', rospy.Time(0))
+            rot_euler = euler_from_quaternion(rot)
+            # angle = np.arctan2(pos[0], pos[1])
+            # print(pos)
+            # print(np.rad2deg(angle))
+            # print(rot_euler)
+            print(np.rad2deg(rot_euler[2]))
             cmd_vel = Twist()
-            cmd_vel.angular.z = -1.0
+            cmd_vel.angular.z = rot_euler[2] / math.tau / 2
             self.context.vel_cmd_publisher.publish(cmd_vel)
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            # TODO: probably go into some waiting state
+            pass
+
         return 'waypoint'
