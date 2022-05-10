dbe0859636b211dc6fb8932fc25f234f2455c36d
Ashwin Gupta <ashwingupta2000@gmail.com>
2022-05-10 01:46:25 -0400

    tf util

--- src/localization.py
+++ src/localization.py
@@ -3,9 +3,7 @@ import rospy
 import smach_ros
 import smach
 from sensor_msgs.msg import NavSatFix
-
-EARTH_RADIUS = 10
-
+from mrover.util.tf_utils import gps_to_world
 
 def gps_callback(data):
     print(data)
