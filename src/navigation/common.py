Convert to tf2, fix sign of turning effort, rotation still innacurate

src/navigation/common.py
@@ -5,7 +5,7 @@ import numpy as np
 
 import rospy
 import smach
-import tf
+import tf2_ros
 from geometry_msgs.msg import Twist
 from visualization_msgs.msg import Marker
 
@@ -15,13 +15,15 @@ class Context:
     is_shutdown: bool
     vel_cmd_publisher: rospy.Publisher
     vis_publisher: rospy.Publisher
-    tf_listener: tf.TransformListener
+    tf_buffer: tf2_ros.Buffer
+    tf_listener: tf2_ros.TransformListener
 
     def __init__(self):
         self.is_shutdown = False
         self.vel_cmd_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
         self.vis_publisher = rospy.Publisher('/navigation', Marker, queue_size=1)
-        self.tf_listener = tf.TransformListener()
+        self.tf_buffer = tf2_ros.Buffer()
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
 
 
 class BaseState(smach.State, ABC):
@@ -49,14 +51,15 @@ class BaseState(smach.State, ABC):
         """Override me instead of execute!"""
         pass
 
-    def transform(self, frame: str, parent_frame: str = 'world', time: rospy.Time = rospy.Time(0)) \
-            -> Tuple[np.ndarray, np.ndarray]:
+    def transform(self, frame: str, parent_frame: str = 'base_link') -> Tuple[np.ndarray, np.ndarray]:
         """Retrieve position and rotation of frame in tf tree. Relative to the point where we linearized.
         :param: frame: Name of desired frame
         :return: position, rotation which are both numpy arrays
         """
-        pos, rot = self.context.tf_listener.lookupTransform(parent_frame, frame, time)
-        return np.array(pos), np.array(rot)
+        stamped_transform = self.context.tf_buffer.lookup_transform(parent_frame, frame, rospy.Time(0))
+        p = stamped_transform.transform.translation
+        r = stamped_transform.transform.rotation
+        return np.array([p.x, p.y, p.z]), np.array([r.x, r.y, r.z, r.w])
 
 
 class DoneState(BaseState):

src/util/tf_utils.py
@@ -1,28 +1,29 @@
 import numpy as np
 
+import geometry_msgs
 import rospy
-from geometry_msgs.msg import TransformStamped, Quaternion
-from sensor_msgs.msg import NavSatFix, geometry_msgs
+from geometry_msgs.msg import TransformStamped
+from sensor_msgs.msg import NavSatFix
 
 EARTH_RADIUS = 6371000
 
 
-def gps_to_world(gps_coord: NavSatFix, ref_coord: NavSatFix, parent_frame: str, world_frame: str = "world") \
+def gps_to_world(gps_coord: NavSatFix, ref_coord: NavSatFix, frame: str, world_frame: str) \
         -> TransformStamped:
     """
     Convert the GPS coordinate to a cartesian coordinate transform
     Returns the transform from the specified parent frame to a world frame with
     origin at the specified GPS reference coordinate
-    :param gps_coord:   The GPS coordinate that we want in cartesian coordinates
-    :param ref_coord:   The reference GPS coordinate
-    :param name:        The name of the returned transform frame
-    :param parent:      The name of the reference world frame
+    :param gps_coord:       The GPS coordinate that we want in cartesian coordinates
+    :param ref_coord:       The reference GPS coordinate
+    :param frame:           The name of the returned transform frame
+    :param world_frame:     The name of the reference world frame
     :return:
     """
     t = geometry_msgs.msg.TransformStamped()
     t.header.stamp = rospy.Time.now()
-    t.header.frame_id = parent_frame
-    t.child_frame_id = world_frame
+    t.header.frame_id = world_frame
+    t.child_frame_id = frame
 
     # TODO: compare accuracy to geodesy.utm transforms
     lat_cos = np.cos(np.radians(gps_coord.latitude))
@@ -32,5 +33,5 @@ def gps_to_world(gps_coord: NavSatFix, ref_coord: NavSatFix, parent_frame: str, 
         gps_coord.latitude - ref_coord.latitude) * EARTH_RADIUS
 
     # use an identity quaternion since we have no rotation info from GPS
-    t.transform.rotation.w = Quaternion(0, 0, 0, 1)
+    t.transform.rotation.w = 1
     return t