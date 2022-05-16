488b5d309cea7275b46c26be60259c70175b83de
ankithu <ankith.udupa@gmail.com>
2022-05-15 22:03:06 -0400

    move driving to point code into its own function

---
+++ src/navigation/drive.py
@@ -0,0 +1,33 @@
+import numpy as np
+from geometry_msgs.msg import Twist
+from typing import Tuple
+
+#generalized drive to target command, returns a tuple of twist of how to get to a particular pos along with a boolean of whether it is done or not
+def get_drive_command(target_pos : np.ndarray, rover_pos : np.ndarray, rover_dir : np.ndarray, completion_tolerance : float, DRIVE_FWD_THRESH : float) -> Tuple[Twist, bool]:
+    # Get vector from rover to target
+    target_dir = target_pos - rover_pos
+    target_dist = np.linalg.norm(target_dir)
+    if target_dist == 0:
+        target_dist = np.finfo(float).eps
+    # Normalize direction
+    target_dir /= target_dist
+    # Both vectors are unit vectors so the dot product magnitude is 0-1
+    # 0 alignment is perpendicular, 1 is parallel (fully aligned)
+    alignment = np.dot(target_dir, rover_dir)
+
+    if target_dist < completion_tolerance:
+       return [Twist(), True]
+    else:
+        cmd_vel = Twist()
+        if alignment > DRIVE_FWD_THRESH:
+            # We are pretty aligned so we can drive straight
+            error = target_dist
+            cmd_vel.linear.x = np.clip(error, 0.0, 1.0)
+        # Determine the sign of our effort by seeing if we are to the left or to the right of the target
+        # This is done by dotting rover_dir and target_dir rotated 90 degrees ccw
+        perp_alignment = rover_dir[0] * -target_dir[1] + rover_dir[1] * target_dir[0]
+        sign = -np.sign(perp_alignment)
+        # 1 is target alignment
+        error = 1.0 - alignment
+        cmd_vel.angular.z = np.clip(error * 100.0 * sign, -1.0, 1.0)
+        return [cmd_vel, False]
--- src/navigation/waypoint.py
+++ src/navigation/waypoint.py
@@ -4,11 +4,11 @@ import numpy as np
 import tf2_ros
 from geometry_msgs.msg import Twist
 from tf.transformations import quaternion_matrix
+from drive import get_drive_command
 
 from common import Context, BaseState
 
 DRIVE_FWD_THRESH = 0.95
-
 
 class WaypointState(BaseState):
     def __init__(self, context: Context):
@@ -33,33 +33,10 @@ class WaypointState(BaseState):
         try:
             course_pos, course_rot = self.waypoint_transform(ud, ud.waypoint_index)
             rover_pos, _ = self.rover_transform()
-            # Get vector from rover to waypoint
-            target_dir = course_pos - rover_pos
-            target_dist = np.linalg.norm(target_dir)
-            if target_dist == 0:
-                target_dist = np.finfo(float).eps
-            # Normalize direction
-            target_dir /= target_dist
-            rover_dir = self.rover_forward()
-            # Both vectors are unit vectors so the dot product magnitude is 0-1
-            # 0 alignment is perpendicular, 1 is parallel (fully aligned)
-            alignment = np.dot(target_dir, rover_dir)
-
-            if target_dist < 0.5:
+            cmd_vel, done = get_drive_command(course_pos, rover_pos, self.rover_forward(), 0.5, DRIVE_FWD_THRESH)
+            if done:
                 ud.waypoint_index += 1
             else:
-                cmd_vel = Twist()
-                if alignment > DRIVE_FWD_THRESH:
-                    # We are pretty aligned so we can drive straight
-                    error = target_dist
-                    cmd_vel.linear.x = np.clip(error, 0.0, 1.0)
-                # Determine the sign of our effort by seeing if we are to the left or to the right of the target
-                # This is done by dotting rover_dir and target_dir rotated 90 degrees ccw
-                perp_alignment = rover_dir[0] * -target_dir[1] + rover_dir[1] * target_dir[0]
-                sign = -np.sign(perp_alignment)
-                # 1 is target alignment
-                error = 1.0 - alignment
-                cmd_vel.angular.z = np.clip(error * 100.0 * sign, -1.0, 1.0)
                 self.context.vel_cmd_publisher.publish(cmd_vel)
         except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
             # TODO: probably go into some waiting state
