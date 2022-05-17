25d6ef857b26eaec826d7ec08c07ee75296c0e8b
ankithu <ankith.udupa@gmail.com>
2022-05-17 15:37:31 -0400

    addresses comments

--- src/navigation/drive.py
+++ src/navigation/drive.py
@@ -2,10 +2,13 @@ import numpy as np
 from geometry_msgs.msg import Twist
 from typing import Tuple
 
-#
-def get_drive_command(target_pos : np.ndarray, rover_pos : np.ndarray, rover_dir : np.ndarray, completion_tolerance : float, DRIVE_FWD_THRESH : float) -> Tuple[Twist, bool]:
+MAX_DRIVING_EFFORT = 1
+MIN_DRIVING_EFFORT = -1
+TURNING_P = 100.0
+
+def get_drive_command(target_pos : np.ndarray, rover_pos : np.ndarray, rover_dir : np.ndarray, completion_tolerance : float, turn_in_place_tolerance_rad : float) -> Tuple[Twist, bool]:
     """generalized drive to target command, returns a 
-        :param: target_pos :  target position ndarray, rover_pos : current rover position ndarray, rover_dir : current rover rotatation ndarray, completion tolerance: float scalar distance threshold, drive_fwd_thresh : float angular threshold for turning vs driving
+        :param: target_pos :  target position ndarray, rover_pos : current rover position ndarray, rover_dir : current rover rotatation (must be normalized) ndarray, completion tolerance: float scalar distance threshold, drive_fwd_thresh : float angular threshold for turning vs driving
         :return: Twist, Bool : twist is how to get to a particular pos along with a boolean of whether it is done or not
     """
     # Get vector from rover to target
@@ -21,17 +24,17 @@ def get_drive_command(target_pos : np.ndarray, rover_pos : np.ndarray, rover_dir
 
     if target_dist < completion_tolerance:
        return Twist(), True
-    else:
-        cmd_vel = Twist()
-        if alignment > DRIVE_FWD_THRESH:
-            # We are pretty aligned so we can drive straight
-            error = target_dist
-            cmd_vel.linear.x = np.clip(error, 0.0, 1.0)
-        # Determine the sign of our effort by seeing if we are to the left or to the right of the target
-        # This is done by dotting rover_dir and target_dir rotated 90 degrees ccw
-        perp_alignment = rover_dir[0] * -target_dir[1] + rover_dir[1] * target_dir[0]
-        sign = -np.sign(perp_alignment)
-        # 1 is target alignment
-        error = 1.0 - alignment
-        cmd_vel.angular.z = np.clip(error * 100.0 * sign, -1.0, 1.0)
-        return cmd_vel, False
+    
+    cmd_vel = Twist()
+    if alignment > turn_in_place_tolerance_rad:
+        # We are pretty aligned so we can drive straight
+        error = target_dist
+        cmd_vel.linear.x = np.clip(error, 0.0, MAX_DRIVING_EFFORT)
+    # Determine the sign of our effort by seeing if we are to the left or to the right of the target
+    # This is done by dotting rover_dir and target_dir rotated 90 degrees ccw
+    perp_alignment = rover_dir[0] * -target_dir[1] + rover_dir[1] * target_dir[0]
+    sign = -np.sign(perp_alignment)
+    # 1 is target alignment (dot product of two normalized vectors that are parrallel is 1)
+    error = 1.0 - alignment
+    cmd_vel.angular.z = np.clip(error * TURNING_P * sign, MIN_DRIVING_EFFORT, MAX_DRIVING_EFFORT)
+    return cmd_vel, False
