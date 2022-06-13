a42fe2bd3925dc5513d01cd46e3818cb0806ac53
tabiosg <71603173+tabiosg@users.noreply.github.com>
2022-06-13 16:43:42 -0400

    Remove over-indent

--- src/science/science.py
+++ src/science/science.py
@@ -50,55 +50,55 @@ class ScienceBridge():
 
     def format_mosfet_msg(self, device: int, enable: bool) -> str:
         """Formats a mosfet message"""
-        uart_msg = "$MOSFET,{dev},{en},"
-        return uart_msg.format(dev=device, en=enable)
+        tx_msg = "$MOSFET,{dev},{en},"
+        return tx_msg.format(dev=device, en=enable)
 
     def heater_auto_shut_off_transmit(self, ros_msg: Enable) -> None:
         """Transmits a heater auto shut off command over uart"""
-        uart_msg = "$AUTOSHUTOFF,{enable}"
-        uart_msg = uart_msg.format(enable=int(ros_msg.auto_shut_off_enabled))
-        send_msg(uart_msg)
+        tx_msg = "$AUTOSHUTOFF,{enable}"
+        tx_msg = tx_msg.format(enable=int(ros_msg.auto_shut_off_enabled))
+        send_msg(tx_msg)
 
-    def heater_auto_shut_off_handler(self, uart_msg: str, ros_msg: Enable) -> None:
+    def heater_auto_shut_off_handler(self, tx_msg: str, ros_msg: Enable) -> None:
         """Handles a received heater auto shut off message"""
-        # uart_msg format: <"$AUTOSHUTOFF,device,enabled">
-        arr = uart_msg.split(",")
+        # tx_msg format: <"$AUTOSHUTOFF,device,enabled">
+        arr = tx_msg.split(",")
         enabled = bool(int(arr[1]))
         ros_msg.enable = enabled
 
-    def heater_state_handler(self, uart_msg: str, ros_msg: Heater) -> None:
+    def heater_state_handler(self, tx_msg: str, ros_msg: Heater) -> None:
         """Handles a received heater state message"""
-        # uart_msg format: <"$HEATER,device,enabled">
-        arr = uart_msg.split(",")
+        # tx_msg format: <"$HEATER,device,enabled">
+        arr = tx_msg.split(",")
         ros_msg.device = int(arr[1])
         ros_msg.enable = bool(int(arr[2]))
 
     def heater_transmit(self, ros_msg: Heater) -> None:
         """Transmits a heater state command message"""
         translated_device = self.heater_map[ros_msg.device]
-        uart_msg = self.format_mosfet_msg(translated_device, int(ros_msg.enable))
-        send_msg(uart_msg)
+        tx_msg = self.format_mosfet_msg(translated_device, int(ros_msg.enable))
+        send_msg(tx_msg)
 
     def mosfet_transmit(self, ros_msg: Enable, device_name: str) -> None:
         """Transmits a mosfet device state command message"""
         translated_device = self.mosfet_dev_map[device_name]
-        uart_msg = self.format_mosfet_msg(translated_device, int(ros_msg.enable))
-        send_msg(uart_msg)
+        tx_msg = self.format_mosfet_msg(translated_device, int(ros_msg.enable))
+        send_msg(tx_msg)
 
     def servo_transmit(self, ros_msg: Servo) -> None:
         """Transmits a servo angle command message"""
-        uart_msg = "$SERVO,{angle_0},{angle_1},{angle_2}"
-        uart_msg = uart_msg.format(angle_0=ros_msg.angle_0, angle_1=ros_msg.angle_1,
+        tx_msg = "$SERVO,{angle_0},{angle_1},{angle_2}"
+        tx_msg = tx_msg.format(angle_0=ros_msg.angle_0, angle_1=ros_msg.angle_1,
                                    angle_2=ros_msg.angle_2)
-        send_msg(uart_msg)
+        send_msg(tx_msg)
 
     def spectral_handler(self, msg: str, ros_msg: Spectral) -> None:
         """Handles a received spectral data message"""
         msg.split(',')
         arr = [s.strip().strip('\x00') for s in msg.split(',')]
         ros_msg_variables = ["d0_1", "d0_2", "d0_3", "d0_4", "d0_5", "d0_6",
-                                "d1_1", "d1_2", "d1_3", "d1_4", "d1_5", "d1_6",
-                                "d2_1", "d2_2", "d2_3", "d2_4", "d2_5", "d2_6"]
+                             "d1_1", "d1_2", "d1_3", "d1_4", "d1_5", "d1_6",
+                             "d2_1", "d2_2", "d2_3", "d2_4", "d2_5", "d2_6"]
         # There are 3 spectral sensors, each having 6 channels.
         # We read a uint16_t from each channel.
         # The jetson reads byte by byte, so the program combines every two byte of information
@@ -112,9 +112,9 @@ class ScienceBridge():
                 setattr(ros_msg, var, 0)
             count += 2
 
-    def thermistor_handler(self, uart_msg: str, ros_msg: Thermistor) -> None:
+    def thermistor_handler(self, tx_msg: str, ros_msg: Thermistor) -> None:
         """Handles a receieved thermistor data message"""
-        arr = uart_msg.split(",")
+        arr = tx_msg.split(",")
         ros_msg.temp_0 = float(arr[1])
         ros_msg.temp_1 = float(arr[2])
         ros_msg.temp_2 = float(arr[3])
@@ -124,8 +124,8 @@ class ScienceBridge():
         msg.split(',')
         arr = [s.strip().strip('\x00') for s in msg.split(',')]
         ros_msg_variables = ["d0_1", "d0_2", "d0_3", "d0_4", "d0_5", "d0_6",
-                                "d1_1", "d1_2", "d1_3", "d1_4", "d1_5", "d1_6",
-                                "d2_1", "d2_2", "d2_3", "d2_4", "d2_5", "d2_6"]
+                             "d1_1", "d1_2", "d1_3", "d1_4", "d1_5", "d1_6",
+                             "d2_1", "d2_2", "d2_3", "d2_4", "d2_5", "d2_6"]
 
         # There are 18 channels.
         # We read a uint16_t from each channel.
@@ -143,18 +143,18 @@ class ScienceBridge():
     def receive(self) -> None:
         """Infinite loop that reads in messages from the UART RX line and processes them"""
         while True:
-            uart_msg = read_msg()
+            tx_msg = read_msg()
             match_found = False
             for tag, handler_func in self.nmea_handle_mapper.items():
-                if tag in uart_msg:
-                    print(uart_msg)
+                if tag in tx_msg:
+                    print(tx_msg)
                     match_found = True
-                    handler_func(uart_msg, self.nmea_message_mapper[tag])
+                    handler_func(tx_msg, self.nmea_message_mapper[tag])
                     self.nmea_publisher_mapper[tag].publish(self.nmea_message_mapper[tag])
                     break
             if not match_found:
-                if not uart_msg:
-                    print(f'Error decoding message stream: {uart_msg}')
+                if not tx_msg:
+                    print(f'Error decoding message stream: {tx_msg}')
             rospy.sleep(self.sleep)
 
 
