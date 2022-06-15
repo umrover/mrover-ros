f62eeb93b333d76784575eae4e1efe27172e903e
tabiosg <71603173+tabiosg@users.noreply.github.com>
2022-06-15 14:20:05 -0400

    isort error

--- src/esw/cameras/cameras.py
+++ src/esw/cameras/cameras.py
@@ -1,14 +1,15 @@
 """
 This file controls the cameras that the jetson will stream.
 """
-from config import (DEFAULT_MISSION, MAX_VIDEO_DEVICE_ID_NUMBER,
-                    MISSION_MAP, NUMBER_OF_PIPELINES)
+import sys
+
+import rospy
+from config import (DEFAULT_MISSION, MAX_VIDEO_DEVICE_ID_NUMBER, MISSION_MAP,
+                    NUMBER_OF_PIPELINES)
 from mrover.srv import (ChangeCameraMission, ChangeCameraMissionRequest,
                         ChangeCameraMissionResponse, ChangeCameras,
                         ChangeCamerasRequest, ChangeCamerasResponse)
-import rospy
 
-import sys
 sys.path.insert(0, "/usr/lib/python3.6/dist-packages")  # 3.6 vs 3.8
 import jetson.utils  # noqa
 
