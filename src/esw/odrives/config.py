504341667cff212a9b3d9d678e2a1ee7b5d60f92
tabiosg <71603173+tabiosg@users.noreply.github.com>
2022-06-16 15:11:57 -0400

    Alphabetize

--- src/esw/odrives/config.py
+++ src/esw/odrives/config.py
@@ -23,18 +23,6 @@ class Pair(Enum):
     BACK = config['pair']['back']
 
 
-MOTOR_MAP = {(Axis.LEFT, Pair.FRONT): config['combo']['front_left'],
-             (Axis.RIGHT, Pair.FRONT): config['combo']['front_right'],
-             (Axis.LEFT, Pair.MIDDLE): config['combo']['middle_left'],
-             (Axis.RIGHT, Pair.MIDDLE): config['combo']['middle_right'],
-             (Axis.LEFT, Pair.BACK): config['combo']['back_left'],
-             (Axis.RIGHT, Pair.BACK): config['combo']['back_right']}
-
-
-TURNS_TO_M_S_MULTIPLIER = config['info']['turns_to_m_s_multiplier']
-CURRENT_LIM = config['config']['current_lim']
-ODRIVE_WATCHDOG_TIMEOUT = config['config']['watchdog_timeout']
-
 AXIS_SPEED_MULTIPLIER_MAP = [None] * 2
 AXIS_SPEED_MULTIPLIER_MAP[Axis.LEFT.value] = \
     config['info']['speed_multiplier_left']
@@ -47,7 +35,19 @@ AXIS_VEL_ESTIMATE_MULTIPLIER_MAP[Axis.LEFT.value] = \
 AXIS_VEL_ESTIMATE_MULTIPLIER_MAP[Axis.RIGHT.value] = \
     config['info']['turns_to_m_s_multiplier_right']
 
+CURRENT_LIM = config['config']['current_lim']
+
+MOTOR_MAP = {(Axis.LEFT, Pair.FRONT): config['combo']['front_left'],
+             (Axis.RIGHT, Pair.FRONT): config['combo']['front_right'],
+             (Axis.LEFT, Pair.MIDDLE): config['combo']['middle_left'],
+             (Axis.RIGHT, Pair.MIDDLE): config['combo']['middle_right'],
+             (Axis.LEFT, Pair.BACK): config['combo']['back_left'],
+             (Axis.RIGHT, Pair.BACK): config['combo']['back_right']}
+
 ODRIVE_IDS = [None] * 3
 ODRIVE_IDS[Pair.FRONT.value] = config['ids']['front']
 ODRIVE_IDS[Pair.MIDDLE.value] = config['ids']['middle']
 ODRIVE_IDS[Pair.BACK.value] = config['ids']['back']
+
+ODRIVE_WATCHDOG_TIMEOUT = config['config']['watchdog_timeout']
+TURNS_TO_M_S_MULTIPLIER = config['info']['turns_to_m_s_multiplier']
