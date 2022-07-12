from_tf_tree with tf buffer as argument, fixed default quaternion for SO3

src/util/SO3.py
@@ -8,7 +8,7 @@ class SO3:
 
     def __init__(self, quaternion: np.ndarray = None):
         if quaternion is None:
-            self.quaternion = np.zeros(4)
+            self.quaternion = np.array([0, 0, 0, 1])
         else:
             self.quaternion = quaternion
 