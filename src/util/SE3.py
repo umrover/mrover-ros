641d93ba72a275a2cb7f4217f6c1a05fb41a7675
Riley Bridges <rlybrdgs@gmail.com>
2022-07-12 21:15:12 -0400

    untested SE3 and SO3, deleted SE2 since we dont need it

--- src/util/SE3.py
+++ src/util/SE3.py
@@ -6,8 +6,7 @@ import tf2_ros
 import rospy
 
 from ros_numpy import numpify
-from tf.transformations import (quaternion_inverse, quaternion_matrix,
-                                quaternion_multiply, rotation_from_matrix)
+from geometry_msgs.msg import TransformStamped, Vector3, Quaternion
 from .SO3 import SO3
 
 
@@ -58,29 +57,28 @@ class SE3:
         result.rotation = SO3(numpify(tf_msg.rotation))
         return result
 
-    def publish_to_tf_tree(self, parent_frame: str, child_frame: str):
-        ...
+    def publish_to_tf_tree(self, tf_broadcaster: tf2_ros.TransformBroadcaster,
+                           parent_frame: str, child_frame: str):
+        """
+        Publish the SE3 to the TF tree as a transform from parent_frame to child_frame.
+
+        :param tf_broadcaster: the TF broadcaster used to publish to the TF tree
+        :param parent_frame: the parent frame of the transform to be published
+        :param child_frame: the child frame of the transform to be published
+        """
+        tf = TransformStamped()
+        tf.transform.translation = Vector3(*self.position)
+        tf.transform.rotation = Quaternion(*self.rotation.quaternion_vector())
+        tf.header.stamp = rospy.Time.now()
+        tf.header.frame_id = parent_frame
+        tf.child_frame_id = child_frame
+        tf_broadcaster.sendTransform(tf)
 
     def pos_distance_to(self, p: SE3) -> float:
         """
-        Get the euclidean distance from the position of this SE3 pose to the position of another SE3 pose
+        Get the euclidean distance from the position of this SE3 to the position of another SE3
 
-        :param p: another SE3 pose object
-        :returns: euclidean distance between the two SE3 poses
+        :param p: another SE3
+        :returns: euclidean distance between the two SE3s
         """
-        return np.linalg.norm(p.position_vector() - self.position_vector())
-
-    def rot_distance_to(self, p: SE3) -> float:
-        """
-        Get the rotational distance between this SE3 pose and another SE3 pose,
-        defined as the angle of rotation from one quaternion to the other along the shortest arc.
-
-        :param p: the other SE3 pose object
-        :returns: the angle in radians between the orientations of the two poses
-        """
-        q1 = self.quaternion_array()
-        q2 = p.quaternion_array()
-        q_diff = quaternion_multiply(quaternion_inverse(q1), q2)
-        rotation_matrix = quaternion_matrix(q_diff)
-        angle, _, _ = rotation_from_matrix(rotation_matrix)
-        return angle
+        return np.linalg.norm(p.position - self.position)
