c81abf10ca91a0404171972d9f156867b6c1e1a4
Riley Bridges <rlybrdgs@gmail.com>
2022-07-12 21:15:12 -0400

    untested SE3 and SO3, deleted SE2 since we dont need it

--- src/util/SO3.py
+++ src/util/SO3.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 import numpy as np
-from tf.transformations import quaternion_matrix, quaternion_from_matrix
+from tf.transformations import (quaternion_inverse, quaternion_matrix, quaternion_from_matrix,
+                                quaternion_multiply, rotation_from_matrix)
 
 
 class SO3:
@@ -20,10 +21,11 @@ class SO3:
         :param rotation_matrix: the 3x3 rotation matrix
         :returns: the created SO3 object
         """
-        homogenous = np.eye((4,4))
+        homogenous = np.eye((4, 4))
         homogenous[:3, :3] = rotation_matrix
         return quaternion_from_matrix(homogenous)
 
+    # TODO: is this method necessary?
     def quaternion_vector(self) -> np.ndarray:
         """
         Get the quaternion vector of the SO3.
@@ -47,3 +49,18 @@ class SO3:
         :returns: unit direction vector [x, y, z]
         """
         return self.rotation_matrix[:, 0]
+
+    def rot_distance_to(self, r: SO3) -> float:
+        """
+        Get the rotational distance between this SO3 and another SO3,
+        defined as the angle of rotation from one SO3 to the other along the shortest arc.
+
+        :param p: the other SO3
+        :returns: the angle in radians between the orientations of the two SO3s
+        """
+        q1 = self.quaternion_vector()
+        q2 = r.quaternion_vector()
+        q_diff = quaternion_multiply(quaternion_inverse(q1), q2)
+        rotation_matrix = quaternion_matrix(q_diff)
+        angle, _, _ = rotation_from_matrix(rotation_matrix)
+        return angle
