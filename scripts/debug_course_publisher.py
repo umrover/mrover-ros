3d0ed27657e101b0b26eead6bf56eb962f89b181
Ashwin Gupta <ashwingupta2000@gmail.com>
2022-08-09 01:59:13 -0400

    address review comments

--- scripts/debug_course_publisher.py
+++ scripts/debug_course_publisher.py
@@ -1,23 +1,35 @@
 #!/usr/bin/env python3
-
-import time
-
 import rospy
 import tf2_ros
 from geometry_msgs.msg import TransformStamped
+from typing import List, Tuple
 
 from mrover.msg import Course, Waypoint
 import mrover.srv
 
+"""
+The purpose of this file is for testing courses in the sim 
+without needing the auton GUI. You can add waypoints with
+and without fiducials and these will get published to nav
+"""
 
-def add_waypoint(frame_id: str, x: float, y: float, fid_id: int = -1) -> Waypoint:
+
+def add_waypoint(frame_id: str, fid_id: int) -> Waypoint:
+    """
+    Generates a waypoint message with the given waypoint name and
+    fiducial ID
+    """
     w = Waypoint()
     w.fiducial_id = fid_id
     w.tf_id = frame_id
     return w
 
 
-def get_waypoint_tf(frame_id: str, x: float, y: float, fid_id: int = -1):
+def get_waypoint_tf(frame_id: str, x: float, y: float):
+    """
+    Generates a transform for describing the cartesian position
+    of the waypoint in the map
+    """
     t = TransformStamped()
     t.header.stamp = rospy.Time.now()
     t.header.frame_id = "odom"
@@ -28,9 +40,12 @@ def get_waypoint_tf(frame_id: str, x: float, y: float, fid_id: int = -1):
     return t
 
 
-def send_waypoints(waypoints) -> Waypoint:
+def send_waypoints(waypoints: List[Tuple]) -> Waypoint:
+    """
+    Publishes the array of waypoint tuples over TF
+    """
     tf_broadcaster = tf2_ros.StaticTransformBroadcaster()
-    tfs = [get_waypoint_tf(*waypoint) for waypoint in waypoints]
+    tfs = [get_waypoint_tf(*waypoint[:3]) for waypoint in waypoints]
     tf_broadcaster.sendTransform(tfs)
 
 
@@ -41,14 +56,14 @@ if __name__ == "__main__":
     # These are the waypoints (name, x, y, fiducial)
     waypoints = [
         ("course1", 3, 3, 0),
-        # ("course1", -3, -3),
+        # ("course1", -3, -3, -1),
         # ("course2", -5, -5, 0)
     ]
 
     # This executes an RPC that gives the state machine the course
     c = Course()
     for w in waypoints:
-        c.waypoints.append(add_waypoint(*w))
+        c.waypoints.append(add_waypoint(w[0], w[3]))
     publish_course(c)
 
     # Lastly, lets publish their positions over TF to mimic what auton GUI does
--- src/navigation/search.py
+++ src/navigation/search.py
@@ -1,10 +1,9 @@
 from __future__ import annotations
-from typing import List
-from black import color_diff
+from typing import ClassVar
 
 import numpy as np
 
-from context import Context
+from context import Context, Environment
 from state import BaseState
 from dataclasses import dataclass
 from drive import get_drive_command
@@ -15,12 +14,14 @@ DRIVE_FWD_THRESH = 0.95
 
 @dataclass
 class SearchTrajectory:
-    # coordinates
+    # Coordinates of the trajectory
     coordinates: np.ndarray
     # Associated fiducial for this trajectory
     fid_id: int
-    # pt
+    # Currently tracked coordinate index along trajectory
     cur_pt: int = 0
+    # Helper for building spiral
+    dirs: ClassVar[np.ndarray] = np.array([[0, -1], [-1, 0], [0, 1], [1, 0]])
 
     def get_cur_pt(self) -> np.ndarray:
         return self.coordinates[self.cur_pt]
@@ -44,8 +45,7 @@ class SearchTrajectory:
         """
         # First we will attempt to create the "delta" vectors that get added add each point
         # in the spiral to get to the next.
-        dirs = np.array([[0, -1], [-1, 0], [0, 1], [1, 0]])
-        deltas = np.tile(dirs, (num_turns, 1))
+        deltas = np.tile(cls.dirs, (num_turns, 1))
         # We will build the coeficients for the delta vecs now that we have the correct
         # layout of unit vectors Given the distance parameter 'd', the coef layout we
         # need is [d,d,2d,2d,3d,3d...]
@@ -56,7 +56,10 @@ class SearchTrajectory:
         # also make sure to add the center coordinate in here too so the spiral is in
         # the correct location
         coordinates = np.cumsum(np.vstack((center, deltas)), axis=0)
-        return SearchTrajectory(np.hstack((coordinates, np.zeros(coordinates.shape[0]).reshape(-1, 1))), fid_id)
+        return SearchTrajectory(
+            np.hstack((coordinates, np.zeros(coordinates.shape[0]).reshape(-1, 1))),
+            fid_id,
+        )
 
 
 class SearchState(BaseState):
@@ -76,7 +79,10 @@ class SearchState(BaseState):
         waypoint = self.context.course.current_waypoint()
         if self.traj is None or self.traj.fid_id != waypoint.fiducial_id:
             self.traj = SearchTrajectory.spiral_traj(
-                self.context.rover.get_pose().position_vector()[0:2], 5, 2, waypoint.fiducial_id
+                self.context.rover.get_pose().position_vector()[0:2],
+                5,
+                2,
+                waypoint.fiducial_id,
             )
 
         # continue executing this path from wherever it left off
@@ -94,5 +100,8 @@ class SearchState(BaseState):
 
         self.context.rover.send_drive_command(cmd_vel)
         # if we see the fiduicial, go to the fiducial state
+        current_waypoint = self.context.course.current_waypoint()
+        if current_waypoint.fiducial_id != Environment.NO_FIDUCIAL and self.context.env.current_fid_pos() is not None:
+            return "single_fiducial"
 
         return "search"
