376c2331056dbd85892909d4d5e57f2367d4e45b
Riley Bridges <rlybrdgs@gmail.com>
2022-08-23 14:08:04 -0400

    more SO3 unit tests

--- test/util/SO3_test.py
+++ test/util/SO3_test.py
@@ -19,30 +19,59 @@ from util.SO3 import SO3
 class TestSO3(unittest.TestCase):
     def test_init(self):
         r1 = SO3()
-        self.assertTrue(np.array_equal(r1.quaternion, np.array([0, 0, 0, 1])))
-        
+        self.assertTrue(np.allclose(r1.quaternion, np.array([0, 0, 0, 1])))
+
         r2 = SO3(quaternion=np.array([0, 1, 0, 0]))
-        self.assertTrue(np.array_equal(r2.quaternion, np.array([0, 1, 0, 0])))
-        
+        self.assertTrue(np.allclose(r2.quaternion, np.array([0, 1, 0, 0])))
+
     def test_from_matrix(self):
         r1 = SO3.from_matrix(np.eye(3))
-        self.assertTrue(np.array_equal(r1.quaternion, np.array([0, 0, 0, 1])))
+        self.assertTrue(np.allclose(r1.quaternion, np.array([0, 0, 0, 1])))
 
-        matrix = np.array([[-1, 0, 0],
-                           [0, 1, 0],
-                           [0, 0, -1]])
+        matrix = np.array([[-1, 0, 0], [0, 1, 0], [0, 0, -1]])
         r2 = SO3.from_matrix(matrix)
-        self.assertTrue(np.array_equal(r2.quaternion, np.array([0, 1, 0, 0])))
-        
-        
+        self.assertTrue(np.allclose(r2.quaternion, np.array([0, 1, 0, 0])))
+
     def test_rotation_matrix(self):
-        ...
-        
+        r1 = SO3()
+        self.assertTrue(np.allclose(r1.rotation_matrix(), np.eye(3)))
+
+        r2 = SO3(quaternion=np.array([0.5, 0.5, 0.5, 0.5]))
+        matrix = np.array([[0, 0, 1], [1, 0, 0], [0, 1, 0]])
+        self.assertTrue(np.allclose(r2.rotation_matrix(), matrix))
+
     def test_direction_vector(self):
-        ...
-        
+        r1 = SO3()
+        dir = np.array([1, 0, 0])
+        self.assertTrue(np.allclose(r1.direction_vector(), dir))
+
+        r2 = SO3(quaternion=np.array([0, 0, 0.3826834, 0.9238795]))
+        dir = np.array([2 ** (1 / 2) / 2, 2 ** (1 / 2) / 2, 0])
+        self.assertTrue(np.allclose(r2.direction_vector(), dir))
+
+        r3 = SO3(quaternion=np.array([0.5, -0.5, 0.5, 0.5]))
+        dir = np.array([0, 0, 1])
+        self.assertTrue(np.allclose(r3.direction_vector(), dir))
+
     def test_rot_distance_to(self):
-        ...
+        r1 = SO3()
+        r2 = SO3()
+        self.assertTrue(np.isclose(r1.rot_distance_to(r1), 0))
+        self.assertTrue(np.isclose(r2.rot_distance_to(r2), 0))
+        self.assertTrue(np.isclose(r1.rot_distance_to(r2), 0))
+        self.assertTrue(np.isclose(r2.rot_distance_to(r1), 0))
+        
+        r3 = SO3(quaternion=np.array([0, 0, 2**(1/2)/2, 2**(1/2)/2]))
+        self.assertTrue(np.isclose(r3.rot_distance_to(r3), 0))
+        self.assertTrue(np.isclose(r1.rot_distance_to(r3), np.pi/2))
+        self.assertTrue(np.isclose(r3.rot_distance_to(r1), np.pi/2))
+
+        r4 = SO3(quaternion=np.array([0, 0, -2**(1/2)/2, 2**(1/2)/2]))
+        self.assertTrue(np.isclose(r4.rot_distance_to(r4), 0))
+        self.assertTrue(np.isclose(r1.rot_distance_to(r4), np.pi/2))
+        self.assertTrue(np.isclose(r4.rot_distance_to(r1), np.pi/2))
+        self.assertTrue(np.isclose(r3.rot_distance_to(r4), np.pi))
+        self.assertTrue(np.isclose(r4.rot_distance_to(r3), np.pi))
 
 
 if __name__ == "__main__":
