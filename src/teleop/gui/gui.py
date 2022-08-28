4ec9b31909db52d43f0741bbe61068085233f014
jnnanni <jnnanni@umich.edu>
2022-08-28 12:26:19 -0400

    Fixed gui.py to use rospkg to find mrover packagae directory

--- src/teleop/gui/gui.py
+++ src/teleop/gui/gui.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 import subprocess
 import rospy
+import rospkg
 import os
 
+rospack = rospkg.RosPack()
 rospy.init_node("gui")
-
-os.chdir("../../../src/mrover/src/teleop/gui")
-bashCommand = "yarn serve"
+guiPath = os.path.join(rospack.get_path('mrover'), "src/teleop/gui/")
+os.chdir(guiPath)
+bashCommand = "yarn run serve"
 process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
 output, error = process.communicate()
