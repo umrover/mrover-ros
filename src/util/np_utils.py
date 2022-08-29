72c71fb8292955b9d4a6683c65fa273f8b3e3160
ankithu <ankith.udupa@gmail.com>
2022-08-29 14:42:57 -0400

    addresses util comments

--- src/util/np_utils.py
+++ src/util/np_utils.py
@@ -7,11 +7,6 @@ def normalized(v):
 
 def perpendicular_2d(v):
     if v.shape != (2,) and v.shape != (1,2) and v.shape != (2, 1):
-        print("vector must be 2D!!!")
-        return v
-    if v.shape == (2,):
-        return np.array([-v[0], -v[1]])
-    elif v.shape == (2,1):
-        return np.array([ [-v.flatten()[1]], [-v.flatten()[0]] ])
-    else:
-        return np.array([ [-v.flatten()[1], -v.flatten()[0]] ])
+        raise Exception("vector must be 2D!!!")
+    orig_shape = v.shape
+    return np.reshape((np.array([-v.flatten()[1], -v.flatten()[0]])), orig_shape)
