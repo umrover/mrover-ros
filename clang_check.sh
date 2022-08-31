af3ea6d0d9e16e04861012360d984134b479c4e4
qhdwight <qhdwight@gmail.com>
2022-08-31 17:47:29 -0400

    Made CI use clang-format 12 since that is what is available on Ubuntu 20.04, updated script checker to be better

--- clang_check.sh
+++ clang_check.sh
@@ -1,1 +1,8 @@
-find . -regex '.*\.\(cpp\|hpp\|h\)' -exec clang-format-12 -style=file -i {} \;
+format_executable=clang-format-12
+format_executable_path=$(which "$format_executable")
+if [ ! -x "$format_executable_path" ]; then
+  echo "Please install clang-format with: sudo apt install $format_executable"
+  exit 1
+fi
+
+find . -regex '.*\.\(cpp\|hpp\|h\)' -exec "$format_executable" --dry-run -style=file -i {} \;
