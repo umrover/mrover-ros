b95bc05b737d0b1a714da5ae7681357c36caa491
qhdwight <qhdwight@gmail.com>
2022-09-01 13:27:41 -0400

    Add node and callback

--- starter_project/src/perception.cpp
+++ starter_project/src/perception.cpp
@@ -1,9 +1,9 @@
 #include "perception.hpp"
 
 // C++ Standard Library Headers, std namespace
-#include <string>
 #include <memory>
 #include <optional>
+#include <string>
 #include <unordered_map>
 
 // OpenCV Headers, cv namespace
@@ -11,12 +11,18 @@
 #include <opencv2/core/mat.hpp>
 
 // ROS Headers, ros namespace
-#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <ros/init.h>
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "perception_starter_project");
+    ros::NodeHandle nodeHandle("starter_project"); // Set namespace (See: http://wiki.ros.org/Names)
 
-
+    image_transport::ImageTransport imageTransport(nodeHandle);
+    imageTransport.subscribe("camera/color/image_raw", 1, &imageCallback);
 
     ros::spin();
+}
+
+void imageCallback(const sensor_msgs::ImageConstPtr& image) {
 }
--- starter_project/src/perception.hpp
+++ starter_project/src/perception.hpp
@@ -1,1 +1,5 @@
 #pragma once
+
+#include <sensor_msgs/Image.h>
+
+void imageCallback(sensor_msgs::ImageConstPtr const& image);
