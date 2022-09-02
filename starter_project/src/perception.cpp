2141b214bd373e3b5a92af41fabb0947ceab5f51
qhdwight <qhdwight@gmail.com>
2022-09-02 10:16:52 -0400

    Add skeleton functions to perception

--- starter_project/src/perception.cpp
+++ starter_project/src/perception.cpp
@@ -5,21 +5,22 @@
 #include <optional>
 #include <string>
 #include <unordered_map>
+#include <vector>
 
 // OpenCV Headers, cv namespace
 #include <opencv2/aruco.hpp>
 #include <opencv2/core/mat.hpp>
 
 // ROS Headers, ros namespace
+#include <cv_bridge/cv_bridge.h>
 #include <image_transport/image_transport.h>
 #include <ros/init.h>
 
+
 int main(int argc, char** argv) {
     ros::init(argc, argv, "perception_starter_project"); // Our node name (See: http://wiki.ros.org/Nodes)
-    ros::NodeHandle nodeHandle("starter_project");       // Set namespace (See: http://wiki.ros.org/Names)
 
-    image_transport::ImageTransport imageTransport(nodeHandle);
-    imageTransport.subscribe("camera/color/image_raw", 1, &imageCallback);
+    [[maybe_unused]] mrover::Perception perception;
 
     // "spin" blocks until our node dies
     // It listens for new messages and calls our subscribed functions with them
@@ -28,5 +29,43 @@ int main(int argc, char** argv) {
     return EXIT_SUCCESS;
 }
 
-void imageCallback(sensor_msgs::ImageConstPtr const& image) {
-}
+namespace mrover {
+    Perception::Perception() {
+        ros::NodeHandle nodeHandle("starter_project"); // Set namespace (See: http://wiki.ros.org/Names)
+
+        image_transport::ImageTransport imageTransport(nodeHandle);
+        // Subscribe to camera image messages
+        // Every time another node publishes to this topic we will be notified
+        // Specifically the callback we passed will be invoked
+        // Extra note: We need to use a lambda to bind "this" to "imageCallback"
+        imageTransport.subscribe("camera/color/image_raw", 1,
+                                 [this](sensor_msgs::ImageConstPtr const& image) {
+                                     imageCallback(image);
+                                 });
+
+        mTagPublisher = nodeHandle.advertise<StarterProjectTag>("tag", 1);
+    }
+
+    void Perception::imageCallback(sensor_msgs::ImageConstPtr const& image) {
+        cv_bridge::CvImagePtr cvImage = cv_bridge::toCvCopy(image, sensor_msgs::image_encodings::BGR8);
+        // Detect tags in the image pixels
+        findTagsInImage(cvImage, mTags);
+        // Select the tag that is closest to the middle of the screen
+        StarterProjectTag tag = selectTag(mTags);
+        // Publish the message to our topic so navigation or others can receive it
+        publishTag(tag);
+    }
+
+    void Perception::findTagsInImage(cv_bridge::CvImagePtr const& image, std::vector<StarterProjectTag>& tags) {
+        cv::aruco::detectMarkers(image->image, mTagDictionary, mTagCorners, mTagIds, mTagDetectorParams);
+    }
+
+    StarterProjectTag Perception::selectTag(std::vector<StarterProjectTag> const& tags) {
+        // TODO: implement me!
+        return {};
+    }
+
+    void Perception::publishTag(const StarterProjectTag& tag) {
+        // TODO: implement me!
+    }
+} // namespace mrover
--- starter_project/src/perception.hpp
+++ starter_project/src/perception.hpp
@@ -1,5 +1,58 @@
 #pragma once
 
+#include <opencv2/aruco.hpp>
+
+#include <cv_bridge/cv_bridge.h>
+#include <ros/publisher.h>
+
+#include <mrover/StarterProjectTag.h>
 #include <sensor_msgs/Image.h>
 
-void imageCallback(sensor_msgs::ImageConstPtr const& image);
+namespace mrover {
+
+    /**
+     *  Starter project perception node
+     *
+     *  Input: Image data
+     *  Output: ArUco tag pixel coordinates that is closest to the center of the camera
+     */
+    class Perception {
+    private:
+        cv::Ptr<cv::aruco::DetectorParameters> mTagDetectorParams;
+        cv::Ptr<cv::aruco::Dictionary> mTagDictionary;
+        std::vector<std::vector<cv::Point2f>> mTagCorners;
+        std::vector<int> mTagIds;
+        std::vector<StarterProjectTag> mTags;
+        ros::Publisher mTagPublisher;
+
+    public:
+        Perception();
+
+        /**
+         *
+         * @param image
+         */
+        void imageCallback(sensor_msgs::ImageConstPtr const& image);
+
+        /**
+         *
+         * @param image
+         * @param tags
+         */
+        void findTagsInImage(cv_bridge::CvImagePtr const& image, std::vector<StarterProjectTag>& tags);
+
+        /**
+         *
+         * @param tag
+         */
+        void publishTag(StarterProjectTag const& tag);
+
+        /**
+         *
+         * @param tags
+         * @return
+         */
+        [[nodiscard]] StarterProjectTag selectTag(std::vector<StarterProjectTag> const& tags);
+    };
+
+} // namespace mrover
