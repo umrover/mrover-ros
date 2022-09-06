7048fa6a47d282c5a92260d2565e917c9a0456e5
jnnanni <jnnanni@umich.edu>
2022-09-06 12:20:15 -0400

    Move Joystick data inside gamepad loop

--- starter_project/teleop/src/components/DriveControls.vue
+++ starter_project/teleop/src/components/DriveControls.vue
@@ -54,11 +54,11 @@ created: function () {
             // forward on joystick is -1, so invert
             this.linear = -1 * gamepad.axes[JOYSTICK_CONFIG['forward_back']]
             
+            const joystickData = {
+              'forward_back': this.linear,
+              'left_right': this.rotational,
+            }
           }
-        }
-        const joystickData = {
-          'forward_back': this.linear,
-          'left_right': this.rotational,
         }
         
       }
