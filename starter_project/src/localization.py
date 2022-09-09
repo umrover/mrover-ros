5a71510c481169f9ca176160a347a1aeeaf60e29
Riley Bridges <rlybrdgs@gmail.com>
2022-09-08 22:34:58 -0400

    fixed docstrings

--- starter_project/src/localization.py
+++ starter_project/src/localization.py
@@ -7,24 +7,34 @@ import rospy
 # ROS message types we need to use
 from sensor_msgs.msg import NavSatFix, Imu
 
+# SE3 library for handling poses and TF tree
+from util.SE3 import SE3
+
 
 class Localization:
+    pose: SE3
+
     def __init__(self):
         # create subscribers for GPS and IMU data, linking them to our callback functions
         rospy.Subscriber("gps", NavSatFix, self.gps_callback)
         rospy.Subscriber("imu", Imu, self.imu_callback)
-        self.SE3
 
     def gps_callback(msg: NavSatFix):
         """
         This function will be called every time this node receives a NavSatFix message
         on the /gps topic. It should read the GPS location from the given NavSatFix message,
-        convert it to cartesian coordiantes, and then store that value in `self.SE3`
+        convert it to cartesian coordiantes, store that value in `self.pose`, then publish
+        that pose to the TF tree.
         """
-        ...
+        # TODO
 
     def imu_callback(msg: Imu):
-        ...
+        """
+        This function will be called every time this node receives an Imu message
+        on the /imu topic. It should read the orientation data from the given Imu message,
+        store that value in `self.pose`, then publish that pose to the TF tree.
+        """
+        # TODO
 
     def spherical_to_cartesian(spherical_coord: np.ndarray, reference_coord: np.ndarray) -> np.ndarray:
         """
@@ -37,14 +47,19 @@ class Localization:
         :param reference_coord: the reference coordinate to use for conversion,
                                 given as a numpy array [latiude, longitude]
 
+        :returns: the approximated cartesian coordinates, given as a numpy array [x, y, z]
         """
-        ...
-
+        # TODO
 
 def main():
     # initialize the node
     rospy.init_node("localization")
+
+    # create and start our localization system
     localization = Localization()
+
+    # let the callback functions run asynchronously in the background
+    rospy.spin()
 
 
 if __name__ == "__main__":
