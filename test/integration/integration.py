8bbdb0ddbbe87a14e029cc790715193642f02547
qhdwight <qhdwight@gmail.com>
2022-09-09 11:05:29 -0400

    Refactor out course library into a service class

--- test/integration/integration.py
+++ test/integration/integration.py
@@ -2,16 +2,29 @@
 
 import unittest
 
+import numpy as np
+
 import rospy
+from mrover.msg import Waypoint
+from util.SE3 import SE3
+from util.course_service import CourseService
 
 
 class TestIntegration(unittest.TestCase):
     def test_integration(self):
-        rospy.init_node("test_integration")
+        rospy.init_node("integration_tester")
+        publish_course = CourseService("integration_course_service")
+
+        waypoints = [
+            (Waypoint(fiducial_id=0, tf_id="course0"), SE3(position=np.ndarray([-5.5, -5.5, 0.0]))),
+        ]
+
+        publish_course(waypoints)
+
         rospy.spin()
 
 
 if __name__ == "__main__":
     import rostest
 
-    rostest.rosrun("mrover", "integration", TestIntegration)
+    rostest.rosrun("mrover", "integration_test", TestIntegration)
