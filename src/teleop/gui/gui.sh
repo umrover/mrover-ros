9184c643a780920774aef85a42104dce9cf6f7ae
jnnanni <jnnanni@umich.edu>
2022-09-13 16:29:14 -0400

    Changed gui.sh to use yarn or yarnpkg depending on which is installed, also added yarn.lock to gitignore

--- src/teleop/gui/gui.sh
+++ src/teleop/gui/gui.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 
-#Ensure proper working directory
-roscd mrover/src/teleop/gui
 #Ensure proper NODE_PATH
 export NODE_PATH=/usr/lib/nodejs:/usr/share/nodejs
+
+#Check for yarn vs yarnpkg
+yarn_executable=yarn
+yarn_executable_path=$(which "$yarn_executable")
+yarnpkg_executable=yarnpkg
+yarnpkg_executable_path=$(which "$yarnpkg_executable")
 
 #Install web packages if no node_modules folder
 if [ ! -d "./node_modules/" ] 
 then
-    yarnpkg install
+    if [ ! -z "$yarn_executable_path" ]; then
+        yarn install
+    elif [ ! -z "$yarnpkg_executable_path" ]; then
+        yarnpkg install 
+    else
+        echo "Please run sudo apt install yarn"
+        exit 1
+    fi
 fi
 
-yarnpkg run serve
+
+if [ ! -z "$yarn_executable_path" ]; then
+    yarn run serve
+elif [ ! -z "$yarnpkg_executable_path" ]; then
+    yarnpkg run serve 
+else
+    echo "Please run sudo apt install yarn"
+    exit 1
+fi
