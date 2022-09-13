db174804bf0bcb36777bdae980efa08d8e966ad0
jnnanni <71604997+jnnanni@users.noreply.github.com>
2022-09-13 16:39:08 -0400

    Add Teleop Starter Project (#131)
    * Teleop Training Project Starter Code
    
    * Updated teleop_starter CMakeLists for nested message folder
    
    * Move Joystick data inside gamepad loop

---
+++ starter_project/teleop/gui_starter.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Ensure proper working directory
+roscd mrover/starter_project/teleop
+#Ensure proper NODE_PATH
+export NODE_PATH=/usr/lib/nodejs:/usr/share/nodejs
+
+#Install web packages if no node_modules folder
+if [ ! -d "./node_modules/" ] 
+then
+    yarnpkg install
+fi
+
+yarnpkg run serve
