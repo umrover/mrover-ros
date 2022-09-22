6d6054743d3fd33835aa47e95cbdd1814ee628ec
Riley Bridges <rlybrdgs@gmail.com>
2022-09-22 00:58:56 -0400

    prettier parsing, raise error instead of printing message, better topic names

--- src/esw/imu_driver.py
+++ src/esw/imu_driver.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
 import rospy
 from sensor_msgs.msg import Imu, MagneticField, Temperature
+from geometry_msgs.msg import Quaternion, Vector3
+from std_msgs.msg import Header
 from mrover.msg import CalibrationStatus
 
 import serial
-from serial import SerialException
+from serial import SerialException, SerialTimeoutException
 
 
 def main():
     # publishers for all types of IMU data, queue size is 1 to make sure we don't publish old data
-    imu_pub = rospy.Publisher("imu", Imu, queue_size=1)
-    mag_pub = rospy.Publisher("magnetometer", MagneticField, queue_size=1)
-    temp_pub = rospy.Publisher("imu_temp", Temperature, queue_size=1)
-    calibration_pub = rospy.Publisher("imu_calibration", CalibrationStatus, queue_size=1)
+    imu_pub = rospy.Publisher("imu/data", Imu, queue_size=1)
+    mag_pub = rospy.Publisher("imu/magnetometer", MagneticField, queue_size=1)
+    temp_pub = rospy.Publisher("imu/temp", Temperature, queue_size=1)
+    calibration_pub = rospy.Publisher("imu/calibration", CalibrationStatus, queue_size=1)
 
     # no rate used because rate is effectively set by Arduino serial publisher
     rospy.init_node("imu_driver")
@@ -26,12 +28,6 @@ def main():
     ser = serial.Serial(port, baud)
     attempts = 0
 
-    # initialize messages for IMU data
-    imu_msg = Imu()
-    mag_msg = MagneticField()
-    temp_msg = Temperature()
-    calibration_msg = CalibrationStatus()
-
     while not rospy.is_shutdown():
 
         # try to read a line from the serial connection,
@@ -42,8 +38,7 @@ def main():
         except SerialException:
             attempts += 1
             if attempts > 100:
-                print("unable to read from serial port...ending program")
-                break
+                raise SerialTimeoutException(f"Unable to read from serial port {port}, timed out after 100 attempts")
 
         # parse data into a list of floats,
         # if it fails to parse then skip this message
@@ -51,56 +46,39 @@ def main():
             data = [float(val.strip()) for val in line.split()]
 
         except ValueError:
-            data = []
             print("invalid msg format")
             continue
 
-        # load parsed data into ROS messages,
-        # if it fails to get the data then skip this message
+        # partition data into different sensors, converting calibration data from float to int
+        # if the indices of data don't exist, then skip this message
         try:
-            # orientation
-            imu_msg.orientation.x = data[0]
-            imu_msg.orientation.y = data[1]
-            imu_msg.orientation.z = data[2]
-            imu_msg.orientation.w = data[3]
-
-            # acceleration
-            imu_msg.linear_acceleration.x = data[4]
-            imu_msg.linear_acceleration.y = data[5]
-            imu_msg.linear_acceleration.z = data[6]
-
-            # angular velocity
-            imu_msg.angular_velocity.x = data[7]
-            imu_msg.angular_velocity.y = data[8]
-            imu_msg.angular_velocity.z = data[9]
-            mag_msg.magnetic_field.x = data[10]
-            mag_msg.magnetic_field.y = data[11]
-            mag_msg.magnetic_field.z = data[12]
-
-            # temperature
-            temp_msg.temperature = data[13]
-
-            # calibration status, convert to ints
-            calibration_msg.system_calibration = int(data[14])
-            calibration_msg.gyroscope_calibration = int(data[15])
-            calibration_msg.accelerometer_calibration = int(data[16])
-            calibration_msg.magnetometer_calibration = int(data[17])
+            orientation_data = data[:4]
+            accel_data = data[4:7]
+            gyro_data = data[7:10]
+            mag_data = data[10:13]
+            temp_data = data[13]
+            cal_data = [int(n) for n in data[14:18]]
 
         except IndexError:
             print("incomplete msg")
             continue
 
-        # set timestamps of each message to right now
-        imu_msg.header.stamp = rospy.Time.now()
-        mag_msg.header.stamp = rospy.Time.now()
-        temp_msg.header.stamp = rospy.Time.now()
-        calibration_msg.header.stamp = rospy.Time.now()
+        # fill in all sensor messages, setting timestamps of each message to right now,
+        # and setting the reference frame of all messages to IMU frame
+        imu_msg = Imu(
+            header=Header(stamp=rospy.Time.now(), frame_id=imu_frame),
+            orientation=Quaternion(*orientation_data),
+            linear_acceleration=Vector3(*accel_data),
+            angular_velocity=Vector3(*gyro_data),
+        )
 
-        # set the reference frame of all messages to IMU frame
-        imu_msg.header.frame_id = imu_frame
-        mag_msg.header.frame_id = imu_frame
-        temp_msg.header.frame_id = imu_frame
-        calibration_msg.header.frame_id = imu_frame
+        mag_msg = MagneticField(
+            header=Header(stamp=rospy.Time.now(), frame_id=imu_frame), magnetic_field=Vector3(*mag_data)
+        )
+
+        temp_msg = Temperature(header=Header(stamp=rospy.Time.now(), frame_id=imu_frame), temperature=temp_data)
+
+        calibration_msg = CalibrationStatus(Header(stamp=rospy.Time.now(), frame_id=imu_frame), *cal_data)
 
         # publish each message
         imu_pub.publish(imu_msg)
