bd6cc725e5f3509165d5d867926e91f275595173
qhdwight <qhdwight@gmail.com>
2022-09-25 12:46:04 -0400

    Fix formatting

--- src/esw/drive_placeholder.py
+++ src/esw/drive_placeholder.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
-import moteus
-import moteus_pi3hat
 import rospy
 import time as t
+
 
 def main():
     rospy.init_node(f"brushless_control")
 
     for i in range(5):
-        print('Driving motors')
+        print("Driving motors")
         t.sleep(1)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
