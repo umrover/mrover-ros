c652bef45ee3a6a77f566696cff2f282bdc1a5fa
Polishdudealan <55666129+Polishdudealan@users.noreply.github.com>
2022-09-26 19:24:36 -0400

    Add Raspberry Pi 4 Support & CMakeLists restructure (#153)
    * Added build compatibility for raspi4 64-bit Buster. Added esw directory
    
    * Cleaned up cmake
    
    * removed moteus deps from package.xml
    
    * In progress cmakelist architecture restructure
    
    * CMakelist rewrite
    
    * removed old cmake file. Replaced with new version
    
    * Fix formatting
    
    * Merged with master and removed placeholder code.
    
    * removing placeholder code
    
    * Add find_package for opencv
    
    * Remove gazebo from required
    
    * Add macro function for declaring a new cpp node, fix gazebo includes not working
    
    * Fix tests macro
    
    * Moved individual find_package calls to macro
    
    * Rename variables
    
    Co-authored-by: qhdwight <qhdwight@gmail.com>

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from distutils.core import setup
 
 from catkin_pkg.python_setup import generate_distutils_setup
 
-d = generate_distutils_setup(packages=["localization", "util", "navigation"], package_dir={"": "src"})
+d = generate_distutils_setup(packages=["localization", "util", "navigation", "esw"], package_dir={"": "src"})
 
 setup(**d)
