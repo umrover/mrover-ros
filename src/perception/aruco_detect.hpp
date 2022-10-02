3472178b36b5732ab3f4199f1a44e0297f3a6063
Max Berkowitz <maxberk@umich.edu>
2022-10-02 16:32:08 -0400

    sorry for the dastardly # of commits quinten

--- src/perception/aruco_detect.hpp
+++ src/perception/aruco_detect.hpp
@@ -29,90 +29,90 @@ constexpr char const* ROVER_FRAME = "base_link";
 * @brief Fiducials that are currently visible by the camera.
 */
 struct ImmediateFiducial {
-   int id = -1;
-   cv::Point2f imageCenter{};
-   std::optional<SE3> fidInCam;
+	int id = -1;
+	cv::Point2f imageCenter{};
+	std::optional<SE3> fidInCam;
 };
  
 /**
 * @brief Fiducials that have not been seen enough to be added to global positioning.
 */
 struct IntermediateFiducial {
-   int id = -1;
-   XYZFilter fidInOdomXYZ;
-   double initTimestamp = std::numeric_limits<double>::max();
-   bool seenThisIteration = true;
-   int timesSeen;
+	int id = -1;
+	XYZFilter fidInOdomXYZ;
+	double initTimestamp = std::numeric_limits<double>::max();
+	bool seenThisIteration = true;
+	int timesSeen;
 };
  
 /**
 * @brief Filtered global positioning of fiducials that persist even when off screen.
 */
 struct PersistentFiducial {
-   int id = -1;
-   XYZFilter fidInOdomXYZ;
+	int id = -1;
+	XYZFilter fidInOdomXYZ;
 };
  
 class FiducialsNode {
 private:
-   ros::NodeHandle mNh;
-   ros::NodeHandle mPnh;
+	ros::NodeHandle mNh;
+	ros::NodeHandle mPnh;
 
-   image_transport::Publisher mImgPub;
-   ros::ServiceServer mServiceEnableDetections;
+	image_transport::Publisher mImgPub;
+	ros::ServiceServer mServiceEnableDetections;
 
-   ros::Subscriber mCamInfoSub;
-   ros::Subscriber mIgnoreSub;
-   ros::Subscriber mPcSub;
-   image_transport::Subscriber mImgSub;
-   image_transport::ImageTransport mIt;
-   tf2_ros::Buffer mTfBuffer;
-   tf2_ros::TransformListener mTfListener;
-   tf2_ros::TransformBroadcaster mTfBroadcaster;
+	ros::Subscriber mCamInfoSub;
+	ros::Subscriber mIgnoreSub;
+	ros::Subscriber mPcSub;
+	image_transport::Subscriber mImgSub;
+	image_transport::ImageTransport mIt;
+	tf2_ros::Buffer mTfBuffer;
+	tf2_ros::TransformListener mTfListener;
+	tf2_ros::TransformBroadcaster mTfBroadcaster;
 
-   bool mPublishImages = false; // If set, we publish the images with the fiducials drawn on top
-   bool mEnableDetections = true;
-   bool mIsVerbose = false;
-   bool mHasCamInfo = false;
-   bool mPublishFiducialTf = false;
-   double mMinTimeSeen = 5; //in seconds
-   double mMinFramesSeen = 5;
-   bool mIsTimeBased = true; //as opposed to frame based
-   double mFiducialLen{};
-   std::vector<int> mIgnoreIds;
-   int mFilterCount{};
-   double mFilterProportion{};
-   cv::Ptr<cv::aruco::DetectorParameters> mDetectorParams;
-   cv::Ptr<cv::aruco::Dictionary> mDictionary;
+	bool mPublishImages = false; // If set, we publish the images with the fiducials drawn on top
+	bool mEnableDetections = true;
+	bool mIsVerbose = false;
+	bool mHasCamInfo = false;
+	bool mPublishFiducialTf = false;
+	double mMinTimeSeen = 5; //in seconds
+	double mMinFramesSeen = 5;
+	bool mIsTimeBased = true; //as opposed to frame based
+	double mFiducialLen{};
+	std::vector<int> mIgnoreIds;
+	int mFilterCount{};
+	double mFilterProportion{};
+	cv::Ptr<cv::aruco::DetectorParameters> mDetectorParams;
+	cv::Ptr<cv::aruco::Dictionary> mDictionary;
 
-   uint32_t mSeqNum{};
-   cv_bridge::CvImagePtr mCvPtr;
-   cv::Mat mCamMat;
-   cv::Mat mDistCoeffs;
-   std::string mFrameId;
-   std::optional<size_t> mPrevDetectedCount; // Log spam prevention
-   std::vector<std::vector<cv::Point2f>> mCorners;
-   std::vector<int> mIds;
-   std::unordered_map<int, ImmediateFiducial> mImmediateFiducials;
-   std::unordered_map<int, IntermediateFiducial> mIntermediateFiducials; 
-   std::unordered_map<int, PersistentFiducial> mPersistentFiducials;
-   dynamic_reconfigure::Server<mrover::DetectorParamsConfig> mConfigServer;
-   dynamic_reconfigure::Server<mrover::DetectorParamsConfig>::CallbackType mCallbackType;
+	uint32_t mSeqNum{};
+	cv_bridge::CvImagePtr mCvPtr;
+	cv::Mat mCamMat;
+	cv::Mat mDistCoeffs;
+	std::string mFrameId;
+	std::optional<size_t> mPrevDetectedCount; // Log spam prevention
+	std::vector<std::vector<cv::Point2f>> mCorners;
+	std::vector<int> mIds;
+	std::unordered_map<int, ImmediateFiducial> mImmediateFiducials;
+	std::unordered_map<int, IntermediateFiducial> mIntermediateFiducials; 
+	std::unordered_map<int, PersistentFiducial> mPersistentFiducials;
+	dynamic_reconfigure::Server<mrover::DetectorParamsConfig> mConfigServer;
+	dynamic_reconfigure::Server<mrover::DetectorParamsConfig>::CallbackType mCallbackType;
 
-   void handleIgnoreString(std::string const& str);
+	void handleIgnoreString(std::string const& str);
 
-   void ignoreCallback(std_msgs::String const& msg);
+	void ignoreCallback(std_msgs::String const& msg);
 
-   void imageCallback(sensor_msgs::ImageConstPtr const& msg);
+	void imageCallback(sensor_msgs::ImageConstPtr const& msg);
 
-   void pointCloudCallback(sensor_msgs::PointCloud2ConstPtr const& msg);
+	void pointCloudCallback(sensor_msgs::PointCloud2ConstPtr const& msg);
 
-   void camInfoCallback(sensor_msgs::CameraInfo::ConstPtr const& msg);
+	void camInfoCallback(sensor_msgs::CameraInfo::ConstPtr const& msg);
 
-   void configCallback(mrover::DetectorParamsConfig& config, uint32_t level);
+	void configCallback(mrover::DetectorParamsConfig& config, uint32_t level);
 
-   bool enableDetectionsCallback(std_srvs::SetBool::Request& req, std_srvs::SetBool::Response& res);
+	bool enableDetectionsCallback(std_srvs::SetBool::Request& req, std_srvs::SetBool::Response& res);
  
 public:
-   FiducialsNode();
+	FiducialsNode();
 };
