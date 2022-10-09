afec9d1213eb7cadd07950fdddabf15c5d299743
jnnanni <jnnanni@umich.edu>
2022-10-08 22:45:38 -0400

    Remove useless imports

--- src/util/topic_services.py
+++ src/util/topic_services.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python3
 # Node for teleop-related callback functions
 
-from curses.panel import top_panel
-from math import copysign
-from struct import pack
 import rospy as ros
 import rosmsg
 import rospkg
 from mrover.srv._FetchPackages import FetchPackages, FetchPackagesResponse, FetchPackagesRequest
-import subprocess
 
 class TopicServices():
     
