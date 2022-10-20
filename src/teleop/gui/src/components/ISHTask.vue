3f0f610ada981085d5990b758fa39c5f43a63fa8
ErikLytle <erik@golytle.com>
2022-10-19 20:21:39 -0400

    Initial ISH gui frontend, with site selector component frontend

--- src/teleop/gui/src/components/ISHTask.vue
+++ src/teleop/gui/src/components/ISHTask.vue
@@ -1,10 +1,34 @@
 <template>
-<div>
-    <p>ISH</p>
+<div class="wrapper">
+  <div class="box header">
+      <img src="/static/mrover.png" alt="MRover" title="MRover" width="48" height="48" />
+      <h1>ISH Dashboard</h1>
+      <div class="spacer"></div>
+      <!-- TODO: Add back comms indicators for ROSBridge Server Connection and Rover Connection -->
+      <!-- <div class="comms">
+        <ul id="vitals">
+          <li><CommIndicator v-bind:connected="connections.websocket" name="Web Socket" /></li>
+          <li><CommIndicator v-bind:connected="connections.lcm" name="Rover Connection Status" /></li>
+        </ul>
+      </div> -->
+      <div class="spacer"></div>
+      <div class="help">
+        <img src="/static/help.png" alt="Help" title="Help" width="48" height="48" />
+      </div>
+      <div class="helpscreen"></div>
+      <div class="helpimages" style="display: flex; align-items: center; justify-content: space-evenly">
+        <img src="/static/joystick.png" alt="Joystick" title="Joystick Controls" style="width: auto; height: 70%; display: inline-block" />
+      </div>
+    </div>
+  <div class="box light-bg siteSelect">
+    <SelectSite/>
+  </div>
 </div>
 </template>
 
 <script>
+
+import SelectSite from "./SelectSite.vue"
 
 export default {
   data() {
@@ -14,7 +38,125 @@ export default {
   },
 
   components:{
+    SelectSite
 }
 }
 </script>
+
+<style scoped>
+  .wrapper {
+        display: grid;
+        grid-gap: 10px;
+        grid-template-columns: auto auto auto auto;
+        grid-template-rows: 60px auto auto auto auto auto auto auto auto auto;
+        grid-template-areas: "header header header header"
+                             "cameras cameras cameras siteSelect" 
+                             "cameras cameras cameras raman"
+                             "cameras cameras cameras sudan" 
+                             "carousel carousel chlorophyll chlorophyll"
+                             "carousel carousel chlorophyll chlorophyll"
+                             "cache cache chlorophyll chlorophyll"
+                             "cache cache chlorophyll chlorophyll"
+                             "cache cache amino amino"
+                             "cache cache amino amino";
+        font-family: sans-serif;
+        height: auto;
+  }
+
+  img {
+        border: none;
+        border-radius: 0px;
+  }
+
+  .spacer {
+    flex-grow: 0.8;
+  }
+
+  .helpscreen {
+    z-index: 1000000000;
+    display: block;
+    visibility: hidden;
+    background-color: black;
+    opacity: 0.8;
+    position: absolute;
+    left: 0px;
+    top: 0px;
+    width: 100%;
+    height: 100%;
+  }
+
+  .helpimages {
+    z-index: 1000000001;
+    visibility: hidden;
+    position: absolute;
+    left: 5%;
+    top: 5%;
+    width: 90%;
+    height: 90%;
+  }
+
+  .help {
+    z-index: 1000000002;
+    display: flex;
+    float: right;
+    opacity: 0.8;
+    cursor: auto;
+  }
+
+  .help:hover {
+    opacity: 1.0;
+    cursor: pointer;
+  }
+
+  .help:hover ~ .helpscreen, .help:hover ~ .helpimages {
+    visibility: visible;
+  }
+
+  .box {
+        border-radius: 5px;
+        padding: 10px;
+        border: 1px solid black;
+  }
+
+  .light-bg {
+        background-color: LightGrey;
+  }
+
+  .header {
+        grid-area: header;
+        display: flex;
+        align-items: center;
+  }
+        .header h1 {
+            margin-left: 5px;
+        }
+
+  .cameras {
+      grid-area: cameras;
+  }
+
+  .siteSelect {
+      grid-area: siteSelect;
+  }
+
+  .raman {
+      grid-area: raman;
+  }
+
+  .carousel {
+      grid-area: carousel;
+  }
+
+  .chlorophyll {
+      grid-area: chloropyhll;
+  }
+
+  .cache {
+      grid-area: cache;
+  }
+
+  .amino {
+      grid-area: amino;
+  }
+</style>
 
---
+++ src/teleop/gui/src/components/SelectSite.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+      <div>
+          <h3> Select Site </h3>
+      </div>
+      <div class="box1">
+        <div>
+          <label for="position">Change site to position:</label>
+          <select>
+            <option> Position 0 </option>
+            <option> Position 1 </option>
+            <option> Position 2 </option>
+          </select>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+
+  components:{
+}
+}
+</script>
+
+<style scoped>
+  .box1 {
+    text-align: left;
+    vertical-align: top;
+    display: inline-block;
+  }
+</style>
