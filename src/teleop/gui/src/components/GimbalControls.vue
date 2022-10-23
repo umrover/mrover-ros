4d14f662903e29c98418631057d82600bf7dd899
amszuch <amszuch@umich.edu>
2022-10-23 14:29:46 -0400

    Resolved conflicts and style requests for gimbal controls

--- src/teleop/gui/src/components/GimbalControls.vue
+++ src/teleop/gui/src/components/GimbalControls.vue
@@ -1,10 +1,9 @@
 <template>
     <div class="wrap">
         <div v-show='false' id="key">
-          <input v-on:keydown="keymonitordown">
-          <input v-on:keyup="keymonitorup">
+          <input v-on:keydown="keyMonitorDown" />
+          <input v-on:keyup="keyMonitorUp" />
         </div>
-        <p>Gimbal Controls</p>
     </div>
 </template>
 
@@ -12,25 +11,31 @@
 
 import ROSLIB from "roslib"
 
+//keycodes
+const W = 87
+const A = 65
+const S = 83
+const D = 68
+
 export default {
 
     data() {
-    return {
+        return {
+            keyboard_pub: null,
 
-        keyboard_pub: null,
-
-        inputData : {
-            w_key: 0,
-            a_key: 0,
-            s_key: 0,
-            d_key: 0
-        } 
-    }
+            inputData : {
+                w_key: 0,
+                a_key: 0,
+                s_key: 0,
+                d_key: 0
+            } 
+        }
     },
 
     created: function () {
-        document.addEventListener('keydown', this.keymonitordown);
-        document.addEventListener('keyup', this.keymonitorup);
+
+        document.addEventListener('keyup', this.keyMonitorUp);
+        document.addEventListener('keydown', this.keyMonitorDown);
 
         this.keyboard_pub = new ROSLIB.Topic({
             ros : this.$ros,
@@ -40,38 +45,40 @@ export default {
     },
 
   methods: {
-    keymonitordown: function(event) {
 
-        if (event.keyCode == 87) {
+    // when a key is being pressed down, sets input for that key as 1
+    keyMonitorDown: function(event) {
+        if (event.keyCode == W) {
             this.inputData.w_key = 1;
         }
-        else if (event.keyCode == 65) {
+        else if (event.keyCode == A) {
             this.inputData.a_key = 1;
         }
-        else if (event.keyCode == 83) {
+        else if (event.keyCode == S) {
             this.inputData.s_key = 1;
         }
-        else if (event.keyCode == 68) {
+        else if (event.keyCode == D) {
             this.inputData.d_key = 1;
         }
 
         this.publish()
     },
 
-    keymonitorup: function(event) {
-        if (event.keyCode == 87) {
+    // when a key is released, sets input for that key as 0
+    keyMonitorUp: function(event) {
+        if (event.keyCode == W) {
             this.inputData.w_key = 0;
         }
-        else if (event.keyCode == 65) {
+        else if (event.keyCode == A) {
             this.inputData.a_key = 0;
         }
-        else if (event.keyCode == 83) {
+        else if (event.keyCode == S) {
             this.inputData.s_key = 0;
         }
-        else if (event.keyCode == 68) {
+        else if (event.keyCode == D) {
             this.inputData.d_key = 0;
         }
-
+        
         this.publish()
     },
 
@@ -82,13 +89,12 @@ export default {
         };
 
         this.keyboard_pub.publish(keyboardData)
-    }
-
+    },
   },
-
+  
   beforeDestroy: function () {
-    document.removeEventListener('keydown', this.keymonitordown);
-    document.removeEventListener('keyup', this.keymonitorup);
+        document.removeEventListener('keyup', this.keyMonitorUp);
+        document.removeEventListener('keydown', this.keyMonitorDown);
   }
 }
 </script>
