ba9f1583881b40967653d8def8b0cb1140df6ac7
umroverPerception <umroverPerception@gmail.com>
2022-10-26 20:01:50 -0400

    merged with master

--- src/navigation/test_enable_auton.py
+++ src/navigation/test_enable_auton.py
@@ -11,17 +11,14 @@ def main():
     rospy.init_node("debug_gps_publisher")
     # target = np.array([42.2, -83.7, 0.0])
     target = np.array([42.2, -83.7001, 0.0])
-    print("Starting publisher")
     pub = rospy.Publisher("auton/enable_state", EnableAuton, queue_size=1)
 
     waypoints = [
         GPSWaypoint(target[0], target[1], False, True, 1)
     ]
-    print("Starting")
     
     r = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-        print("message published")
         enable_msg = EnableAuton(True, waypoints)
         pub.publish(enable_msg)
         r.sleep()
