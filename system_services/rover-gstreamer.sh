521d0d49899171f8265d4bde88b7771483a3ec0c
Bill Nye <science-laptop@mrover.com>
2022-10-27 20:49:08 -0400

    Add code

---
+++ system_services/rover-gstreamer.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+/usr/bin/gst-launch-1.0 -v udpsrc port=5000 caps="application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96" ! rtph264depay ! decodebin ! videoconvert ! autovideosink
+/usr/bin/gst-launch-1.0 -v udpsrc port=5001 caps="application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96" ! rtph264depay ! decodebin ! videoconvert ! autovideosink
+/usr/bin/gst-launch-1.0 -v udpsrc port=5002 caps="application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96" ! rtph264depay ! decodebin ! videoconvert ! autovideosink
+/usr/bin/gst-launch-1.0 -v udpsrc port=5003 caps="application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264, payload=(int)96" ! rtph264depay ! decodebin ! videoconvert ! autovideosink
