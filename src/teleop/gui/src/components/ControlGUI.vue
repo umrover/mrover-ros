d9d1589a3cd9a4627482f3297e3970490a09695f
eric <emzhang@umich.edu>
2022-11-09 18:53:55 -0500

    Cleaned up formatting

--- src/teleop/gui/src/components/ControlGUI.vue
+++ src/teleop/gui/src/components/ControlGUI.vue
@@ -2,35 +2,30 @@
 <div>
     <DriveControls></DriveControls>
     <ArmControls></ArmControls>
-    <DriveVelDataH></DriveVelDataH>>
-    <DriveVelDataV></DriveVelDataV>>
-    
-
+    <DriveVelDataH></DriveVelDataH>
+    <DriveVelDataV></DriveVelDataV>
 
 </div>
-
 </template>
 
 <script>
-    import DriveControls from './DriveControls.vue';
-    import ArmControls from './ArmControls.vue';
-    import GimbalControls from './GimbalControls.vue';
-    import DriveVelDataH from './DriveVelDataH.vue'
-    import DriveVelDataV from './DriveVelDataV.vue'
+import DriveControls from './DriveControls.vue';
+import ArmControls from './ArmControls.vue';
+import GimbalControls from './GimbalControls.vue';
+import DriveVelDataH from './DriveVelDataH.vue'
+import DriveVelDataV from './DriveVelDataV.vue'
 
-    export default {
-        data() {
-            return {
-            }
-        },
+export default {
+    data() {
+        return {}
+    },
 
-
-        components:{
-            DriveControls,
-            ArmControls,
-            GimbalControls,
-            DriveVelDataV,
-            DriveVelDataH
-        }
+    components: {
+        DriveControls,
+        ArmControls,
+        GimbalControls,
+        DriveVelDataV,
+        DriveVelDataH
     }
+}
 </script>
--- src/teleop/gui/src/router/index.js
+++ src/teleop/gui/src/router/index.js
@@ -47,7 +47,5 @@ export default new Router({
       name: 'ControlsGUI',
       component: ControlGUI
     },
-    
-    
   ]
 })
