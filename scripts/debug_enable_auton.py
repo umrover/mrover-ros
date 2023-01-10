e558adefde46887ba5c18535ad974087df5bb8ed
Ankith Udupa <ankith.udupa@gmail.com>
2023-01-10 00:02:54 +0000

    moved conversion and publishing logic to navigation, enable auton service 😎

--- scripts/debug_enable_auton.py
+++ scripts/debug_enable_auton.py
@@ -6,6 +6,7 @@ import rospy
 from mrover.msg import GPSWaypoint, WaypointType, EnableAuton
 from util.SE3 import SE3
 from util.course_service import EnableService
+from mrover.srv import PublishEnableAuton
 
 """
 The purpose of this file is for testing courses in the sim 
@@ -15,12 +16,15 @@ and without fiducials and these will get published to nav
 
 if __name__ == "__main__":
     rospy.init_node("debug_enable_auton")
-    publish_enable = EnableService()
-    waypoints = [
-        GPSWaypoint(42.2, -83.7001, WaypointType(val=WaypointType.NO_SEARCH), 1),
-        GPSWaypoint(42.2, -83.7002, WaypointType(val=WaypointType.NO_SEARCH), 1),
-    ]
-    msg = EnableAuton(waypoints, True)
-    publish_enable(msg)
-
+    rospy.wait_for_service("enable_auton")
+    try:
+        publish_enable = rospy.ServiceProxy("enable_auton", PublishEnableAuton)
+        waypoints = [
+            GPSWaypoint(42.2, -83.7001, WaypointType(val=WaypointType.NO_SEARCH), 1),
+            GPSWaypoint(42.2, -83.7002, WaypointType(val=WaypointType.NO_SEARCH), 1),
+        ]
+        msg = EnableAuton(waypoints, True)
+        publish_enable(msg)
+    except rospy.ServiceException as e:
+        print(f"Service call failed: {e}")
     rospy.spin()
