73e75fa3f3eab2da3b02df0d46302f13f813709f
Ankith Udupa <ankith.udupa@gmail.com>
2023-01-10 00:44:38 +0000

    added layer to allow use of debug course publisher and updated integration test 🌎

--- scripts/debug_enable_auton.py
+++ scripts/debug_enable_auton.py
@@ -3,10 +3,9 @@
 import numpy as np
 
 import rospy
-from mrover.msg import GPSWaypoint, WaypointType, EnableAuton
-from util.SE3 import SE3
-from util.course_service import EnableService
-from mrover.srv import PublishEnableAuton
+from mrover.msg import GPSWaypoint, WaypointType
+from util.course_publish_helpers import publish_waypoints
+
 
 """
 The purpose of this file is for testing courses in the sim 
@@ -14,17 +13,12 @@ without needing the auton GUI. You can add waypoints with
 and without fiducials and these will get published to nav
 """
 
+
 if __name__ == "__main__":
     rospy.init_node("debug_enable_auton")
-    rospy.wait_for_service("enable_auton")
-    try:
-        publish_enable = rospy.ServiceProxy("enable_auton", PublishEnableAuton)
-        waypoints = [
+    publish_waypoints(
+        [
             GPSWaypoint(42.2, -83.7001, WaypointType(val=WaypointType.NO_SEARCH), 1),
             GPSWaypoint(42.2, -83.7002, WaypointType(val=WaypointType.NO_SEARCH), 1),
         ]
-        msg = EnableAuton(waypoints, True)
-        publish_enable(msg)
-    except rospy.ServiceException as e:
-        print(f"Service call failed: {e}")
-    rospy.spin()
+    )
