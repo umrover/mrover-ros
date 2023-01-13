90ddac9cc113a246f10bf051ba16454de1cec8ee
jnnanni <jnnanni@umich.edu>
2023-01-13 17:00:31 -0500

    add setup for prettier and eslint

---
+++ src/teleop/gui/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  env: {
+    yarn: true,
+  },
+  extends: ["eslint:recommended", "plugin:vue/vue2-recommended", "prettier"],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+  },
+};
