0aa656a130ae6dca60cfb7de369bc59caad1a9b3
tabiosg <gutstabios@gmail.com>
2023-01-16 14:00:37 -0500

    Remove old brushless.sh file since networking_setup_pi exists now

--- scripts/brushless.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-source /opt/ros/noetic/setup.bash
-source /home/pi/catkin_ws/devel/setup.bash
-export PYTHONPATH=/home/pi/catkin_ws/devel/lib/python3/dist-packages:/opt/ros/noetic/lib/python3/dist-packages
-export ROS_IP=10.0.0.3
-roslaunch mrover brushless.launch
