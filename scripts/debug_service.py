f11e89f9d340c95063ebcf87cfe96403f6de30ae
tabiosg <gutstabios@gmail.com>
2023-01-15 19:33:32 -0500

    STYLE CHECK

--- scripts/debug_service.py
+++ scripts/debug_service.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-'''
+"""
 	Node for testing service requests for services that are still in development
 	Logs the service request to stdout
-'''
+"""
 
 from typing import Any
 import rospy
@@ -11,6 +11,7 @@ from mrover.srv import PublishEnableAuton
 # Change these values for the service name and type definition to test different values
 SERVICE_NAME = "/enable_auton"
 SERVICE_TYPE = PublishEnableAuton
+
 
 def printServiceRequest(service_request: Any):
     rospy.loginfo(service_request)
