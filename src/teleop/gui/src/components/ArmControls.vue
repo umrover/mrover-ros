305c3f78fb0586676c89b53701ffce8007f59c25
eric <emzhang@umich.edu>
2023-01-29 08:21:23 -0500

    Changed styling in armcontrols

--- src/teleop/gui/src/components/ArmControls.vue
+++ src/teleop/gui/src/components/ArmControls.vue
@@ -1,156 +1,184 @@
 <template>
-<div class="wrap">
-    <h3> Arm controls </h3>
+  <div class="wrap">
+    <h3>Arm controls</h3>
     <div class="controls">
-        <Checkbox ref="arm-enabled" v-bind:name="'Arm Enabled'" v-on:toggle="updateArmEnabled($event)" />
-        <Checkbox ref="servo-enabled" v-bind:name="'Servo'" v-on:toggle="updateServo($event)" />
+      <Checkbox
+        ref="arm-enabled"
+        :name="'Arm Enabled'"
+        @toggle="updateArmEnabled($event)"
+      />
+      <Checkbox
+        ref="servo-enabled"
+        :name="'Servo'"
+        @toggle="updateServo($event)"
+      />
     </div>
     <h3>Joint Locks</h3>
     <div class="controls">
-        <Checkbox ref="A" v-bind:name="'A'" v-on:toggle="updateJointsEnabled(0, $event)" />
-        <Checkbox ref="B" v-bind:name="'B'" v-on:toggle="updateJointsEnabled(1, $event)" />
-        <Checkbox ref="C" v-bind:name="'C'" v-on:toggle="updateJointsEnabled(2, $event)" />
-        <Checkbox ref="D" v-bind:name="'D'" v-on:toggle="updateJointsEnabled(3, $event)" />
-        <Checkbox ref="E" v-bind:name="'E'" v-on:toggle="updateJointsEnabled(4, $event)" />
-        <Checkbox ref="F" v-bind:name="'F'" v-on:toggle="updateJointsEnabled(5, $event)" />
+      <Checkbox
+        ref="A"
+        :name="'A'"
+        @toggle="updateJointsEnabled(0, $event)"
+      />
+      <Checkbox
+        ref="B"
+        :name="'B'"
+        @toggle="updateJointsEnabled(1, $event)"
+      />
+      <Checkbox
+        ref="C"
+        :name="'C'"
+        @toggle="updateJointsEnabled(2, $event)"
+      />
+      <Checkbox
+        ref="D"
+        :name="'D'"
+        @toggle="updateJointsEnabled(3, $event)"
+      />
+      <Checkbox
+        ref="E"
+        :name="'E'"
+        @toggle="updateJointsEnabled(4, $event)"
+      />
+      <Checkbox
+        ref="F"
+        :name="'F'"
+        @toggle="updateJointsEnabled(5, $event)"
+      />
     </div>
-</div>
+  </div>
 </template>
 
 <script>
-import ROSLIB from 'roslib'
-import Checkbox from './Checkbox.vue'
-import {
-    mapGetters,
-    mapMutations
-} from 'vuex'
+import ROSLIB from "roslib";
+import Checkbox from "./Checkbox.vue";
+import { mapGetters, mapMutations } from "vuex";
 
 let interval;
 
 export default {
-    data() {
-        return {
-            arm_enabled: false,
-            servo: false,
-            joystick_pub: null,
-            joystickservo_pub: null,
-            jointlock_pub: null,
-            joints_array: [false, false, false, false, false, false]
-        }
-    },
+  data() {
+    return {
+      arm_enabled: false,
+      servo: false,
+      joystick_pub: null,
+      joystickservo_pub: null,
+      jointlock_pub: null,
+      joints_array: [false, false, false, false, false, false]
+    };
+  },
 
-    computed: {
+  computed: {
+    ...mapGetters("controls", {
+      controlMode: "controlMode"
+    })
+  },
 
-        ...mapGetters('controls', {
-            controlMode: 'controlMode'
-        }),
-    },
+  beforeUnmount: function () {
+    window.clearInterval(interval);
+  },
 
-    beforeDestroy: function () {
-        window.clearInterval(interval)
-    },
+  created: function () {
+    this.joystick_pub = new ROSLIB.Topic({
+      ros: this.$ros,
+      name: "/xbox/ra_open_loop",
+      messageType: "sensor_msgs/Joy"
+    });
+    this.joystickservo_pub = new ROSLIB.Topic({
+      ros: this.$ros,
+      name: "/xbox/ra_servo",
+      messageType: "sensor_msgs/Joy"
+    });
+    this.jointlock_pub = new ROSLIB.Topic({
+      ros: this.$ros,
+      name: "/joint_lock",
+      messageType: "mrover/JointLock"
+    });
+    const jointData = {
+      //publishes array of all falses when refreshing the page
+      joints: this.joints_array
+    };
+    var jointlockMsg = new ROSLIB.Message(jointData);
+    this.jointlock_pub.publish(jointlockMsg);
 
-    created: function () {
-        this.joystick_pub = new ROSLIB.Topic({
-            ros: this.$ros,
-            name: '/xbox/ra_open_loop',
-            messageType: 'sensor_msgs/Joy'
-        })
-        this.joystickservo_pub = new ROSLIB.Topic({
-            ros: this.$ros,
-            name: '/xbox/ra_servo',
-            messageType: 'sensor_msgs/Joy'
-        })
-        this.jointlock_pub = new ROSLIB.Topic({
-            ros: this.$ros,
-            name: '/joint_lock',
-            messageType: 'mrover/JointLock'
-        })
-        const jointData = { //publishes array of all falses when refreshing the page
-            joints: this.joints_array
+    const updateRate = 0.1;
+    interval = window.setInterval(() => {
+      if (this.arm_enabled) {
+        const gamepads = navigator.getGamepads();
+        for (let i = 0; i < 4; i++) {
+          const gamepad = gamepads[i];
+          if (gamepad) {
+            if (
+              gamepad.id.includes("Microsoft") ||
+              gamepad.id.includes("Xbox")
+            ) {
+              let buttons = gamepad.buttons.map((button) => {
+                return button.value;
+              });
+
+              const joystickData = {
+                axes: gamepad.axes,
+                buttons: buttons
+              };
+              var joystickMsg = new ROSLIB.Message(joystickData);
+              if (this.servo === true) {
+                this.joystickservo_pub.publish(joystickMsg);
+              } else {
+                this.joystick_pub.publish(joystickMsg);
+              }
+            }
           }
-        var jointlockMsg = new ROSLIB.Message(jointData)
-        this.jointlock_pub.publish(jointlockMsg)
-
-        const updateRate = 0.1
-        interval = window.setInterval(() => {
-            if (this.arm_enabled) {
-                const gamepads = navigator.getGamepads()
-                for (let i = 0; i < 4; i++) {
-                    const gamepad = gamepads[i]
-                    if (gamepad) {
-                        if (gamepad.id.includes('Microsoft') || gamepad.id.includes('Xbox')) {
-                            let buttons = gamepad.buttons.map((button) => {
-                                return button.value
-                            })
-
-                            const joystickData = {
-                                axes: gamepad.axes,
-                                buttons: buttons
-                            }
-                            var joystickMsg = new ROSLIB.Message(joystickData)
-                            if(servo === true)
-                            {
-                              this.joystickservo_pub.publish(joystickMsg)
-                            }
-                            else
-                            {
-                              this.joystick_pub.publish(joystickMsg)
-                            }
-                            
-                        }
-                    }
-                }
-            }
-        }, updateRate * 1000)
-    },
-
-    methods: {
-        updateArmEnabled: function (enabled) {
-            this.arm_enabled = enabled
-        },
-        updateServo: function(enabled) {
-            this.servo = enabled;
-        },
-        updateJointsEnabled: function (jointnum, enabled) {
-            this.joints_array[jointnum] = enabled
-            const jointData = {
-                joints: this.joints_array
-            }
-            var jointlockMsg = new ROSLIB.Message(jointData)
-            this.jointlock_pub.publish(jointlockMsg)
         }
-    },
+      }
+    }, updateRate * 1000);
+  },
 
-    components: {
-        Checkbox
+  methods: {
+    updateArmEnabled: function (enabled) {
+      this.arm_enabled = enabled;
     },
-}
+    updateServo: function (enabled) {
+      this.servo = enabled;
+    },
+    updateJointsEnabled: function (jointnum, enabled) {
+      this.joints_array[jointnum] = enabled;
+      const jointData = {
+        joints: this.joints_array
+      };
+      var jointlockMsg = new ROSLIB.Message(jointData);
+      this.jointlock_pub.publish(jointlockMsg);
+    }
+  },
+
+  components: {
+    Checkbox
+  }
+};
 </script>
 
 <style scoped>
 .wrap {
-    display: inline-block;
-    align-items: center;
-    justify-items: center;
-    width: 100%;
+  display: inline-block;
+  align-items: center;
+  justify-items: center;
+  width: 100%;
 }
 
 .controls {
-    display: flex;
-    align-items: center;
+  display: flex;
+  align-items: center;
 }
 
 .header {
-    display: flex;
-    align-items: center;
+  display: flex;
+  align-items: center;
 }
 
 .joint-b-calibration {
-    display: flex;
-    gap: 10px;
-    width: 250px;
-    font-weight: bold;
-    color: red;
+  display: flex;
+  gap: 10px;
+  width: 250px;
+  font-weight: bold;
+  color: red;
 }
 </style>
