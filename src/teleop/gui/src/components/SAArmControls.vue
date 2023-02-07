47daec8e0cf8203f9b6833c76be92b7920cafeb9
jnnanni <jnnanni@umich.edu>
2023-02-07 11:27:42 -0500

    Add SA Open Loop Controls, document other arm stuff better and abstract some things in ArmControl Class

---
+++ src/teleop/gui/src/components/SAArmControls.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="wrap">
+    <h3>SA Arm controls</h3>
+    <div class="controls">
+      <Checkbox
+        ref="arm-enabled"
+        :name="'SA Arm Enabled'"
+        @toggle="updateArmEnabled($event)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import ROSLIB from "roslib";
+import Checkbox from "./Checkbox.vue";
+
+let interval;
+
+export default {
+  components: {
+    Checkbox
+  },
+  data() {
+    return {
+      arm_enabled: false,
+      joystick_pub: null
+    };
+  },
+
+  beforeDestroy: function () {
+    window.clearInterval(interval);
+  },
+
+  created: function () {
+    this.joystick_pub = new ROSLIB.Topic({
+      ros: this.$ros,
+      name: "/xbox/sa_control",
+      messageType: "sensor_msgs/Joy"
+    });
+    const updateRate = 0.1;
+    interval = window.setInterval(() => {
+      if (this.arm_enabled) {
+        const gamepads = navigator.getGamepads();
+        for (let i = 0; i < 4; i++) {
+          const gamepad = gamepads[i];
+          if (
+            (gamepad && gamepad.id.includes("Microsoft")) ||
+            gamepad.id.includes("Xbox")
+          ) {
+            let buttons = gamepad.buttons.map((button) => {
+              return button.value;
+            });
+
+            const joystickData = {
+              axes: gamepad.axes,
+              buttons: buttons
+            };
+            var joystickMsg = new ROSLIB.Message(joystickData);
+            this.joystick_pub.publish(joystickMsg);
+          }
+        }
+      }
+    }, updateRate * 1000);
+  },
+
+  methods: {
+    updateArmEnabled: function (enabled) {
+      this.arm_enabled = enabled;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.wrap {
+  display: inline-block;
+  align-items: center;
+  justify-items: center;
+  width: 100%;
+}
+.controls {
+  display: flex;
+  align-items: center;
+}
+.header {
+  display: flex;
+  align-items: center;
+}
+.joint-b-calibration {
+  display: flex;
+  gap: 10px;
+  width: 250px;
+  font-weight: bold;
+  color: red;
+}
+</style>
--- src/teleop/jetson/jetson_teleop.py
+++ src/teleop/jetson/jetson_teleop.py
@@ -4,18 +4,19 @@
 import math
 from math import copysign
 import typing
-from enum import IntEnum
+from enum import Enum
 import rospy as ros
 from sensor_msgs.msg import Joy, JointState
 from geometry_msgs.msg import Twist
+from typing import NoReturn
 
 
-def quadratic(val):
+def quadratic(val: float) -> float:
     return copysign(val**2, val)
 
 
 # If below threshold, make output zero
-def deadzone(magnitude, threshold):
+def deadzone(magnitude: float, threshold: float) -> float:
     temp_mag = abs(magnitude)
     if temp_mag <= threshold:
         temp_mag = 0
@@ -36,7 +37,7 @@ class Drive:
         self.max_angular_speed = self.max_wheel_speed / wheel_radius
         self.twist_pub = ros.Publisher("/cmd_vel", Twist, queue_size=100)
 
-    def teleop_drive_callback(self, msg):
+    def teleop_drive_callback(self, msg: Joy):
         joints: typing.Dict[str, JointState] = {}
 
         # Super small deadzone so we can safely e-stop with dampen switch
@@ -81,13 +82,20 @@ class Drive:
 
 
 class ArmControl:
+    class ArmType(Enum):
+        UNKNOWN = 0
+        RA = 1
+        SA = 2
+
     def __init__(self):
         self.xbox_mappings = ros.get_param("teleop/xbox_mappings")
         self.ra_config = ros.get_param("teleop/ra_controls")
+        self.sa_config = ros.get_param("teleop/sa_controls")
 
         self.ra_cmd_pub = ros.Publisher("ra_cmd", JointState, queue_size=100)
+        self.sa_cmd_pub = ros.Publisher("sa_cmd", JointState, queue_size=100)
 
-        self.ra_names = [
+        RA_NAMES = [
             "joint_a",
             "joint_b",
             "joint_c",
@@ -97,36 +105,107 @@ class ArmControl:
             "finger",
             "gripper",
         ]
+        SA_NAMES = ["joint_a", "joint_b", "joint_c", "joint_d", "scoop", "microscope"]
+
         self.ra_cmd = JointState(
-            name=[name for name in self.ra_names],
-            position=[math.nan for i in range(len(self.ra_names))],
-            velocity=[0.0 for i in range(len(self.ra_names))],
-            effort=[math.nan for i in range(len(self.ra_names))],
+            name=[name for name in RA_NAMES],
+            position=[math.nan for i in range(len(RA_NAMES))],
+            velocity=[0.0 for i in range(len(RA_NAMES))],
+            effort=[math.nan for i in range(len(RA_NAMES))],
         )
 
-    def ra_control_callback(self, msg):
+        self.sa_cmd = JointState(
+            name=[name for name in SA_NAMES],
+            position=[math.nan for i in range(len(SA_NAMES))],
+            velocity=[0.0 for i in range(len(SA_NAMES))],
+            effort=[math.nan for i in range(len(SA_NAMES))],
+        )
+
+    def filter_xbox_axis(
+        self,
+        axes_array: "list[float]",
+        axis_name: str,
+        deadzone_threshold: float,
+        quad_control: bool = True,
+    ) -> float:
+        """
+        Applies various filtering functions to an axis for controlling the arm
+        :param axes_array: Axis array from sensor_msgs/Joy, each value is a float from [-1,1]
+        :param axis_name: String representing the axis you are controlling, should match teleop.yaml
+        :param deadzone_threshold: Float representing the deadzone of the axis that you would like to use
+        :param quad_control: Bool for whether or not we want the axis to follow an x^2 curve instead of a linear one
+        Velocities are sent in range [-1,1]
+        :return: Returns an output velocity value for the given joint using the given axis_name
+        """
+        return (
+            quadratic(deadzone(axes_array[self.xbox_mappings[axis_name]], deadzone_threshold))
+            if quad_control
+            else deadzone(axes_array[self.xbox_mappings[axis_name]], deadzone_threshold)
+        )
+
+    def filter_xbox_button(self, button_array: "list[int]", pos_button: str, neg_button: str) -> float:
+        """
+        Applies various filtering functions to an axis for controlling the arm
+        :param button_array: Button array from sensor_msgs/Joy, each value is an int 0 or 1
+        :param pos_button: String representing the positive button for controlling a joint
+        :param neg_button: String representing the negtaive button for controlling a joint
+        :return: Return -1, 0, or 1 depending on what buttons are being pressed
+        """
+        return button_array[self.xbox_mappings[pos_button]] - button_array[self.xbox_mappings[neg_button]]
+
+    def ra_control_callback(self, msg: Joy) -> NoReturn:
+        """
+        Converts a Joy message with the Xbox inputs
+        to a JointState to control the RA Arm in open loop
+        :param msg: Has axis and buttons array for Xbox controller
+        Velocities are sent in range [-1,1]
+        :return:
+        """
+
+        # Filter for xbox triggers, they are typically [-1,1]
         raw_left_trigger = msg.axes[self.xbox_mappings["left_trigger"]]
         left_trigger = raw_left_trigger if raw_left_trigger > 0 else 0
         raw_right_trigger = msg.axes[self.xbox_mappings["right_trigger"]]
         right_trigger = raw_right_trigger if raw_right_trigger > 0 else 0
+
         self.ra_cmd.velocity = [
-            self.ra_config["joint_a"]["multiplier"]
-            * quadratic(deadzone(msg.axes[self.xbox_mappings["left_js_x"]], 0.15)),
-            self.ra_config["joint_b"]["multiplier"]
-            * quadratic(-deadzone(msg.axes[self.xbox_mappings["left_js_y"]], 0.15)),
-            self.ra_config["joint_c"]["multiplier"]
-            * quadratic(-deadzone(msg.axes[self.xbox_mappings["right_js_y"]], 0.15)),
-            self.ra_config["joint_d"]["multiplier"]
-            * quadratic(deadzone(msg.axes[self.xbox_mappings["right_js_x"]], 0.15)),
+            self.ra_config["joint_a"]["multiplier"] * self.filter_xbox_axis(msg.axes, "left_js_x", 0.15, True),
+            self.ra_config["joint_b"]["multiplier"] * self.filter_xbox_axis(msg.axes, "left_js_y", 0.15, True),
+            self.ra_config["joint_c"]["multiplier"] * self.filter_xbox_axis(msg.axes, "right_js_y", 0.15, True),
+            self.ra_config["joint_d"]["multiplier"] * self.filter_xbox_axis(msg.axes, "right_js_x", 0.15, True),
             self.ra_config["joint_e"]["multiplier"] * (right_trigger - left_trigger),
             self.ra_config["joint_f"]["multiplier"]
-            * (msg.buttons[self.xbox_mappings["right_bumper"]] - msg.buttons[self.xbox_mappings["left_bumper"]]),
-            self.ra_config["finger"]["multiplier"]
-            * (msg.buttons[self.xbox_mappings["y"]] - msg.buttons[self.xbox_mappings["a"]]),
-            self.ra_config["gripper"]["multiplier"]
-            * (msg.buttons[self.xbox_mappings["b"]] - msg.buttons[self.xbox_mappings["x"]]),
+            * self.filter_xbox_button(msg.buttons, "right_bumper", "left_bumper"),
+            self.ra_config["finger"]["multiplier"] * self.filter_xbox_button(msg.buttons, "y", "a"),
+            self.ra_config["gripper"]["multiplier"] * self.filter_xbox_button(msg.buttons, "b", "x"),
         ]
         self.ra_cmd_pub.publish(self.ra_cmd)
+
+    def sa_control_callback(self, msg: Joy) -> NoReturn:
+        """
+        Converts a Joy message with the Xbox inputs
+        to a JointState to control the RA Arm in open loop
+        :param msg: Has axis and buttons array for Xbox controller
+        Velocities are sent in range [-1,1]
+        :return:
+        """
+
+        # Filter for xbox triggers, they are typically [-1,1]
+        raw_left_trigger = msg.axes[self.xbox_mappings["left_trigger"]]
+        left_trigger = raw_left_trigger if raw_left_trigger > 0 else 0
+        raw_right_trigger = msg.axes[self.xbox_mappings["right_trigger"]]
+        right_trigger = raw_right_trigger if raw_right_trigger > 0 else 0
+
+        self.sa_cmd.velocity = [
+            self.sa_config["joint_a"]["multiplier"] * self.filter_xbox_axis(msg.axes, "left_js_x", 0.15, True),
+            self.sa_config["joint_b"]["multiplier"] * self.filter_xbox_axis(msg.axes, "left_js_y", 0.15, True),
+            self.sa_config["joint_c"]["multiplier"] * self.filter_xbox_axis(msg.axes, "right_js_y", 0.15, True),
+            self.sa_config["joint_d"]["multiplier"] * self.filter_xbox_axis(msg.axes, "right_js_x", 0.15, True),
+            self.sa_config["scoop"]["multiplier"] * (right_trigger - left_trigger),
+            self.sa_config["microscope"]["multiplier"]
+            * self.filter_xbox_button(msg.buttons, "right_bumper", "left_bumper"),
+        ]
+        self.sa_cmd_pub.publish(self.ra_cmd)
 
 
 def main():
@@ -137,6 +216,7 @@ def main():
 
     ros.Subscriber("joystick", Joy, drive.teleop_drive_callback)
     ros.Subscriber("xbox/ra_control", Joy, arm.ra_control_callback)
+    ros.Subscriber("xbox/sa_control", Joy, arm.sa_control_callback)
 
     ros.spin()
 
