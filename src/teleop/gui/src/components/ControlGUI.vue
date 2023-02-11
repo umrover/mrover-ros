93bf21bef295b100fc266075ad4d64addd0fab9a
tabiosg <gutstabios@gmail.com>
2023-02-11 01:02:36 -0500

    Change to use naming convention MastGimbal

--- src/teleop/gui/src/components/ControlGUI.vue
+++ src/teleop/gui/src/components/ControlGUI.vue
@@ -23,8 +23,8 @@ export default {
   components: {
     DriveControls,
     ArmControls,
-    MastGimbalControls,
     JointStateTable,
+    MastGimbalControls,
     MoteusStateTable,
   },
   data() {
