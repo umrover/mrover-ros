a41038a27cb9a1bd37190dade07ecac87a700ec8
Rocker <rocker@umich.edu>
2023-02-12 17:49:00 -0500

    opencv

--- src/esw/opencvgst.py
+++ src/esw/opencvgst.py
@@ -3,9 +3,9 @@ import cv2
 from multiprocessing import Process
 
 def send():
-    cap_send = cv2.VideoCapture('v4l2src device=/dev/video0 do-timestamp=true ! image/jpeg, width=1280, height=720 ! jpegdec ! videorate ! video/x-raw, framerate=30/1 ! nvvidconv ! appsink name=mysink', cv2.CAP_GSTREAMER)
+    cap_send = cv2.VideoCapture('v4l2src device=/dev/video0 do-timestamp=true ! image/jpeg, width=1280, height=720 ! jpegdec ! videorate ! video/x-raw, framerate=30/1 ! nvvidconv ! appsink', cv2.CAP_GSTREAMER)
 
-    out_send = cv2.VideoWriter('appsrc name=mysource is-live=true do-timestamp=true format=3 ! nvvidconv ! video/x-raw(memory:NVMM) ! nvv4l2h264enc bitrate=4000000 ! video/x-h264 !  rtph264pay config-interval=1 ! udpsink host=10.0.0.7 port=5001 auto-multicast=true' ,cv2.CAP_GSTREAMER,0, 30, (1280,720), True)
+    out_send = cv2.VideoWriter('appsrc is-live=true do-timestamp=true format=3 ! nvvidconv ! video/x-raw(memory:NVMM) ! nvv4l2h264enc bitrate=4000000 ! video/x-h264 !  rtph264pay config-interval=1 ! udpsink host=10.0.0.7 port=5001 auto-multicast=true' ,cv2.CAP_GSTREAMER,0, 30, (1280,720), True)
 
     if not cap_send.isOpened() or not out_send.isOpened():
         print('VideoCapture or VideoWriter not opened')
