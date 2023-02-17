3361773430076fb928922f37d990c7f92f95201d
Riley Bridges <rlybrdgs@gmail.com>
2023-02-16 22:59:34 -0500

    set IMU covariances from config in imu packager

--- src/localization/imu_packager.py
+++ src/localization/imu_packager.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import numpy as np
 import rospy
 from geometry_msgs.msg import Vector3Stamped
 from mrover.msg import ImuAndMag
@@ -11,7 +12,20 @@ class ImuPackager:
     published by gazebo & publish to /imu/data, to replicate the behavior of imu_driver.py
     """
 
+    imu_pub: rospy.Publisher
+    curr_mag: Vector3Stamped
+    orientation_covariance: np.ndarray
+    gyro_covariance: np.ndarray
+    accel_covariance: np.ndarray
+    mag_covariance: np.ndarray
+
     def __init__(self):
+        # TODO: comments
+        self.orientation_covariance = np.array(rospy.get_param("global_ekf/imu_orientation_covariance"))
+        self.gyro_covariance = np.array(rospy.get_param("global_ekf/imu_gyro_covariance"))
+        self.accel_covariance = np.array(rospy.get_param("global_ekf/imu_accel_covariance"))
+        self.mag_covariance = np.array(rospy.get_param("global_ekf/imu_mag_covariance"))
+
         rospy.Subscriber("imu/imu_only", Imu, self.imu_callback)
         rospy.Subscriber("imu/mag_only", Vector3Stamped, self.mag_callback)
 
@@ -19,12 +33,20 @@ class ImuPackager:
         self.curr_mag = None
 
     def imu_callback(self, msg: Imu):
+        msg.orientation_covariance = self.orientation_covariance.flatten().tolist()
+        msg.angular_velocity_covariance = self.gyro_covariance.flatten().tolist()
+        msg.linear_acceleration_covariance = self.accel_covariance.flatten().tolist()
+
         if self.curr_mag is not None:
             self.imu_pub.publish(
                 ImuAndMag(
                     header=msg.header,
                     imu=msg,
-                    mag=MagneticField(header=self.curr_mag.header, magnetic_field=self.curr_mag.vector),
+                    mag=MagneticField(
+                        header=self.curr_mag.header,
+                        magnetic_field=self.curr_mag.vector,
+                        magnetic_field_covariance=self.mag_covariance.flatten().tolist(),
+                    ),
                 )
             )
 
