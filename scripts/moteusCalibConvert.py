c03eba501a2c2d11530bedeec77d9759535de5db
Pi 3 <pi@example.com>
2023-02-16 20:18:36 -0500

    Added ID and aux2 pin config options

--- scripts/moteusCalibConvert.py
+++ scripts/moteusCalibConvert.py
@@ -9,49 +9,72 @@ Write the config to a moteus with:
 
 sudo moteus_tool -t X --write-config filename_write.cfg
 
+Program input prompts for moteus ID, and aux pin configurations.
+
 """
+
+
 def main():
     print("To save a particular configuration, use sudo moteus_tool -t X --dump-config > filename.cfg")
-    print("Enter filename: ")
+    print("Enter source config filename: ")
     input_filename = input()
     output_filename = input_filename.replace(".cfg", "") + "_write.cfg"
-    
+
     with open(input_filename, "r") as input_file:
         output_file = open(output_filename, "w+")
-        
+
         print("Enter target moteus ID:")
         target_id = int(input())
-        print("Enter aux2 pin3 mode: (0 or 6)")
-        target_pin3_mode = input()
-        print("Enter aux2 pin3 pull: (0 or 1)")
-        target_pin3_pull = input()
-        
-        if(target_id >= 0 or target_id <= 8):
-        
-            while(1):
+        print("Default configuration is aux2 pin0 as hall pin.")
+        print("Would you like to configure aux2 pin1 as a hall pin instead? (y/n)")
+        aux2_config = input()
+
+        pin0mode = 6
+        pin0pull = 1
+        pin1mode = 0
+        pin1pull = 0
+        if aux2_config == "y":
+            pin0mode = 0
+            pin0pull = 0
+            pin1mode = 6
+            pin1pull = 1
+
+        if target_id >= 0 or target_id <= 8:
+            while 1:
                 line = input_file.readline()
-                if(line == "\n" or line == ""):
+                if line == "\n" or line == "":
                     break
-                
+
                 # check for id field
-                if(line[0:5] == "id.id"):
+                if line[0:5] == "id.id":
                     line = "id.id " + str(target_id) + "\n"
-                    
-                # check for aux2 pin3 mode
-                if(line[0:16] == "aux2.pins.3.mode"):
-                    line = "aux2.pins.3.mode " + str(target_pin3_mode) + "\n"
-                    
-                # check for aux2 pin3 pull
-                if(line[0:16] == "aux2.pins.3.pull"):
-                    line = "aux2.pins.3.pull " + str(target_pin3_pull) + "\n"
-        
+
+                # check for aux2 pin0 mode
+                elif line[0:16] == "aux2.pins.0.mode":
+                    line = "aux2.pins.0.mode " + str(pin0mode) + "\n"
+
+                # check for aux2 pin0 pull
+                elif line[0:16] == "aux2.pins.0.pull":
+                    line = "aux2.pins.0.pull " + str(pin0pull) + "\n"
+
+                # check for aux2 pin1 mode
+                elif line[0:16] == "aux2.pins.1.mode":
+                    line = "aux2.pins.1.mode " + str(pin1mode) + "\n"
+
+                # check for aux2 pin1 pull
+                elif line[0:16] == "aux2.pins.1.pull":
+                    line = "aux2.pins.1.pull " + str(pin1pull) + "\n"
+
                 output_file.write("conf set " + line)
-                
+
             input_file.close()
             output_file.close()
-            print(f"\nDone converting config \n Run:\n sudo moteus_tool -t [X] --write-config {output_filename} \n to load onto moteus.")
+            print(
+                f"\nDone converting config \n Run:\n sudo moteus_tool -t [X] --write-config {output_filename} \n to load onto moteus."
+            )
         else:
             print(f"Target ID out of range: 0 <= {target_id} <= 8 \n Write cancelled.")
+
 
 if __name__ == "__main__":
     main()
