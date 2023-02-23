7291a4171e650997487fbbf1a4b6b83c16e94de9
epdal <epdal@umich.edu>
2023-02-23 18:28:37 -0500

    fixed so that it doesn't just calculate the correction whenever we change direction

--- src/localization/gps_correction.py
+++ src/localization/gps_correction.py
@@ -15,15 +15,16 @@ class GPS_Correction:
         self.tf_buffer = tf2_ros.Buffer()
 
         # Tunable Parameters
-        self.time_threshold = 3 # seconds
+        self.time_threshold = 3             # seconds
         self.num_points_threshold = 100
-        self.cooling_off_period = 300 # seconds
-        self.callback_rate = 10 # Hz
-        self.linear_vel_threshold = 0.1 #TODO: definitely need to be tuned
-        self.angular_vel_threshold = 0.1 #TODO: definitely need to be tuned
+        self.cooling_off_period = 300       # seconds
+        self.callback_rate = 10             # Hz
+        self.linear_vel_threshold = 0.1     # TODO: definitely need to be tuned
+        self.angular_vel_threshold = 0.1    # TODO: definitely need to be tuned
+        # TODO: deal with times
 
         self.gps_points = np.empty([0,3])
-        self.current_vel = np.ndarray((2,3)) # unit linear and angular velocity vectors
+        self.current_vel = np.zeros((2,3))    # unit linear and angular velocity vectors
         self.driving_straight = False
 
         self.last_update_time = 0
@@ -41,7 +42,7 @@ class GPS_Correction:
         new_vel[0] = new_vel[0] / np.linalg.norm(new_vel[0])    # only normalize the linear component
         self.driving_straight = True    # Assume the robot is driving straight
 
-        if(self.current_vel.empty() or (np.dot(self.current_vel[0], new_vel[0]) > self.linear_vel_threshold)):
+        if(np.dot(self.current_vel[0], new_vel[0]) > self.linear_vel_threshold):
             self.current_vel = new_vel
             return True
         elif(np.linalg.norm(new_vel[0]) > self.angular_vel_threshold):
@@ -57,26 +58,29 @@ class GPS_Correction:
             self.get_new_readings() # Keep collecting points since the heading hasn't changed
         else:
             # TODO: think about when we want to update the heading
-            if((rospy.Time.now() - last_update_time > self.cooling_off_period) and 
-              (self.ast_heading_time > self.time_threshold) and (self.gps_points.size > self.num_points_threshold)):
+            if((rospy.Time.now() - self.last_update_time > self.cooling_off_period) and 
+              (self.last_heading_time > self.time_threshold) and (self.gps_points.size > self.num_points_threshold)):
                   self.heading_correction = self.get_heading_correction()
-                  last_update_time = rospy.Time.now()
+                  self.last_update_time = rospy.Time.now()
             np.delete(self.gps_points, [0,1,2], axis=1)
     
-    # TODO: problem here is unless I call get_heading_correction in the middle of this somewhere it will only correct heading at the end of driving straight 
     def get_new_readings(self):
         """
         Tries to read gps data at a rate specified by self.callback_rate
         Assumes that the rover is in a valid driving configuration so these points are be valid for heading correction
         """
-        lookup_rate = rospy.Rate(self.callback_rate) # X Hz callback rate
+        lookup_rate = rospy.Rate(self.callback_rate)    # X Hz callback rate
         while(self.driving_straight):
             try:
                 # Get linearized transform and append the position in the world frame to our gps_points array
                 transform = SE3.from_tf_tree(self.tf_buffer, self.world_frame, self.rover_frame)
-                self.gps_points.append(transform.position, axis=0) #TODO: might have to reshape transform.position to (1,3)
+                self.gps_points.append(transform.position, axis=0)      # TODO: might have to reshape transform.position to (1,3)
             except(tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
                 continue
+
+            if(self.gps_points.size() > 100 * self.num_points_threshold):   # compute the heading correction if we cross an upper points threshold
+                self.get_heading_correction()
+                self.last_update_time = rospy.Time.now()
 
             lookup_rate.sleep()
 
@@ -89,7 +93,7 @@ class GPS_Correction:
         heading_rotation = np.array([[heading[0], heading[1],  0],
                                     [heading[1], -heading[0], 0], 
                                     [0,          0,           1]])
-        IMU_rotation = SE3.from_tf_tree(self.tf_buffer, self.world_frame, self.rover_frame) #TODO: Gives us the last one, we want to look one up from the middle
+        IMU_rotation = SE3.from_tf_tree(self.tf_buffer, self.world_frame, self.rover_frame)     # TODO: Gives us the last one, we want to look one up from the middle
         correction = np.matmul(np.linalg.inv(IMU_rotation), heading_rotation)
         return correction
 
--- src/localization/gps_linearization.py
+++ src/localization/gps_linearization.py
@@ -77,6 +77,5 @@ def main():
     GPSLinearization()
     rospy.spin()
 
-
 if __name__ == "__main__":
     main()
