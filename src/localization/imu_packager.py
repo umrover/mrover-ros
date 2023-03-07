a07716fd624eeb58839f47650d86d7fb2ff92ccb
umroverPerception <umroverPerception@gmail.com>
2023-03-07 18:41:57 -0500

    PoseWithCovariance wrong, but it publishes

--- src/localization/imu_packager.py
+++ src/localization/imu_packager.py
@@ -4,6 +4,9 @@ import numpy as np
 from geometry_msgs.msg import Vector3Stamped
 from mrover.msg import ImuAndMag
 from sensor_msgs.msg import Imu, MagneticField
+from tf.transformations import quaternion_from_matrix
+from geometry_msgs.msg import PoseWithCovarianceStamped, PoseWithCovariance, Pose, Quaternion
+from std_msgs.msg import Header
 
 
 class ImuPackager:
@@ -17,12 +20,19 @@ class ImuPackager:
         rospy.Subscriber("imu/mag_only", Vector3Stamped, self.mag_callback)
 
         self.imu_pub = rospy.Publisher("imu/data", ImuAndMag, queue_size=1)
+        self.mag_pose_pub = rospy.Publisher("mag_pose/data", PoseWithCovarianceStamped, queue_size=1)
         self.curr_mag = None
 
     def mag_pose(self, msg: Vector3Stamped):
-        mag2d = np.array([msg.x, msg.y])
-        rotationMatrix = np.array([[msg.x, -1 * msg.y], [msg.y, msg.x]])
-        poseVec = rotationMatrix * mag2d
+        rotationMatrix = np.array([[msg.vector.x, -1 * msg.vector.y, 0, 0], [msg.vector.y, msg.vector.x, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+        q = quaternion_from_matrix(rotationMatrix)
+        rospy.loginfo(q)
+        self.mag_pose_pub.publish(
+            PoseWithCovarianceStamped(
+                header=Header(stamp=msg.header.stamp, frame_id="map"),
+                pose=PoseWithCovariance(pose=Pose(orientation=Quaternion(x=q[0], y=q[1], z=q[2], w=q[3])))
+            )
+        )
 
     def imu_callback(self, msg: Imu):
         if self.curr_mag is not None:
@@ -35,6 +45,7 @@ class ImuPackager:
             )
 
     def mag_callback(self, msg: Vector3Stamped):
+        # this should return a quaternion
         self.curr_mag = msg
         self.mag_pose(msg)
 
