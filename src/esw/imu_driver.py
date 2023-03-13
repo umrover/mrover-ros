802eb05079245c3c5ad6bd19b9ceef401cf0fa05
Riley Bridges <rlybrdgs@gmail.com>
2023-03-13 01:19:44 -0400

    style

--- src/esw/imu_driver.py
+++ src/esw/imu_driver.py
@@ -16,7 +16,7 @@ def get_covariances() -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     """
     Get the IMU covariances for orientation, gyroscope, accelerometer, and magnetometer pose
     from rosparam.
-    
+
     :returns: a tuple containing the orientation, gyro, accel, and mag pose covariance matrices in order
     """
     orientation = np.array(rospy.get_param("global_ekf/imu_orientation_covariance"))
@@ -30,7 +30,7 @@ def publish_mag_pose(pub: rospy.Publisher, msg: Vector3Stamped, covariance: np.n
     """
     Estimates the rover's yaw angle from the magnetometer measurement and publishes it
     as a PoseWithCovarianceStamped for use in the EKF.
-    
+
     :param pub: PoseWithCovarianceStamped publisher used to publish the message
     :param msg: magnetic field message used to estimate the yaw angle
     :param covariance: 6x6 covariance matrix for the estimated pose,
@@ -60,7 +60,7 @@ def publish_mag_pose(pub: rospy.Publisher, msg: Vector3Stamped, covariance: np.n
 def inject_covariances(imu_msg: Imu, orientation_cov: np.ndarray, gyro_cov: np.ndarray, accel_cov: np.ndarray):
     """
     Inserts the given covariance matrices into the given IMU message, modifying it in place.
-    
+
     :param imu_msg: the IMU message to add covariances to
     :param orientation_cov: 3x3 orientation covariance matrix [roll, pitch, yaw]
     :param gyro_cov: 3x3 gyroscope (angular vel) covariance matrix [roll, pitch, yaw]
