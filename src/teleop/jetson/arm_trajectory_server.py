2a80871717fb5651c3ef8638b362487817bb2c34
jnnanni <71604997+jnnanni@users.noreply.github.com>
2023-03-20 19:18:17 -0400

    MoveIt Code for 2023 Arm URDF with Rover Model (#355)
    * Arm xacro urdf
    
    * Got arm components to display in rviz, still messed up, need to blenderize stl files bc axes are weird
    
    * Commit progress
    
    * Urdf up to joint E
    
    * visuals of URDF complete
    
    * Define cylinder and box collision xacros
    
    * Add collision models
    
    * Add one collision box and adjust some other, add moveit to Cmake and package.xml
    
    * Clean up old blends and stl files, clean up rviz arm config
    
    * Add stl files to lfs
    
    * Fix moveit dependencies I think
    
    * Fix moveit dependencies this time maybe
    
    * Revert back to having all rospack list-names packages in cmake and package.xml
    
    * Add cylinder for linear actuator to arm urdf
    
    * Change stl files to lfs objects I think
    
    * generated arm package
    
    * xacro link name changes
    
    * Rebuild mrover_arm package
    
    * Regenerated moveit package with controllers for joint groups
    
    * define urdf macro for inertial
    
    * define transmissions in urdf
    
    * Add untested action server for arm joint trajectories
    
    * gazebo integration with arm
    
    * add arm_trajectory server and fake arm launch file, not working/tested rn
    
    * disable self collision gazebo
    
    * update inertials
    
    * Update arm.xacro with box and no collision with world link, regenerate package, set up position control in gazebo with action server
    
    * Update launch file for arm, regenerate package again with new config files and such
    
    * Add lots of comments
    
    * remove unused arg from arm_gazebo launch
    
    * Remove unused arg in arm_gazebo_sim
    
    * remove mrover_arm2
    
    * Make changes based on Cam's suggestions. Add params to arm_gazebo_sim.launch file
    
    * Added some things
    
    * Align all visual geometry
    
    * collision models for links b-c and c-d
    
    * adjust c-d collision
    
    * Finish 2023 URDF.
    
    * Clean up new_arm stuff
    
    * Add pedestal to bottom of arm for now, set up new mrover_arm package and remove joint_f from motion planning since it has no encoder
    
    * Add rover to URDF
    
    * low res rover model
    
    * Generate package for arm with rover viz, need to fix bc link collisions:
    
    * Make bc link collision box tighter
    
    * Fix issues with bc link colliding/not colliding with the rover.
    
    * Style and rviz config file for moveit package fixed
    
    * minor changes to action server
    
    * make mypy happy
    
    * Delete test.txt
    
    ---------
    
    Co-authored-by: bli76 <32941474+bli76@users.noreply.github.com>

---
+++ src/teleop/jetson/arm_trajectory_server.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+# Adapted from https://github.com/ros-industrial/robot_movement_interface/blob/master/ur_driver/scripts/joint_trajectory_action.py
+import rospy
+import time
+import actionlib
+from threading import Lock
+from collections import deque
+from typing import Deque, List
+
+from control_msgs.msg import FollowJointTrajectoryGoal
+from control_msgs.msg import FollowJointTrajectoryResult
+from control_msgs.msg import FollowJointTrajectoryFeedback
+from control_msgs.msg import FollowJointTrajectoryAction
+from trajectory_msgs.msg import JointTrajectory
+from std_msgs.msg import Float64MultiArray
+from sensor_msgs.msg import JointState
+from trajectory_msgs.msg import JointTrajectoryPoint
+
+conf_joint_names = rospy.get_param("teleop/ra_joints/")
+lock = Lock()
+joint_states = JointState()
+
+
+# ------------------------------------------------------------------------
+# Callback function executed after the publication of the current robot position
+# ------------------------------------------------------------------------
+def joint_states_callback(msg: JointState):
+    with lock:
+        global joint_states
+        joint_states = msg
+
+
+class MoveItAction(object):
+
+    # Rearranges the point path following the name convention joint_0, ... joint_6
+    def rearrange(self, joint_trajectory: JointTrajectory) -> None:
+
+        mapping = [joint_trajectory.joint_names.index(j) for j in conf_joint_names]
+
+        # Return early if already arranged properly
+        if mapping == sorted(mapping):
+            return
+        for point in joint_trajectory.points:
+
+            temp_positions: List[float] = []
+            temp_velocities: List[float] = []
+            temp_accelerations: List[float] = []
+            temp_effort: List[float] = []
+
+            # Motion Plan will publish 5 (every joint besides f)
+            # postions velocities and accelerations for each point of the plan
+            for i in range(len(point.positions)):
+                temp_positions.append(point.positions[mapping[i]])
+                temp_velocities.append(point.velocities[mapping[i]])
+                temp_accelerations.append(point.accelerations[mapping[i]])
+
+            point.positions = temp_positions
+            point.velocities = temp_velocities
+            point.accelerations = temp_accelerations
+            point.effort = temp_effort
+
+        joint_trajectory.joint_names = conf_joint_names
+
+    # Action initialisation
+    def __init__(self, name: str) -> None:
+        self._feedback = FollowJointTrajectoryFeedback()
+        self._result = FollowJointTrajectoryResult()
+        self.publisher = rospy.Publisher("ra_cmd", JointState, queue_size=100)
+        self._action_name = name
+        self._as = actionlib.SimpleActionServer(
+            self._action_name, FollowJointTrajectoryAction, execute_cb=self.execute_cb, auto_start=False
+        )
+        self.trajectory_point_queue: Deque[JointTrajectoryPoint] = deque()
+        self.desired_joint_state = JointState(name=conf_joint_names)
+        # Publisher for gazebo position controller
+        self.gazebo_pub = rospy.Publisher("gazebo_arm_controller/command", Float64MultiArray, queue_size=100)
+        self._as.start()
+
+    # Action callback
+    def execute_cb(self, goal: FollowJointTrajectoryGoal) -> None:
+
+        rospy.loginfo("Executing FollowJointTrajectory Action")
+        # It is required to rearrange the arrays because MoveIt doesn't guarantee order preservation
+        self.rearrange(goal.trajectory)
+
+        # A trajectory needs at least 2 points
+        if len(goal.trajectory.points) < 2:
+            return
+
+        time_start = rospy.Time.from_sec(time.time())
+
+        # ------------- Set up command queue
+
+        self.trajectory_point_queue = deque(goal.trajectory.points)
+
+        # ------------- Wait until the termination while providing feedback
+
+        last_point = self.trajectory_point_queue[0]
+        self.trajectory_point_queue.popleft()
+        # Initialize gazebo Float array
+        gazebo_positions = Float64MultiArray(data=[0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+        while len(self.trajectory_point_queue) > 0:
+            point = self.trajectory_point_queue[0]
+            self.trajectory_point_queue.popleft()
+            self.desired_joint_state.position = point.positions
+            self.publisher.publish(self.desired_joint_state)
+            gazebo_positions.data = point.positions
+            self.gazebo_pub.publish(gazebo_positions)
+            # Wait
+            rospy.sleep(point.time_from_start - last_point.time_from_start)
+            # Trajectory abort!
+            # To abort the current movement, it is possible to send an empty trajectory
+            if self._as.is_preempt_requested():
+                self.publisher.publish(joint_states)
+                self._as.set_preempted()
+                return
+            # ---------------------------------------
+            # Feedback
+            self._feedback.joint_names = goal.trajectory.joint_names
+            self._feedback.desired = point
+            with lock:
+                self._feedback.actual.positions = joint_states.position
+                self._feedback.actual.velocities = joint_states.velocity
+                self._feedback.actual.time_from_start = rospy.Time.from_sec(time.time()) - time_start
+            self._as.publish_feedback(self._feedback)
+            # ---------------------------------------
+            last_point = point
+
+        # ---------------------------------------
+        # Result
+        self._result.error_code = 0
+        self._as.set_succeeded(self._result)
+        # ---------------------------------------
+
+
+if __name__ == "__main__":
+    rospy.init_node("mrover_arm_follow_joint_trajectory")
+    rospy.Subscriber("joint_states", JointState, joint_states_callback)
+    MoveItAction("arm_controller/follow_joint_trajectory")
+    rospy.spin()
