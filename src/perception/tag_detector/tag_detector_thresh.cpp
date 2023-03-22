3b8ff7cd577e0f7dd035de916c3862b9f30d573e
umroverPerception <umroverPerception@gmail.com>
2023-03-21 20:35:04 -0400

    fixed build problems

--- src/perception/tag_detector/tag_detector_thresh.cpp
+++ src/perception/tag_detector/tag_detector_thresh.cpp
@@ -3,7 +3,70 @@
 #include "filter.hpp"
 #include <cmath>
 #include <opencv2/core/mat.hpp>
+#include <opencv2/core/types.hpp>
 #include <vector>
+
+
+constexpr size_t IMAGE_WIDTH_WARN_SIZE = 640;
+constexpr size_t IMAGE_HEIGHT_WARN_SIZE = 480;
+
+/**
+  * @brief Convert input image to gray if it is a 3-channels image
+  */
+void convertToGrey(InputArray _in, OutputArray _out) {
+
+    CV_Assert(_in.type() == CV_8UC1 || _in.type() == CV_8UC3);
+
+    if(_in.type() == CV_8UC3)
+        cvtColor(_in, _out, COLOR_BGR2GRAY);
+    else
+        _in.copyTo(_out);
+}
+
+/**
+ * @brief Initial steps on finding square candidates
+ */
+void detectInitialCandidates(Mat &grey, Ptr<DetectorParameters> &params) {
+
+    CV_Assert(params->adaptiveThreshWinSizeMin >= 3 && params->adaptiveThreshWinSizeMax >= 3);
+    CV_Assert(params->adaptiveThreshWinSizeMax >= params->adaptiveThreshWinSizeMin);
+    CV_Assert(params->adaptiveThreshWinSizeStep > 0);
+
+    // number of window sizes (scales) to apply adaptive thresholding
+    int nScales =  (params->adaptiveThreshWinSizeMax - params->adaptiveThreshWinSizeMin) /
+                      params->adaptiveThreshWinSizeStep + 1;
+
+    ////for each value in the interval of thresholding window sizes
+    parallel_for_(Range(0, nScales), [&](const Range& range) {
+        const int begin = range.start;
+        const int end = range.end;
+
+        for (int i = begin; i < end; i++) {
+            int currScale = params->adaptiveThreshWinSizeMin + i * params->adaptiveThreshWinSizeStep;
+            // threshold
+            Mat thresh;
+            _threshold(grey, thresh, currScale, params->adaptiveThreshConstant);
+            cv::imshow("test", thresh);
+            cv::waitKey(0);
+        }
+    });
+}
+
+/**
+ * @brief Detect square candidates in the input image
+ */
+void detectCandidates(InputArray _image, const Ptr<DetectorParameters> &_params) {
+
+    Mat image = _image.getMat();
+    CV_Assert(image.total() != 0);
+
+    /// 1. CONVERT TO GRAY
+    Mat grey;
+    convertToGrey(image, grey);
+
+    /// 2. DETECT FIRST SET OF CANDIDATES
+    detectInitialCandidates(grey, _params);
+}
 
 /**
  * Detect tags from raw image using OpenCV and calculate their screen space centers.
@@ -30,19 +93,19 @@ void TagDetectorNode::imageCallback(sensor_msgs::ImageConstPtr const& msg) {
         // {mCorners, mIds} are the outputs from OpenCV
 
         // change image to greyscale
-        CV_Assert(!_imagePtr->image.empty());
-        Mat grey;
-        convertToGrey(_image.getMat(), grey);
+        CV_Assert(!imagePtr->image.empty());
+        cv::Mat grey;
+        convertToGrey(image.getMat(), grey);
 
         // detect marker candidates
 
-        std::vector< std::vector< Point2f > > candidates;
-        std::vector< std::vector< Point > > contours;
+        std::vector< std::vector< cv::Point2f > > candidates;
+        std::vector< std::vector< cv::Point > > contours;
         std::vector< int > ids;
 
-        std::vector< std::vector< std::vector< Point2f > > > candidatesSet;
-        std::vector< std::vector< std::vector< Point > > > contoursSet;
-        detectCandidates(grey, candidatesSet, countoursSet, mDetectorParams);
+        std::vector< std::vector< std::vector< cv::Point2f > > > candidatesSet;
+        std::vector< std::vector< std::vector< cv::Point > > > contoursSet;
+        detectCandidates(grey, candidatesSet, contoursSet, mDetectorParams);
         cv::aruco::detectMarkers(imagePtr->image, mDictionary, mCorners, mIds, mDetectorParams);
 
 
@@ -61,70 +124,4 @@ void TagDetectorNode::imageCallback(sensor_msgs::ImageConstPtr const& msg) {
     } catch (cv::Exception const& e) {
         ROS_ERROR("cv exception: %s", e.what());
     }
-}
-
-/**
-  * @brief Convert input image to gray if it is a 3-channels image
-  */
-static void convertToGrey(InputArray _in, OutputArray _out) {
-
-    CV_Assert(_in.type() == CV_8UC1 || _in.type() == CV_8UC3);
-
-    if(_in.type() == CV_8UC3)
-        cvtColor(_in, _out, COLOR_BGR2GRAY);
-    else
-        _in.copyTo(_out);
-}
-
-/**
- * @brief Detect square candidates in the input image
- */
-static void detectCandidates(InputArray _image, vector< vector< vector< Point2f > > >& candidatesSetOut,
-                              vector< vector< vector< Point > > >& contoursSetOut, const Ptr<DetectorParameters> &_params) {
-
-    Mat image = _image.getMat();
-    CV_Assert(image.total() != 0);
-
-    /// 1. CONVERT TO GRAY
-    Mat grey;
-    convertToGrey(image, grey);
-
-    std::vector< std::vector< Point2f > > candidates;
-    std::vector< std::vector< Point > > contours;
-    /// 2. DETECT FIRST SET OF CANDIDATES
-    detectInitialCandidates(grey, candidates, contours, _params);
-}
-
-/**
- * @brief Initial steps on finding square candidates
- */
-static void detectInitialCandidates(const Mat &grey, vector< vector< Point2f > > &candidates,
-                                     vector< vector< Point > > &contours,
-                                     const Ptr<DetectorParameters> &params) {
-
-    CV_Assert(params->adaptiveThreshWinSizeMin >= 3 && params->adaptiveThreshWinSizeMax >= 3);
-    CV_Assert(params->adaptiveThreshWinSizeMax >= params->adaptiveThreshWinSizeMin);
-    CV_Assert(params->adaptiveThreshWinSizeStep > 0);
-
-    // number of window sizes (scales) to apply adaptive thresholding
-    int nScales =  (params->adaptiveThreshWinSizeMax - params->adaptiveThreshWinSizeMin) /
-                      params->adaptiveThreshWinSizeStep + 1;
-
-    std::vector< std::vector< std::vector< Point2f > > > candidatesArrays((size_t) nScales);
-    std::vector< std::vector< std::vector< Point > > > contoursArrays((size_t) nScales);
-
-    ////for each value in the interval of thresholding window sizes
-    parallel_for_(Range(0, nScales), [&](const Range& range) {
-        const int begin = range.start;
-        const int end = range.end;
-
-        for (int i = begin; i < end; i++) {
-            int currScale = params->adaptiveThreshWinSizeMin + i * params->adaptiveThreshWinSizeStep;
-            // threshold
-            Mat thresh;
-            _threshold(grey, thresh, currScale, params->adaptiveThreshConstant);
-            cv::imshow("test", thresh);
-            cv::waitKey(0);
-        }
-    });
 }
