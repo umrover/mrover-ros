89253523a37e81cb45e4e66e9fa92d295d03778e
alisonryckman <alison.ryckman@gmail.com>
2023-03-24 01:55:56 -0400

    more build issues

--- src/perception/tag_detector/tag_detector_thresh.cpp
+++ src/perception/tag_detector/tag_detector_thresh.cpp
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <opencv2/core/mat.hpp>
 #include <opencv2/core/types.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <vector>
 
 
@@ -11,14 +12,67 @@ constexpr size_t IMAGE_WIDTH_WARN_SIZE = 640;
 constexpr size_t IMAGE_HEIGHT_WARN_SIZE = 480;
 
 /**
+  *
+  */
+cv::aruco::DetectorParameters::DetectorParameters()
+    : adaptiveThreshWinSizeMin(3),
+      adaptiveThreshWinSizeMax(23),
+      adaptiveThreshWinSizeStep(10),
+      adaptiveThreshConstant(7),
+      minMarkerPerimeterRate(0.03),
+      maxMarkerPerimeterRate(4.),
+      polygonalApproxAccuracyRate(0.03),
+      minCornerDistanceRate(0.05),
+      minDistanceToBorder(3),
+      minMarkerDistanceRate(0.05),
+      cornerRefinementMethod(CORNER_REFINE_NONE),
+      cornerRefinementWinSize(5),
+      cornerRefinementMaxIterations(30),
+      cornerRefinementMinAccuracy(0.1),
+      markerBorderBits(1),
+      perspectiveRemovePixelPerCell(4),
+      perspectiveRemoveIgnoredMarginPerCell(0.13),
+      maxErroneousBitsInBorderRate(0.35),
+      minOtsuStdDev(5.0),
+      errorCorrectionRate(0.6),
+      aprilTagQuadDecimate(0.0),
+      aprilTagQuadSigma(0.0),
+      aprilTagMinClusterPixels(5),
+      aprilTagMaxNmaxima(10),
+      aprilTagCriticalRad( (float)(10* CV_PI /180) ),
+      aprilTagMaxLineFitMse(10.0),
+      aprilTagMinWhiteBlackDiff(5),
+      aprilTagDeglitch(0),
+      detectInvertedMarker(false){}
+
+
+/**
+  * @brief Create a new set of DetectorParameters with default values.
+  */
+cv::Ptr<cv::aruco::DetectorParameters> cv::aruco::DetectorParameters::create() {
+    cv::Ptr<cv::aruco::DetectorParameters> params = cv::makePtr<cv::aruco::DetectorParameters>();
+    return params;
+}
+
+/**
+  * @brief Threshold input image using adaptive thresholding
+  */
+static void threshold(cv::InputArray _in, cv::OutputArray _out, int winSize, double constant) {
+
+    CV_Assert(winSize >= 3);
+    if(winSize % 2 == 0) winSize++; // win size must be odd
+    cv::adaptiveThreshold(_in, _out, 255, cv::ADAPTIVE_THRESH_MEAN_C, cv::THRESH_BINARY_INV, winSize, constant);
+}
+
+/**
   * @brief Convert input image to gray if it is a 3-channels image
   */
-void convertToGrey(InputArray _in, OutputArray _out) {
+void convertToGrey(cv::InputArray _in, cv::OutputArray _out) {
 
     CV_Assert(_in.type() == CV_8UC1 || _in.type() == CV_8UC3);
 
     if(_in.type() == CV_8UC3)
-        cvtColor(_in, _out, COLOR_BGR2GRAY);
+        cvtColor(_in, _out, cv::COLOR_BGR2GRAY);
     else
         _in.copyTo(_out);
 }
@@ -26,7 +80,7 @@ void convertToGrey(InputArray _in, OutputArray _out) {
 /**
  * @brief Initial steps on finding square candidates
  */
-void detectInitialCandidates(Mat &grey, Ptr<DetectorParameters> &params) {
+void detectInitialCandidates(cv::Mat &grey, const cv::Ptr<cv::aruco::DetectorParameters> &params) {
 
     CV_Assert(params->adaptiveThreshWinSizeMin >= 3 && params->adaptiveThreshWinSizeMax >= 3);
     CV_Assert(params->adaptiveThreshWinSizeMax >= params->adaptiveThreshWinSizeMin);
@@ -37,15 +91,18 @@ void detectInitialCandidates(Mat &grey, Ptr<DetectorParameters> &params) {
                       params->adaptiveThreshWinSizeStep + 1;
 
     ////for each value in the interval of thresholding window sizes
-    parallel_for_(Range(0, nScales), [&](const Range& range) {
+    parallel_for_(cv::Range(0, nScales), [&](const cv::Range& range) {
         const int begin = range.start;
         const int end = range.end;
 
         for (int i = begin; i < end; i++) {
             int currScale = params->adaptiveThreshWinSizeMin + i * params->adaptiveThreshWinSizeStep;
             // threshold
-            Mat thresh;
-            _threshold(grey, thresh, currScale, params->adaptiveThreshConstant);
+            cv::Mat thresh;
+            threshold(grey, thresh, currScale, params->adaptiveThreshConstant);
+            
+            //display thresh test
+            cv::namedWindow("test", cv::WINDOW_AUTOSIZE);
             cv::imshow("test", thresh);
             cv::waitKey(0);
         }
@@ -55,13 +112,13 @@ void detectInitialCandidates(Mat &grey, Ptr<DetectorParameters> &params) {
 /**
  * @brief Detect square candidates in the input image
  */
-void detectCandidates(InputArray _image, const Ptr<DetectorParameters> &_params) {
+void detectCandidates(cv::InputArray _image, const cv::Ptr<cv::aruco::DetectorParameters> &_params) {
 
-    Mat image = _image.getMat();
+    cv::Mat image = _image.getMat();
     CV_Assert(image.total() != 0);
 
     /// 1. CONVERT TO GRAY
-    Mat grey;
+    cv::Mat grey;
     convertToGrey(image, grey);
 
     /// 2. DETECT FIRST SET OF CANDIDATES
@@ -95,17 +152,10 @@ void TagDetectorNode::imageCallback(sensor_msgs::ImageConstPtr const& msg) {
         // change image to greyscale
         CV_Assert(!imagePtr->image.empty());
         cv::Mat grey;
-        convertToGrey(image.getMat(), grey);
 
-        // detect marker candidates
+        convertToGrey(imagePtr->image, grey);
 
-        std::vector< std::vector< cv::Point2f > > candidates;
-        std::vector< std::vector< cv::Point > > contours;
-        std::vector< int > ids;
-
-        std::vector< std::vector< std::vector< cv::Point2f > > > candidatesSet;
-        std::vector< std::vector< std::vector< cv::Point > > > contoursSet;
-        detectCandidates(grey, candidatesSet, contoursSet, mDetectorParams);
+        detectCandidates(grey, mDetectorParams);
         cv::aruco::detectMarkers(imagePtr->image, mDictionary, mCorners, mIds, mDetectorParams);
 
 
