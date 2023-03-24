4833de1f6c860376b1af10dabe00455027e47947
bli76 <32941474+bli76@users.noreply.github.com>
2023-03-24 17:50:08 -0400

    move duplicate code into functions

--- scripts/copy_arm_pkg.sh
+++ scripts/copy_arm_pkg.sh
@@ -21,123 +21,94 @@ fi
 rospack find mrover > /dev/null
 declare mrover_pkg=`rospack find mrover`
 
+# arg1: name of arm package
+# arg2: name of directory containing the stl files
+cp_in() {
+    # verify args
+    echo "$1 $2" > /dev/null
+
+    # check that source directories exist
+    if [ ! -d $mrover_pkg/../$1/ ]; then
+        echo "Error: $1 package doesn't exist in catkin_ws/src/"
+        exit 1
+    fi
+
+    if [ ! -d $mrover_pkg/../$1/rover_description/ ]; then
+        echo "Error: $1 package is missing rover_description/"
+        exit 1
+    fi
+
+    # remove existing directories
+    if [ -d $mrover_pkg/src/teleop/$1/ ]; then
+        rm -r $mrover_pkg/src/teleop/$1/
+    fi
+
+    if [ -d $mrover_pkg/rover_description/$2/ ]; then
+        rm -r $mrover_pkg/rover_description/$2/
+    fi
+
+    # copy directories
+    cp -r $mrover_pkg/../$1/. $mrover_pkg/src/teleop/$1/
+    cp -r $mrover_pkg/src/teleop/$1/rover_description/. $mrover_pkg/rover_description/$2/
+    rm -r $mrover_pkg/src/teleop/$1/rover_description/
+}
+
+# arg1: name of arm package
+# arg2: name of directory containing the stl files
+cp_out() {
+    # verify args
+    echo "$1 $2" > /dev/null
+
+    # check that source directories exist
+    if [ ! -d $mrover_pkg/src/teleop/$1/ ]; then
+        echo "Error: $1/ directory doesn't exist in src/teleop/"
+        exit 1
+    fi
+
+    if [ ! -d $mrover_pkg/rover_description/$2/ ]; then
+        echo "Error: $2/ directory doesn't exist in rover_description/"
+        exit 1
+    fi
+
+    # remove existing package
+    if [ -d $mrover_pkg/../$1/ ]; then
+        rm -r $mrover_pkg/../$1/
+    fi
+
+    # copy directories
+    cp -r $mrover_pkg/src/teleop/$1/. $mrover_pkg/../$1/
+    cp -r $mrover_pkg/rover_description/$2/. $mrover_pkg/../$1/rover_description/
+}
+
 # Parse argument.  $1 is the first argument
 case $2 in
     "in")
         case $1 in
             "mrover_arm")
-                # check that source directories exist
-                if [ ! -d $mrover_pkg/../mrover_arm ]; then
-                    echo "Error: mrover_arm package doesn't exist in catkin_ws/src/"
-                    exit 1
-                fi
-
-                if [ ! -d $mrover_pkg/../mrover_arm/rover_description/ ]; then
-                    echo "Error: mrover_arm package is missing rover_description/"
-                    exit 1
-                fi
-
-                # remove existing directories
-                if [ -d $mrover_pkg/src/teleop/mrover_arm/ ]; then
-                    rm -r $mrover_pkg/src/teleop/mrover_arm/
-                fi
-
-                if [ -d $mrover_pkg/rover_description/arm/ ]; then
-                    rm -r $mrover_pkg/rover_description/arm/
-                fi
-
-                # copy directories
-                cp -r $mrover_pkg/../mrover_arm/. $mrover_pkg/src/teleop/mrover_arm/
-                cp -r $mrover_pkg/src/teleop/mrover_arm/rover_description/. $mrover_pkg/rover_description/arm/
-                rm -r $mrover_pkg/src/teleop/mrover_arm/rover_description/
+                cp_in mrover_arm arm
                 ;;
-
             "sa_arm")
-                # check that source directories exist
-                if [ ! -d $mrover_pkg/../mrover_sa_arm/ ]; then
-                    echo "Error: mrover_sa_arm package doesn't exist in catkin_ws/src/"
-                    exit 1
-                fi
-
-                if [ ! -d $mrover_pkg/../mrover_sa_arm/rover_description/ ]; then
-                    echo "Error: mrover_sa_arm package is missing rover_description/"
-                    exit 1
-                fi
-
-                # remove existing directories
-                if [ -d $mrover_pkg/src/teleop/mrover_sa_arm/ ]; then
-                    rm -r $mrover_pkg/src/teleop/mrover_sa_arm/
-                fi
-
-                if [ -d $mrover_pkg/rover_description/sa_arm/ ]; then
-                    rm -r $mrover_pkg/rover_description/sa_arm/
-                fi
-
-                # copy directories
-                cp -r $mrover_pkg/../mrover_sa_arm/. $mrover_pkg/src/teleop/mrover_sa_arm/
-                cp -r $mrover_pkg/src/teleop/mrover_sa_arm/rover_description/. $mrover_pkg/rover_description/sa_arm/
-                rm -r $mrover_pkg/src/teleop/mrover_sa_arm/rover_description/
+                cp_in mrover_sa_arm sa_arm
                 ;;
-
             *)
                 usage
                 exit 1
                 ;;
-
         esac
         ;;
 
     "out")
         case $1 in
             "mrover_arm")
-                # check that source directories exist
-                if [ ! -d $mrover_pkg/src/teleop/mrover_arm/ ]; then
-                    echo "Error: mrover_arm/ directory doesn't exist in src/teleop/"
-                    exit 1
-                fi
-
-                if [ ! -d $mrover_pkg/rover_description/arm/ ]; then
-                    echo "Error: arm/ directory doesn't exist in rover_description/"
-                    exit 1
-                fi
-
-                # remove existing package
-                if [ -d $mrover_pkg/../mrover_arm/ ]; then
-                    rm -r $mrover_pkg/../mrover_arm/
-                fi
-
-                # copy directories
-                cp -r $mrover_pkg/src/teleop/mrover_arm/. $mrover_pkg/../mrover_arm/
-                cp -r $mrover_pkg/rover_description/arm/. $mrover_pkg/../mrover_arm/rover_description/
+                cp_out mrover_arm arm
                 ;;
-
             "sa_arm")
-                # check that source directories exist
-                if [ ! -d $mrover_pkg/src/teleop/mrover_sa_arm/ ]; then
-                    echo "Error: mrover_sa_arm/ directory doesn't exist in src/teleop/"
-                    exit 1
-                fi
-
-                if [ ! -d $mrover_pkg/rover_description/sa_arm/ ]; then
-                    echo "Error: sa_arm/ directory doesn't exist in rover_description/"
-                    exit 1
-                fi
-
-                # remove existing package
-                if [ -d $mrover_pkg/../mrover_sa_arm/ ]; then
-                    rm -r $mrover_pkg/../mrover_sa_arm/
-                fi
-
-                # copy directories
-                cp -r $mrover_pkg/src/teleop/mrover_sa_arm/. $mrover_pkg/../mrover_sa_arm/
-                cp -r $mrover_pkg/rover_description/sa_arm/. $mrover_pkg/../mrover_sa_arm/rover_description/
+                cp_out mrover_sa_arm sa_arm
                 ;;
-            
             *)
                 usage
                 exit 1
                 ;;
-
         esac
         ;;
     
