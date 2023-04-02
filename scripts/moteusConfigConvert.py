705c1704d1125b5bf9feb2b512c8b184d4454243
Pi 3 <pi@example.com>
2023-04-02 15:17:07 -0400

    Cleaned up. Removed code duplication. General QOL fixes.

--- scripts/moteusConfigConvert.py
+++ scripts/moteusConfigConvert.py
@@ -21,59 +21,51 @@ def main():
     output_filename = input_filename.replace(".cfg", "") + "_write.cfg"
 
     with open(input_filename, "r") as input_file:
-        output_file = open(output_filename, "w+")
+        with open(output_filename, "w+") as output_file:
 
-        print("Enter target moteus ID:")
-        target_id = int(input())
-        print("Default configuration is aux2 pin0 as hall pin.")
-        print("Would you like to configure aux2 pin1 as a hall pin instead? (y/n)")
-        aux2_config = input()
+            print("Enter target moteus ID:")
+            target_id = int(input())
+            if target_id < 0 or target_id > 8:
+                print(f"Target ID out of range: 0 <= {target_id} <= 8 \nWrite cancelled.")
+                return
+    
+            print("Default configuration is aux2 pin0 as hall pin.")
+            print("Would you like to configure aux2 pin1 as a hall pin instead? (y/N)")
+            aux2_config = input().lower()
 
-        pin0mode = 6
-        pin0pull = 1
-        pin1mode = 0
-        pin1pull = 0
-        if aux2_config == "y":
-            pin0mode = 0
-            pin0pull = 0
-            pin1mode = 6
-            pin1pull = 1
+            if aux2_config == "y" or aux2_config == "yes":
+                pin0mode = 0
+                pin0pull = 0
+                pin1mode = 6
+                pin1pull = 1
+            else:
+                pin0mode = 6
+                pin0pull = 1
+                pin1mode = 0
+                pin1pull = 0
+            
+            params = {
+                "id.id ": target_id,
+                "aux2.pins.0.mode ": pin0mode,
+                "aux2.pins.0.pull ": pin0pull,
+                "aux2.pins.1.mode ": pin1mode,
+                "aux2.pins.1.pull ": pin1pull
+            }
 
-        if target_id >= 0 or target_id <= 8:
             while 1:
                 line = input_file.readline()
                 if line == "\n" or line == "":
                     break
 
-                # check for id field
-                if line[0:5] == "id.id":
-                    line = "id.id " + str(target_id) + "\n"
-
-                # check for aux2 pin0 mode
-                elif line[0:16] == "aux2.pins.0.mode":
-                    line = "aux2.pins.0.mode " + str(pin0mode) + "\n"
-
-                # check for aux2 pin0 pull
-                elif line[0:16] == "aux2.pins.0.pull":
-                    line = "aux2.pins.0.pull " + str(pin0pull) + "\n"
-
-                # check for aux2 pin1 mode
-                elif line[0:16] == "aux2.pins.1.mode":
-                    line = "aux2.pins.1.mode " + str(pin1mode) + "\n"
-
-                # check for aux2 pin1 pull
-                elif line[0:16] == "aux2.pins.1.pull":
-                    line = "aux2.pins.1.pull " + str(pin1pull) + "\n"
+                for key in params:
+                    if line.startswith(key):
+                        line = key + str(params[key]) + "\n"
 
                 output_file.write("conf set " + line)
 
-            input_file.close()
-            output_file.close()
             print(
-                f"\nDone converting config \n Run:\n sudo moteus_tool -t [X] --write-config {output_filename} \n to load onto moteus."
+                f"\nDone converting config \n Run:\n sudo moteus_tool -t {target_id} --write-config {output_filename} \n to load onto moteus."
             )
-        else:
-            print(f"Target ID out of range: 0 <= {target_id} <= 8 \n Write cancelled.")
 
 
 if __name__ == "__main__":
