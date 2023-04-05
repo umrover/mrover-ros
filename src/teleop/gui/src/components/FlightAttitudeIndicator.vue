2f1cc86b0134c4c6944284c2ebb417fd3318fd8b
jnnanni <jnnanni@umich.edu>
2023-04-04 23:38:06 -0400

    Add flight indicator package, fix package json issues and webpack issues with loading images

---
+++ src/teleop/gui/src/components/FlightAttitudeIndicator.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+    <Attitude :size="300" :pitch="pitch" :roll="roll"></Attitude>
+</div>
+</template>
+
+<script>
+import {
+    Attitude
+} from 'vue-flight-indicators'
+
+export default {
+    components: {
+        Attitude,
+    },
+    data() {
+        return {
+            pitch: 20,
+            roll: 0
+        };
+    },
+
+};
+</script>
+
+<style scoped>
+</style>
