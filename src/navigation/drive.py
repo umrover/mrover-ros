5a7762b139f046f265a956aa9408e36a848697ce
Emerson-Ramey <emramey@umich.edu>
2023-04-16 12:28:08 -0400

    fix docstring

--- src/navigation/drive.py
+++ src/navigation/drive.py
@@ -24,8 +24,7 @@ def get_drive_command(
     :param completion_thresh:       If the distance to the target is less than this stop.
     :param turn_in_place_thresh     Minimum cosine of the angle in between the target and current heading
                                     in order to drive forward. When below, turn in place.
-    :param drive_direction          Direction the rover will move. This will be positive if moving forward
-                                    and negative if moving backward.
+    :param drive_back               True if the rover needs to move backward.
     :return:                        Rover drive effort command.
     """
 
