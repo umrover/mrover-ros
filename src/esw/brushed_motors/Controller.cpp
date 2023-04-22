568ae08acdbb3459987a23924b6c7e132d98d0c8
tabiosg <gutstabios@gmail.com>
2023-04-22 19:50:22 -0400

    Adjust limit switch code

--- src/esw/brushed_motors/Controller.cpp
+++ src/esw/brushed_motors/Controller.cpp
@@ -228,14 +228,6 @@ void Controller::makeLive() {
         I2C::transact(deviceAddress, motorIDRegMask | CONFIG_K_OP, CONFIG_K_WB,
                       CONFIG_K_RB, buffer, nullptr);
 
-        memcpy(buffer, UINT8_POINTER_T(&limitAPresent), sizeof(limitAPresent));
-        I2C::transact(deviceAddress, motorIDRegMask | ENABLE_LIMIT_A_OP, ENABLE_LIMIT_WB,
-                      ENABLE_LIMIT_RB, buffer, nullptr);
-
-        memcpy(buffer, UINT8_POINTER_T(&limitBPresent), sizeof(limitBPresent));
-        I2C::transact(deviceAddress, motorIDRegMask | ENABLE_LIMIT_B_OP, ENABLE_LIMIT_WB,
-                      ENABLE_LIMIT_RB, buffer, nullptr);
-
         memcpy(buffer, UINT8_POINTER_T(&limitAIsActiveHigh), sizeof(limitAIsActiveHigh));
         I2C::transact(deviceAddress, motorIDRegMask | ACTIVE_LIMIT_A_OP, ACTIVE_LIMIT_WB,
                       ACTIVE_LIMIT_RB, buffer, nullptr);
@@ -252,10 +244,17 @@ void Controller::makeLive() {
         I2C::transact(deviceAddress, motorIDRegMask | COUNTS_LIMIT_B_OP, COUNTS_LIMIT_WB,
                       COUNTS_LIMIT_RB, buffer, nullptr);
 
-
         memcpy(buffer, UINT8_POINTER_T(&limitAIsFwd), sizeof(limitAIsFwd));
         I2C::transact(deviceAddress, motorIDRegMask | LIMIT_A_IS_FWD_OP, LIMIT_A_IS_FWD_WB,
                       LIMIT_A_IS_FWD_RB, buffer, nullptr);
+
+        memcpy(buffer, UINT8_POINTER_T(&limitAPresent), sizeof(limitAPresent));
+        I2C::transact(deviceAddress, motorIDRegMask | ENABLE_LIMIT_A_OP, ENABLE_LIMIT_WB,
+                      ENABLE_LIMIT_RB, buffer, nullptr);
+
+        memcpy(buffer, UINT8_POINTER_T(&limitBPresent), sizeof(limitBPresent));
+        I2C::transact(deviceAddress, motorIDRegMask | ENABLE_LIMIT_B_OP, ENABLE_LIMIT_WB,
+                      ENABLE_LIMIT_RB, buffer, nullptr);
 
         isLive = true;
 
