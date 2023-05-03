9071705fb0981b726cdebc02aad16d0a45c51327
tabiosg <gutstabios@gmail.com>
2023-05-03 16:47:59 -0400

    For some odd reason, rock camera doesn't work otherwise

--- src/esw/cameras.py
+++ src/esw/cameras.py
@@ -278,26 +278,29 @@ def send(device=0, host="10.0.0.7", port=5000, bitrate=4000000, quality=0, fps=3
         # These are the settings for the rock camera.
         # Quality 0 is for decent fps for poor quality.
         # Everything else is at 2 fps with increasing quality.
-        if quality == 0:
-            width = 640
-            height = 480
-            fps = 10
-        elif quality == 1:
-            width = 1280
-            height = 720
-            fps = 2
-        elif quality == 2:
-            width = 1600
-            height = 1200
-            fps = 2
-        elif quality == 3:
-            width = 1920
-            height = 1080
-            fps = 2
-        else:
-            width = 3264
-            height = 2448
-            fps = 2
+        # if quality == 0:
+        #     width = 640
+        #     height = 480
+        #     fps = 10
+        # elif quality == 1:
+        #     width = 1280
+        #     height = 720
+        #     fps = 2
+        # elif quality == 2:
+        #     width = 1600
+        #     height = 1200
+        #     fps = 2
+        # elif quality == 3:
+        #     width = 1920
+        #     height = 1080
+        #     fps = 2
+        # else:
+        #     width = 3264
+        #     height = 2448
+        #     fps = 2
+        width = 3264
+        height = 2448
+        fps = 15
     elif width == 640 and height == 480:
         # These are the settings for the microscope camera.
         if quality == 0:
