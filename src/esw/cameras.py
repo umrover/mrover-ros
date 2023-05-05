5740ea8e1e901ebc73f01b254b3fc1a14d96f7db
tabiosg <gutstabios@gmail.com>
2023-05-05 10:40:04 -0400

    Check if port is available

--- src/esw/cameras.py
+++ src/esw/cameras.py
@@ -260,9 +260,15 @@ class StreamManager:
 
                 # If we cannot handle any more streams, return False.
                 available_port_arr = [True, True, True, True]
-                for stream in self._stream_by_device:
+                for i, stream in enumerate(self._stream_by_device):
                     if stream:
                         available_port_arr[stream.port] = False
+                    else:
+                        poll = stream.poll()
+                        if poll is not None:
+                            # This means that it has terminated
+                            self._stream_by_device[i] = None
+                            available_port_arr[stream.port] = False
 
                 available_port = -1
                 for i, port_is_available in enumerate(available_port_arr):
