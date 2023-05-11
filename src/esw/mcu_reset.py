cbc667e43d4abbdb23744870d02d569fa54b61e7
tabiosg <gutstabios@gmail.com>
2023-05-11 19:07:08 -0400

    Commit

--- src/esw/mcu_reset.py
+++ src/esw/mcu_reset.py
@@ -1,9 +1,7 @@
 import RPi.GPIO as GPIO
-from time import sleep
+import time as t
 
 import rospy
-
-from std_msgs.msg import Bool
 
 from mrover.srv import (
     EnableDevice,
@@ -13,14 +11,45 @@ from mrover.srv import (
 
 MOSFET_GATE_PIN = 32  # the pin used as the gate driver is GPIO 4
 
+reset_mcu_autonomously = True
+time_since_last_reset_mcu = t.time()
+mcu_is_active = True
+MCU_RESET_PERIOD_S = rospy.get_param("science/mcu_reset_period_s")
 
-def reset_mcu(req: EnableDeviceRequest) -> EnableDeviceResponse:
+
+def reset_board() -> None:
+    GPIO.output(MOSFET_GATE_PIN, GPIO.LOW)
+    t.sleep(0.5)
+    GPIO.output(MOSFET_GATE_PIN, GPIO.HIGH)
+
+
+def handle_mcu_board_reset(req: EnableDeviceRequest) -> EnableDeviceResponse:
     if req.enable:
-        GPIO.output(MOSFET_GATE_PIN, GPIO.LOW)
-        sleep(0.5)
-        GPIO.output(MOSFET_GATE_PIN, GPIO.HIGH)
+        reset_board()
 
     return EnableDeviceResponse(True)
+
+
+def handle_reset_mcu_autonomously(req: EnableDeviceRequest) -> EnableDeviceResponse:
+    global reset_mcu_autonomously
+    reset_mcu_autonomously = req.enable
+    return EnableDeviceResponse(True)
+
+
+def check_mcu_disconnected(self, event=None) -> bool:
+    """This should check if the MCU is disconnected.
+    If it is disconnected AND if the MCU board has not been
+    reset in the past 10 seconds, then reset it."""
+    global time_since_last_reset_mcu, mcu_is_active
+
+    if not mcu_is_active and time_since_last_reset_mcu - t.time() < MCU_RESET_PERIOD_S:
+        reset_board()
+        time_since_last_reset_mcu = t.time()
+
+        # Set mcu_is_active to true even though it might not actually be true.
+        # This is because we want to reset the variable in case the science node stops publishing data
+        mcu_is_active = True
+    return True
 
 
 def main():
@@ -28,7 +57,10 @@ def main():
     GPIO.setup(MOSFET_GATE_PIN, GPIO.OUT, initial=GPIO.HIGH)
 
     rospy.init_node("mcu_reset")
-    rospy.Service("mcu_board_reset", EnableDevice, reset_mcu)
+    rospy.Service("mcu_board_reset", EnableDevice, handle_mcu_board_reset)
+    rospy.Service("reset_mcu_autonomously", EnableDevice, handle_reset_mcu_autonomously)
+    rospy.Timer(rospy.Duration(1.0), check_mcu_disconnected)
+    rospy.spin()
 
 
 if __name__ == "__main__":
