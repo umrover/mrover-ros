2a03c842f563d2f28c13afa2ec5480cd77487b0b
Ankith Udupa <ankith.udupa@gmail.com>
2023-05-15 17:26:50 -0400

    style

--- src/navigation/failure_identification/watchdog.py
+++ src/navigation/failure_identification/watchdog.py
@@ -23,10 +23,20 @@ class WatchDog:
 
     def get_start_end_rotations(self, dataframe: DataFrame) -> Tuple[SO3, SO3]:
         start_rot = np.array(
-            [dataframe["rot_x"].iloc[0], dataframe["rot_y"].iloc[0], dataframe["rot_z"].iloc[0], dataframe["rot_w"].iloc[0]]
+            [
+                dataframe["rot_x"].iloc[0],
+                dataframe["rot_y"].iloc[0],
+                dataframe["rot_z"].iloc[0],
+                dataframe["rot_w"].iloc[0],
+            ]
         )
         end_rot = np.array(
-            [dataframe["rot_x"].iloc[-1], dataframe["rot_y"].iloc[-1], dataframe["rot_z"].iloc[-1], dataframe["rot_w"].iloc[-1]]
+            [
+                dataframe["rot_x"].iloc[-1],
+                dataframe["rot_y"].iloc[-1],
+                dataframe["rot_z"].iloc[-1],
+                dataframe["rot_w"].iloc[-1],
+            ]
         )
         return SO3(start_rot), SO3(end_rot)
 
@@ -37,9 +47,11 @@ class WatchDog:
 
     def check_angular_stuck(self, delta_time, delta_rot, dataframe) -> bool:
         # check to make sure all cmd_vel_twist values are the same sign and cmd_vel_x values are 0
-        turn_are_all_same_sign = dataframe["cmd_vel_twist"].apply(np.sign).eq(np.sign(dataframe["cmd_vel_twist"].iloc[0])).all()
+        turn_are_all_same_sign = (
+            dataframe["cmd_vel_twist"].apply(np.sign).eq(np.sign(dataframe["cmd_vel_twist"].iloc[0])).all()
+        )
         linear_are_all_zero = dataframe["cmd_vel_x"].eq(0).all()
-        print(f"turn all same sign: f{turn_are_all_same_sign}, linear_are_all_zero: f{linear_are_all_zero}" )
+        print(f"turn all same sign: f{turn_are_all_same_sign}, linear_are_all_zero: f{linear_are_all_zero}")
         if not turn_are_all_same_sign or not linear_are_all_zero:
             print("not turning")
             return False
@@ -50,7 +62,9 @@ class WatchDog:
 
     def check_linear_stuck(self, delta_time, delta_pos, dataframe) -> bool:
         # check to make sure all cmd_vel_x values are the same sign and cmd_vel_twist values are 0
-        linear_are_all_same_sign = dataframe["cmd_vel_x"].apply(np.sign).eq(np.sign(dataframe["cmd_vel_x"].iloc[0])).all()
+        linear_are_all_same_sign = (
+            dataframe["cmd_vel_x"].apply(np.sign).eq(np.sign(dataframe["cmd_vel_x"].iloc[0])).all()
+        )
         linear_are_all_non_zero = dataframe["cmd_vel_x"].ne(0).all()
         print(linear_are_all_same_sign, linear_are_all_non_zero)
         if not linear_are_all_same_sign or not linear_are_all_non_zero:
