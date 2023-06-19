8b200638637d3ce0d35090b1bed019538798584c
Riley Bridges <rlybrdgs@gmail.com>
2023-06-19 09:38:04 -0400

    loaded and visualized terrain point cloud

--- src/localization/pf_test.py
+++ src/localization/pf_test.py
@@ -1,15 +1,52 @@
 import numpy as np
 import matplotlib.pyplot as plt
+# from mpl_toolkits import mplot3d
 from terrain_particle_filter import TerrainParticleFilter
+import rasterio
+import cv2
 
-pf = TerrainParticleFilter(None, 1, 0.0, 0.0, np.array([0.0, 0.0, 0.0]))
-pf_list = []
 
-for i in range(1000):
-    pf_list.append(np.copy(pf.particles[:, :2]))
-    pf.predict(np.array([0.1, 0.0, -1.2]), 0.01)
+def test_predict():
+    pf = TerrainParticleFilter(None, 1, 0.0, 0.0, np.array([0.0, 0.0, 0.0]))
+    pf_list = []
 
-pf_data = np.vstack(pf_list)
-print(pf_data.shape)
-plt.plot(pf_data[:, 0], pf_data[:, 1], "r-", label="Particle Filter")
-plt.show()
+    for i in range(1000):
+        pf_list.append(np.copy(pf.particles[:, :2]))
+        pf.predict(np.array([0.1, 0.0, -1.2]), 0.01)
+
+    pf_data = np.vstack(pf_list)
+    print(pf_data.shape)
+    plt.plot(pf_data[:, 0], pf_data[:, 1], "r-", label="Particle Filter")
+    plt.show()
+
+
+def test_terrain():
+    length = 50
+    width = 50
+    height = 4.820803273566
+    
+    with rasterio.open("terrain.tif") as f:
+        # <pos>0 0 0</pos>
+        # <size>50 50 4.820803273566</size>
+        terrain_map = f.read().squeeze()
+    p_length = length / terrain_map.shape[0]
+    p_width = width / terrain_map.shape[1]
+    print(f"vmin: {np.min(terrain_map)}, vmax: {np.max(terrain_map)}")
+    
+    points = []
+    for i in range(terrain_map.shape[0]):
+        for j in range(terrain_map.shape[1]):
+            x = (i + 0.5) * p_length
+            y = (j + 0.5) * p_width
+            z = terrain_map[i, j] #* height
+            points.append(np.array([x, y, z]))
+    points = np.vstack(points)
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(projection="3d")
+    ax.scatter(points[:, 0], points[:, 1], points[:, 2], c=points[:, 2], marker=".", s=0.1)
+    plt.show()
+
+
+if __name__ == "__main__":
+    test_terrain()
--- src/localization/terrain_particle_filter.py
+++ src/localization/terrain_particle_filter.py
@@ -2,14 +2,18 @@
 import rospy
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
+from sensor_msgs.msg import PointCloud2
+from std_msgs.msg import Header
 import numpy as np
 import matplotlib.pyplot as plt
 from tf.transformations import euler_from_quaternion
+import sensor_msgs.point_cloud2 as pc2
+import rasterio
 
 
 class TerrainParticleFilter:
-    def __init__(self, terrain_map, num_particles: int, sigma_x: float, sigma_theta: float, initial_pose: np.array = None) -> None:
-        self.terrain_map = terrain_map
+    def __init__(self, terrain_filepath: str, num_particles: int, sigma_x: float, sigma_theta: float, initial_pose: np.array = None) -> None:
+        self.terrain_map = self.load_terrain_map(terrain_filepath)
         self.particles = self.init_particles(num_particles, initial_pose)
         self.sigma_x = sigma_x
         self.sigma_theta = sigma_theta
@@ -26,6 +30,37 @@ class TerrainParticleFilter:
             [[np.cos(theta), -np.sin(theta), x], [np.sin(theta), np.cos(theta), y], [0, 0, 1]]
         )
 
+    def load_terrain_map(self, path: str) -> np.array:
+        """
+        Load terrain point cloud from a raster file.
+
+        :param path: path to raster file
+        :returns: NxMx3 np array of terrain points [x, y, z]
+        """
+        length = 50
+        width = 50
+        height = 4.820803273566
+        
+        with rasterio.open(path) as f:
+            terrain_map = f.read().squeeze()
+        p_length = length / terrain_map.shape[0]
+        p_width = width / terrain_map.shape[1]
+        # print(f"vmin: {np.min(terrain_map)}, vmax: {np.max(terrain_map)}")
+        
+        points = np.empty((terrain_map.shape[0], terrain_map.shape[1], 3))
+        for i in range(terrain_map.shape[0]):
+            for j in range(terrain_map.shape[1]):
+                x = (i + 0.5) * p_length - length / 2
+                y = (j + 0.5) * p_width - width / 2
+                z = terrain_map[i, j] #* height
+                points[i, j] = np.array([x, y, z])
+
+        return points 
+        # fig = plt.figure()
+        # ax = fig.add_subplot(projection="3d")
+        # ax.scatter(points[:, 0], points[:, 1], points[:, 2], c=points[:, 2], marker=".", s=0.1)
+        # plt.show()
+    
     def init_particles(self, num_particles: int, initial_pose: np.array) -> np.array:
         """
         Initialize the particles with a gaussian distribution around the initial pose estimate.
@@ -45,8 +80,8 @@ class TerrainParticleFilter:
 
         :param u: np array of commanded velocities [v_x, v_y, omega]
         :param dt: time step
-        """a in a
-            # integrate velocities to get change in pose in the frame of the previous pose
+        """
+        # integrate velocities to get change in pose in the frame of the previous pose
         x2, y2, theta2 = u * dt
         # print(dt)
         # print(x2, y2, theta2)
@@ -64,10 +99,19 @@ class TerrainParticleFilter:
             p += np.array([dx, dy, dtheta, 0])
             p[2] %= 2 * np.pi
 
+    def update(self, z: np.array) -> None:
+        """
+        Update the particle weights based on the IMU sensor measurement.
+        TODO: use full orientation instead of just gravity vector
+
+        :param z: np array of IMU accelerometer measurement [x, y, z]
+        """
+        ...
+
 
 class TerrainParticleFilterNode:
     def __init__(self) -> None:
-        self.terrain_map = None
+        self.terrain_filepath = "terrain.tif"
         self.num_particles = 1
         self.initialized = False
         self.last_time = rospy.Time.now()
@@ -76,8 +120,21 @@ class TerrainParticleFilterNode:
         self.gt_list = []
         rospy.Subscriber("/cmd_vel", Twist, self.cmd_callback)
         rospy.Subscriber("/ground_truth", Odometry, self.ground_truth_callback)
+        self.terrain_pub = rospy.Publisher("/terrain_map", PointCloud2, queue_size=10)
         rospy.on_shutdown(self.plot_data)
 
+    def publish_terrain_map(self) -> None:
+        """
+        Publish the terrain map as a point cloud.
+        """
+        if not self.initialized:
+            return
+        header = Header()
+        header.stamp = rospy.Time.now()
+        header.frame_id = "map"
+        pc = pc2.create_cloud_xyz32(header, self.pf.terrain_map)
+        self.terrain_pub.publish(pc)
+        
     def cmd_callback(self, msg: Twist) -> None:
         if not self.initialized:
             return
@@ -91,12 +148,13 @@ class TerrainParticleFilterNode:
         p = msg.pose.pose.position
         q = msg.pose.pose.orientation
         _, _, theta = euler_from_quaternion([q.x, q.y, q.z, q.w])
+        theta %= 2 * np.pi
 
         if not self.initialized:
-            self.initialized = True
             self.pf = TerrainParticleFilter(
-                self.terrain_map, self.num_particles, 0.0, 0.0, np.array([p.x, p.y, theta])
+                self.terrain_filepath, self.num_particles, 0.0, 0.0, np.array([p.x, p.y, theta])
             )
+            self.initialized = True
             print(np.array([p.x, p.y, theta]))
 
         if self.gt_count < 100:
@@ -121,12 +179,14 @@ class TerrainParticleFilterNode:
 
         axs[1].plot(self.pf_data[:, 2], "r-", label="Particle Filter")
         axs[1].plot(self.gt_data[:, 2], "b-", label="Ground Truth")
+        plt.legend()
         plt.show()
 
 
 if __name__ == "__main__":
     rospy.init_node("terrain_particle_filter")
-    TerrainParticleFilterNode()
-    rospy.spin()
-    
-4w10:
+    n = TerrainParticleFilterNode()
+    while not rospy.is_shutdown():
+        n.publish_terrain_map()
+        rospy.sleep(0.1)
+    # rospy.spin()
