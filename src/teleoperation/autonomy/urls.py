d4d41fc3408181121a281abf5feee0f12ace1f91
qhdwight <qhdwight@gmail.com>
2023-09-21 10:29:11 -0400

    Fix mypy warnings

--- src/teleoperation/autonomy/urls.py
+++ src/teleoperation/autonomy/urls.py
@@ -2,5 +2,4 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [
-]
+urlpatterns = []  # type: ignore
--- src/teleoperation/teleoperation/settings.py
+++ src/teleoperation/teleoperation/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from typing import List
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-ry$ow)(e+(0g!+b*no_1#57#kjto4bc$z97-9k4zj$)78z$050
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = []  # type: ignore
 
 # Application definition
 
