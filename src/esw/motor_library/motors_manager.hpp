464683fc5d6588650469307bc08d1dce26459127
tabiosg <gutstabios@gmail.com>
2023-09-24 18:43:03 -0400

    Update motors manager to distinguish between brushed and brushless

--- src/esw/motor_library/motors_manager.hpp
+++ src/esw/motor_library/motors_manager.hpp
@@ -6,6 +6,21 @@
 
 class MotorsManager {
 public:
+    MotorsManager(const std::vector<std::string>& controllerNames, XmlRpc::XmlRpcValue root) {
+        for (const std::string& name: controllerNames) {
+            assert(root[name].hasMember("type") &&
+               root[name]["type"].getType() == XmlRpc::XmlRpcValue::TypeString);
+            std::string type = static_cast<std::string>(root[name]["type"]);
+            assert(type == "brushed" || type == "brushless");
+            if (type == "brushed") {
+                m[name] = std::make_unique<BrushedController>();    
+            }
+            else if (type == "brushless") {
+                m[name] = std::make_unique<BrushlessController>();
+            }
+        }
+    }
+
     auto& get_controller(std::string const& name) {
         return m.at(name);
     }
