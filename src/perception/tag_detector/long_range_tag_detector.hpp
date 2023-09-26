ce56c5326ed880cde673c7947e74d320b4c85e55
Marshall Vielmetti <mvielmet@umich.edu>
2023-09-26 16:08:58 -0400

    Wrote detection and hit updating 4 long range tags

--- src/perception/tag_detector/long_range_tag_detector.hpp
+++ src/perception/tag_detector/long_range_tag_detector.hpp
@@ -1,4 +1,12 @@
+#include <opencv2/core/types.hpp>
 namespace mrover {
+
+    typedef struct LongRangeTagType {
+        bool updated = false;
+        int id = -1;
+        int hitCount = 0;
+        cv::Point2f imageCenter{};
+    } LongRangeTag;
 
     class LongRangeTagDetectorNodelet : public nodelet::Nodelet {
         ros::NodeHandle mNh, mPnh;
@@ -16,21 +24,33 @@ namespace mrover {
         bool mPublishImages{}; // If set, we publish the images with the tags drawn on top
         int mMinHitCountBeforePublish{};
         int mMaxHitCount{};
+        int mBaseHitCount{}; //Value the tag is initialized with after first detection. (Not incremented)
         int mTagIncrementWeight{};
         int mTagDecrementWeight{};
+        int mTagRemoveWeight{}; //weight value before the tag is removed
+
+        //ARUCO TAG DETECTION VARIABLES
 
         cv::Ptr<cv::aruco::DetectorParameters> mDetectorParams;
         cv::Ptr<cv::aruco::Dictionary> mDictionary;
+
+        //IMAGE MESSAGE VARIABLES
 
         cv::Mat mImg;
         cv::Mat mGrayImg;
         sensor_msgs::Image mImgMsg;
         sensor_msgs::Image mThreshMsg;
+
+
         uint32_t mSeqNum{};
         std::optional<size_t> mPrevDetectedCount; // Log spam prevention
+
+        //Raw Tag Data from CV::ARUCO
         std::vector<std::vector<cv::Point2f>> mImmediateCorners;
         std::vector<int> mImmediateIds;
-        std::unordered_map<int, Tag> mTags;
+
+        //Map from tagID to Tag
+        std::unordered_map<int, LongRangeTag> mTags;
         dynamic_reconfigure::Server<mrover::DetectorParamsConfig> mConfigServer;
         dynamic_reconfigure::Server<mrover::DetectorParamsConfig>::CallbackType mCallbackType;
 
@@ -40,6 +60,46 @@ namespace mrover {
 
         void publishThresholdedImage();
 
-        std::optional<SE3> getTagInCamFromPixel(sensor_msgs::PointCloud2ConstPtr const& cloudPtr, size_t u, size_t v);       
-    }
-}
+        std::optional<SE3> getTagInCamFromPixel(sensor_msgs::PointCloud2ConstPtr const& cloudPtr, size_t u, size_t v);
+
+
+        /**
+        * Detects tags in an image, draws the detected markers onto the image, and publishes them to /long_range_tag_detection
+        * 1. Updates mImg to store the underlying image matrix
+        * @param msg   image message
+        */
+        void imageCallback(sensor_msgs::Image const& msg);
+
+        /**
+        * Stores msg to mImgMsg
+        * Creates image matrix from message, stores to mImg
+        *
+        * @param msg Current Image Message
+        */
+        void updateImageMatrices(sensor_msgs::Image const& msg);
+
+        /**
+        * Runs the cv::aruco tag detection on the cv::mat 
+        * Modifies mImmedateCorners and mImmediateIds
+        * Uses the mDetectorParams and mDictionary member variables
+        */
+        void runTagDetection();
+
+        /**
+        * For all the tags stored in mImmediateIds and mImmediateCorners:
+        * Check to see if they already exist in mTags
+        * If they do, increment their 
+        */
+        void updateHitCounts();
+
+        LongRangeTag createLrt(int tagId, std::vector<cv::Point2f>& tagCorners);
+
+        /**
+        *  Assumes that mImg exists
+        *  @return a cv::Point2f that contains the centerx and centery
+        * centerx and centery are -0.5, 0.5, and reflect a normalized offset from the image center
+        * Average all x values
+        */
+        cv::Point2f getImageCenter(std::vector<cv::Point2f>& tagCorners);
+    };
+}; // namespace mrover
--- src/perception/tag_detector/long_range_tag_detector.processing.cpp
+++ src/perception/tag_detector/long_range_tag_detector.processing.cpp
@@ -1,6 +1,7 @@
 #include "long_range_tag_detector.hpp"
 
 #include "../point.hpp"
+#include <opencv2/core/types.hpp>
 
 namespace mrover {
 
@@ -9,13 +10,95 @@ namespace mrover {
      *
      * @param msg   image message
      */
-    void TagDetectorNodelet::imageCallback(sensor_msgs::Image const& msg) {
+    void LongRangeTagDetectorNodelet::imageCallback(sensor_msgs::Image const& msg) {
+        if (!mEnableDetections) return;
+
+        //Store image contents to member variables
+        updateImageMatrices(msg);
+
         // 1. Detect tags
+        runTagDetection();
+
         // 2. Update the hit counts of the tags in the mTags map
+        updateHitCounts();
+
         // 3. We only want to publish the tags if the topic has subscribers
         if (mPublishImages && mImgPub.getNumSubscribers()) {
             // Draw the tags on the image using OpenCV
+            // LongRangeTagDetectorNodelet::publishDrawnImages()
         }
+
+        //PUBLISH TAGS
+    }
+
+    //HELPER FUNCTIONS
+
+    void LongRangeTagDetectorNodelet::updateImageMatrices(sensor_msgs::Image const& msg) {
+        //Store image message to mImgMsg member variable
+        mImgMsg = msg;
+
+        cv::Mat cvImage{static_cast<int>(msg.height), static_cast<int>(msg.width), CV_8UC3, const_cast<uint8_t*>(msg.data.data())};
+
+        //Store to mImg member variable - unsure if necessary but need it to persist
+        cvImage.copyTo(mImg);
+
+        // TODO: Set the grayImage if neccesary
+    }
+
+    void LongRangeTagDetectorNodelet::runTagDetection() {
+        cv::aruco::detectMarkers(mImg, mDictionary, mImmediateCorners, mImmediateIds, mDetectorParams);
+    }
+
+    void LongRangeTagDetectorNodelet::updateHitCounts() {
+        //loop through all identified IDs
+        for (size_t i = 0; i < mImmediateIds.size(); i++) {
+            int currentId = mImmediateIds[i];
+
+            //Create new struct for each tag
+            LongRangeTag lrt = createLrt(currentId, mImmediateCorners[i]);
+
+            //Check if the tag was already detected and update hitCount to reflect
+            if (mTags.contains(currentId)) {
+                //Key exist sin mTags
+                lrt.hitCount = std::min(mTags[currentId].hitCount + mTagIncrementWeight, mMaxHitCount);
+            }
+
+            mTags[currentId] = lrt;
+        }
+
+        //Now decrement all the hitcounts for tags that were not updated
+        // Set updated status to false
+
+        for (auto& mTag: mTags) {
+            LongRangeTag& curtag = mTag.second;
+
+            if (curtag.updated) {
+                curtag.updated = false;
+            } else {
+                //Decrement weight of undetected tags
+                curtag.hitCount -= mTagDecrementWeight;
+
+                //if the value has fallen belown the minimum, remove it
+                if (curtag.hitCount <= mTagRemoveWeight) {
+                    mTags.erase(mTag.first);
+                }
+            }
+        }
+
+
+        //decrement non updated & set updated status to false
+    }
+
+    LongRangeTag LongRangeTagDetectorNodelet::createLrt(int tagId, std::vector<cv::Point2f>& tagCorners) {
+        LongRangeTag lrt;
+
+        lrt.hitCount = mBaseHitCount; //Start at base hit count value
+        lrt.id = tagId;
+        lrt.updated = true;
+
+        lrt.imageCenter = getImageCenter(tagCorners);
+
+        return lrt;
     }
 
 } // namespace mrover
