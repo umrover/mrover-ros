bb188be06e7a136198b5914c9b767311f992c54f
qhdwight <qhdwight@gmail.com>
2023-09-29 19:10:41 -0400

    formatting

--- src/teleoperation/streaming/stream_client.c
+++ src/teleoperation/streaming/stream_client.c
@@ -3,22 +3,22 @@
 #include <emscripten/websocket.h>
 #include <libde265/de265.h>
 
-EM_BOOL on_open(int event_type, const EmscriptenWebSocketOpenEvent* websocket_event, void* userData) {
+EM_BOOL on_open(int event_type, const EmscriptenWebSocketOpenEvent* websocket_event, void* user_data) {
     puts("on_open");
 
     return EM_TRUE;
 }
-EM_BOOL on_error(int event_type, const EmscriptenWebSocketErrorEvent* websocket_event, void* userData) {
+EM_BOOL on_error(int event_type, const EmscriptenWebSocketErrorEvent* websocket_event, void* user_data) {
     puts("on_error");
 
     return EM_TRUE;
 }
-EM_BOOL on_close(int event_type, const EmscriptenWebSocketCloseEvent* websocket_event, void* userData) {
+EM_BOOL on_close(int event_type, const EmscriptenWebSocketCloseEvent* websocket_event, void* user_data) {
     puts("on_close");
 
     return EM_TRUE;
 }
-EM_BOOL on_message(int event_type, const EmscriptenWebSocketMessageEvent* websocket_event, void* userData) {
+EM_BOOL on_message(int event_type, const EmscriptenWebSocketMessageEvent* websocket_event, void* user_data) {
     puts("on_message");
 
     return EM_TRUE;
@@ -35,17 +35,14 @@ int main() {
     EMSCRIPTEN_WEBSOCKET_T socket = emscripten_websocket_new(&create_socket_attributes);
     if (socket <= 0) return EXIT_FAILURE;
 
-    EMSCRIPTEN_RESULT result = {0};
 
+    EMSCRIPTEN_RESULT result;
     result = emscripten_websocket_set_onopen_callback(socket, NULL, on_open);
     if (result != EMSCRIPTEN_RESULT_SUCCESS) return EXIT_FAILURE;
-
     result = emscripten_websocket_set_onerror_callback(socket, NULL, on_error);
     if (result != EMSCRIPTEN_RESULT_SUCCESS) return EXIT_FAILURE;
-
     result = emscripten_websocket_set_onclose_callback(socket, NULL, on_close);
     if (result != EMSCRIPTEN_RESULT_SUCCESS) return EXIT_FAILURE;
-
     result = emscripten_websocket_set_onmessage_callback(socket, NULL, on_message);
     if (result != EMSCRIPTEN_RESULT_SUCCESS) return EXIT_FAILURE;
 
