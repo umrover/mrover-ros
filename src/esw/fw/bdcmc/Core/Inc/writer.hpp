13a10ce2abf37498b14c9b3126a77b35bbcdc592
jaccob <jaccob@umich.edu>
2023-09-28 20:08:10 -0400

    BrushedMotorWriter write_output
    fleshed out write_output
    added set_pwm
    added set_direction_pin

--- src/esw/fw/bdcmc/Core/Inc/writer.hpp
+++ src/esw/fw/bdcmc/Core/Inc/writer.hpp
@@ -1,15 +1,20 @@
 #pragma once
 
 #include <units.hpp>
+#include <config.hpp>
 
 namespace mrover {
 
     struct BrushedMotorWriter {
     public:
-        void write_output(Volts output);
+        void write_output(const Config& config, Volts output);
     private:
         void init();
         bool initialized{false};
+        bool forward_pin;
+        bool backward_pin;
+        inline void set_direction_pins(double duty_cycle);
+        inline void set_pwm(double duty_cycle);
     };
 
 } // namespace mrover
--- src/esw/fw/bdcmc/Core/Src/writers/brushedmotor.cpp
+++ src/esw/fw/bdcmc/Core/Src/writers/brushedmotor.cpp
@@ -14,11 +14,28 @@ namespace mrover {
 
     }
 
-    void BrushedMotorWriter::write_output(Volts output) {
+    void BrushedMotorWriter::write_output(const Config& config, Volts output) {
         if (!this->initialized) this->init();
 
-        // TODO write PWM, etc.
-        (void) output;
+        double duty_cycle = (output / config.getMaxVoltage()).get();
+        this->set_direction_pins(duty_cycle);
+        this->set_pwm(duty_cycle);
+    }
+
+    void BrushedMotorWriter::set_direction_pins(double duty_cycle) {
+        this->forward_pin = duty_cycle > 0 ? 1 : 0;
+        this->backward_pin = duty_cycle < 0 ? 1 : 0;
+    }
+
+    void BrushedMotorWrier::set_pwm(double duty_cycle) {
+        // obtain magnitude of duty_cycle and normalize
+        duty_cycle *= duty_cycle > 0 ? 1 : -1;
+        duty_cycle = duty_cycle > 1 ? 1 : duty_cycle;
+
+        // set CCR register
+        // The ccr register compares its value to the timer and outputs a signal based on the result
+        // The arr register sets the limit for when the timer register resets to 0. 
+        *(this->ccr) = duty_cycle * (*(this->arr) + 1);
     }
 
 }
