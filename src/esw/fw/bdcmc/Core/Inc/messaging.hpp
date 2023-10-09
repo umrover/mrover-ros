b795e9e0b471324dc30d8a5f55a2102d7f2852e5
qhdwight <qhdwight@gmail.com>
2023-10-09 13:01:17 -0400

    Update messaging, remove build folder, add renode files (TEMP, NOT FULLY WORKING)

--- src/esw/fw/bdcmc/Core/Inc/messaging.hpp
+++ src/esw/fw/bdcmc/Core/Inc/messaging.hpp
@@ -11,44 +11,38 @@ namespace mrover {
     // CAN FD supports up to 64 bytes per frame
     constexpr size_t FRAME_SIZE = 64;
 
-#define PACKED __attribute__((packed))
-
-    using motor_id_t = uint8_t;
-
     struct BaseCommand {
-        motor_id_t id;
-    } PACKED;
+    };
 
     struct ConfigCommand : BaseCommand {
         Volts maxVolts;
-    } PACKED;
+    };
 
     struct IdleCommand : BaseCommand {
-    } PACKED;
+    };
 
     struct ThrottleCommand : BaseCommand {
         dimensionless_t throttle;
-    } PACKED;
+    };
 
     struct VelocityCommand : BaseCommand {
         RadiansPerSecond velocity;
-    } PACKED;
+    };
 
     struct PositionCommand : BaseCommand {
         Radians position;
-    } PACKED;
+    };
 
     struct StatusState {
-
-    } PACKED;
+    };
 
     using Message = std::variant<
             IdleCommand, ThrottleCommand, VelocityCommand, PositionCommand>;
-    static_assert(sizeof(Message) <= FRAME_SIZE);
 
     union FdCanFrame {
         Message message;
         std::array<std::byte, FRAME_SIZE> bytes;
     };
+    static_assert(sizeof(FdCanFrame) == FRAME_SIZE);
 
 } // namespace mrover
