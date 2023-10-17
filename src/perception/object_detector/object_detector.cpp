20c3693986443bad4fb8e1f6ba1d4e3947f43a39
jbrhm <jrabraham1904@gmail.com>
2023-10-17 14:23:28 -0400

    Can detect water bottle

--- src/perception/object_detector/object_detector.cpp
+++ src/perception/object_detector/object_detector.cpp
@@ -1,6 +1,9 @@
 #include "object_detector.hpp"
 #include "inference.h"
+#include <mrover/DetectedObject.h>
 #include <opencv2/core/types.hpp>
+#include <sensor_msgs/Image.h>
+#include <string>
 
 namespace mrover {
 
@@ -11,10 +14,32 @@ namespace mrover {
         //Inference
 
         inference = Inference("//home//jabra//Desktop//Rover//yolov8s.onnx", cv::Size(640, 640), "", false);
-
         //read ONNX file into this mNet, YOLOV8, second smallest one
         // Note(quintin): I downloaded this pt (PyTorch) model file from: https://github.com/ultralytics/assets/releases
         // TODO(percep/obj-detectr): make this configurable
+
+        cv::Mat rawImg = cv::imread("//home//jabra//Downloads//Water.jpg");
+        cv::Mat sizedImg;
+        cv::resize(rawImg, sizedImg, cv::Size(640, 640));
+        //cv::Mat imageView{static_cast<int>(msg->width), static_cast<int>(msg->height), CV_8UC3, const_cast<uint8_t*>(msg->data.data())};
+
+        std::vector<Detection> detections = inference.runInference(sizedImg);
+
+        Detection firstDetection = detections[0];
+
+        float classConfidence = 0.0;
+        cv::Rect box = firstDetection.box;
+
+
+        DetectedObject msgData;
+        msgData.object_type = firstDetection.className;
+        msgData.detection_confidence = classConfidence;
+        msgData.xBoxPixel = (float) box.x;
+        msgData.yBoxPixel = (float) box.y;
+        msgData.width = (float) box.width;
+        msgData.height = (float) box.height;
+        msgData.heading = 0;
+        ROS_INFO(firstDetection.className.c_str());
 
         mImgSub = mNh.subscribe("image", 1, &ObjectDetectorNodelet::imageCallback, this);
     }
@@ -33,5 +58,7 @@ int main(int argc, char** argv) {
     return EXIT_SUCCESS;
 }
 
+#ifdef MROVER_IS_NODELET
 #include <pluginlib/class_list_macros.h>
 PLUGINLIB_EXPORT_CLASS(mrover::ObjectDetectorNodelet, nodelet::Nodelet)
+#endif
