52371bda137a22f72d645cf95fd2337658e7a4ae
Eric <eteng2012@gmail.com>
2023-10-22 16:33:39 +0000

    menu

--- src/teleoperation/frontend/src/components/Menu.vue
+++ src/teleoperation/frontend/src/components/Menu.vue
@@ -1,81 +1,176 @@
 <template>
-  <div class="wrapper">
-    <div class="shadow p-3 mb-5 header">
-      <h1>Menu</h1>
-      <img
-        src="../static/mrover.png"
-        alt="MRover"
-        title="MRover"
-        width="200"
-      />
+  <div class="wrap">
+    <div v-if="vertical">
+      <div>
+        <h3>Motor Data</h3>
+      </div>
+      <table class="tableFormat" style="undefined;table-layout; fixed; width: 745px">
+        <colgroup>
+          <col style="width: 85px" />
+          <col style="width: 60px" />
+          <col style="width: 75px" />
+          <col style="width: 85px" />
+          <col style="width: 60px" />
+          <col style="width: 75px" />
+          <col style="width: 85px" />
+          <col style="width: 60px" />
+          <col style="width: 75px" />
+          <col style="width: 85px" />
+        </colgroup>
+
+        <thead>
+          <tr>
+            <th class="tableElement">Motor</th>
+            <th class="tableElement">Position (m)</th>
+            <th class="tableElement">Velocity (m/s)</th>
+            <th class="tableElement">Effort (Nm)</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr v-for="(joint, index) in jointStateData.name" : key="index">
+            <td class="tableElement"> {{ joint }}</td>
+            <td class="tableElement">
+              {{ (jointStateData.velocity[index] * radius_m).toFixed(3) }}
+            </td>
+            <td class="tableElement">
+              {{ jointStateData.effort[index].toFixed(3) }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
 
-    <div class="pages">
-      <fieldset class="row">
-        <legend>Tasks</legend>
-        <MenuButton link="#/EDMTask" name="EDM Mission" />
-        <MenuButton link="#/ESTask" name="ES Mission" />
-        <MenuButton link="#/ISHTask" name="ISH GUI" />
-        <MenuButton link="#/SATask" name="Sample Acquisition GUI" />
-        <MenuButton link="#/AutonTask" name="Autonomy Mission" />
-        <MenuButton link="#/Control" name="Temp Controls GUI" />
-        <MenuButton link="#/ROSDebug" name="ROS Debug Tools" />
-      </fieldset>
+    <div v-else>
+      <div>
+        <h3>Motor Data</h3>
+      </div>
+      <table class="tableFormat" style="undefined;table-layout: fixed; width: 745px">
+        <colgroup>
+          <col style="width: 85px" />
+          <col style="width: 60px" />
+          <col style="width: 75px" />
+          <col style="width: 85px" />
+          <col style="width: 60px" />
+          <col style="width: 75px" />
+          <col style="width: 85px" />
+          <col style="width: 60px" />
+          <col style="width: 75px" />
+          <col style="width: 85px" />
+        </colgroup>
+
+        <thead>
+          <tr>
+            <th class="tableElement">Motor</th>
+            <th v-for="name in jointStateData.name" class="tableElement">
+              {{ name.toFixed(3) }}
+            </th>
+          </tr>
+          <tr>
+            <th class="tableElement">Position (m)</th>
+            <td v-for="position in jointStateData.position" class="tableElement">
+              {{ (position * radius_m).toFixed(3) }}
+            </td>
+          </tr>
+          <tr>
+            <th class="tableElement">Velocity (m/s)</th>
+            <td v-for="velocity in jointStateData.position" class="tableElement">
+              {{ (velocity * radius_m).toFixed(3) }}
+            </td>
+          </tr>
+          <tr>
+            <th class="tableElement">Effort (Nm)</th>
+            <td v-for="effort in jointStateData.effort" class="tableElement">
+              {{ effort.toFixed(3) }}
+            </td>
+          </tr>
+        </thead>
+      </table>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import MenuButton from "./MenuButton.vue";
+import ROSLIB from "roslib"
 
-import { defineComponent } from 'vue'
+export default {
+  props: {
+    jointStateData: {
+      type: Object,
+      required: true,
+    },
 
-export default defineComponent({
-  name: "MainMenu",
-  components: {
-    MenuButton,
+    vertical: {
+      type: Boolean,
+      required: true,
+    },
   },
 
   data() {
-    return {};
-  }
+    return {
+      motors: [],
 
-});
+      radium_m: 0
+    };
+  },
+
+  created: function () {
+    let radius_param = new ROSLIB.Param({
+      ros: this.$ros,
+      name: "wheel/radius"
+    });
+
+    radius_param.get((radius_m) => {
+      this.radius_m = radius_m;
+    })
+  }
+};
+
 </script>
 
 <style scoped>
-
-.header {
-  grid-area: header;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+.wrap {
+  display: inline-block;
+  align-content: center;
 }
 
-.header h1 {
-  position: absolute;
-  left: 25px;
-  top: 10px;
+.box {
+  border-radius: 5px;
+  padding: 10px;
+  border: 1px solid black;
+  text-align: right;
+  vertical-align: top;
 }
 
-.pages {
-  grid-area: pages;
+.tableFormat {
+  border-collapse: collapse;
+  border-spacing: 0;
 }
 
-.row {
-  display: flex;
-  justify-content: center;
+.tableFormat td {
+  border-color: black;
+  border-style: solid;
+  border-width: 1px;
+  font-size: 13px;
+  overflow: hidden;
+  padding: 10px 5px;
+  word-break: normal;
+}
+
+.tableFormat th {
+  border-color: black;
+  border-style: solid;
+  border-width: 1px;
+  font-size: 13px;
+  font-weight: normal;
+  overflow: hidden;
+  padding: 10px 5px;
+  word-break: normal;
+}
+
+.tableFormat .tableElement {
+  border-color: inherit;
   text-align: center;
-  margin: 5px;
-}
-
-.wrapper {
-  display: grid;
-  grid-gap: 10px;
-  grid-template-columns: 1fr;
-  grid-template-rows: 125px 1fr;
-  grid-template-areas: "header" "pages";
-  font-family: sans-serif;
-  height: auto;
+  vertical-align: top;
 }
 </style>
