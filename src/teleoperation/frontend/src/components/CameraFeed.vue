73d8f4a9bf73b1999490b678a101717f6fd12358
jnnanni <jnnanni@umich.edu>
2023-10-24 20:16:00 -0400

    Camera feed file

---
+++ src/teleoperation/frontend/src/components/CameraFeed.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		<div>Feed</div>
+    <h1>H.265 Stream</h1>
+    <canvas id="canvas"></canvas>
+</div>
+</template>
+<script src="../../streaming/embuild/stream_client.js"></script>
+
+<script lang="ts">
+</script>
