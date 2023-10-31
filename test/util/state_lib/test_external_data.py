263466d467bd4ac78fec1d9f971037997b41f600
Ankith Udupa <ankith.udupa@gmail.com>
2023-10-31 17:32:17 -0400

    State library (#559)
    * state library and tests
    
    * working simple test
    
    * fixed external data test, added comment
    
    * added messagse for and code for state ros message publishing server
    
    * basically fully integrated with current nav system, tested on a few scenarios in sim
    
    * address comments: fix and integrate publisher + logging levels
    
    * style
    
    * style w correct black version
    
    * fix mypy issues and add stop funcitonality
    
    * Cleanup imports, improve some type hinting for member vars, fix wrong call to context
    
    * address comments
    
    * fix missing config
    
    * fix style
    
    * fix import issue
    
    ---------
    
    Co-authored-by: qhdwight <qhdwight@gmail.com>

---
+++ test/util/state_lib/test_external_data.py
@@ -0,0 +1,82 @@
+from util.state_lib.state_machine import StateMachine
+from util.state_lib.state import State, ExitState
+import random
+from threading import Thread, Lock
+import time
+
+"""
+Test multi-threaded program that has an external thread that feeds a resource that the
+Context object queries. Not a unit-test but run manually to ensure relatively predicatble
+behavior
+"""
+
+
+class Context:
+    def __init__(self):
+        self.stateCapture = ExternalStateCapture()
+
+    def getTrigger(self):
+        self.stateCapture.triggerLock.acquire()
+        trigger = self.stateCapture.trigger
+        self.stateCapture.triggerLock.release()
+        return trigger
+
+
+class ExternalStateCapture:
+    def random_loop(self):
+        # run forever and with probability 0.5 flip the trigger
+        while True:
+            if random.random() < 0.5:
+                self.triggerLock.acquire()
+                self.trigger = not self.trigger
+                self.triggerLock.release()
+                time.sleep(1)
+
+    def __init__(self):
+        self.triggerLock = Lock()
+        self.trigger = False
+
+
+class WaitingState(State):
+    def __init__(self):
+        super().__init__()
+
+    def on_enter(self, context):
+        print("Waiting for trigger")
+
+    def on_loop(self, context):
+        if context.getTrigger():
+            return RunningState()
+        return self
+
+    def on_exit(self, context):
+        print("Triggered!")
+
+
+class RunningState(State):
+    def __init__(self):
+        super().__init__()
+
+    def on_enter(self, context):
+        print("Running")
+
+    def on_loop(self, context):
+        if not context.getTrigger():
+            return WaitingState()
+        return self
+
+    def on_exit(self, context):
+        print("Stopped")
+
+
+if __name__ == "__main__":
+    sm = StateMachine(WaitingState(), "RandomForeverStateMachine")
+    sm.add_transition(WaitingState(), RunningState())
+    sm.add_transition(RunningState(), WaitingState())
+    sm.add_transition(RunningState(), RunningState())
+    sm.add_transition(WaitingState(), WaitingState())
+    context = Context()
+    sm.set_context(context)
+    thread = Thread(target=context.stateCapture.random_loop)
+    thread.start()
+    sm.run()
