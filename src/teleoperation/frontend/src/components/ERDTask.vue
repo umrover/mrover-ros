7629e1b68a34b12c747dd9bb4e2e20a4bd24a0c0
jnnanni <jnnanni@umich.edu>
2023-10-31 20:24:01 -0400

    Add cameraFeed Component with basic styling

--- src/teleoperation/frontend/src/components/ERDTask.vue
+++ src/teleoperation/frontend/src/components/ERDTask.vue
@@ -2,6 +2,7 @@
     <PDBFuse/>
     <DriveMoteusStateTable :moteus-state-data="moteusState" />
     <ArmMoteusStateTable/>
+    <CameraFeed/>
 </template>
 
 <script lang="ts">
@@ -9,12 +10,14 @@ import { defineComponent } from 'vue'
 import PDBFuse from "./PDBFuse.vue";
 import DriveMoteusStateTable from "./DriveMoteusStateTable.vue";
 import ArmMoteusStateTable from "./ArmMoteusStateTable.vue";
+import CameraFeed from "./CameraFeed.vue";
 
 export default defineComponent({
   components : {
     PDBFuse,
     DriveMoteusStateTable,
-    ArmMoteusStateTable
+    ArmMoteusStateTable,
+    CameraFeed
   },
 
   data() {
