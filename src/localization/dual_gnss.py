4b84aed3532822bc2e9d697962c459f0f3e78b68
Pearl Lin <pearllin@umich.edu>
2023-11-02 23:04:37 +0000

    added get_heading_vector function

--- src/localization/dual_gnss.py
+++ src/localization/dual_gnss.py
@@ -3,23 +3,23 @@ import matplotlib.pyplot as plt
 import math
 
 
-def get_heading(antenna_point_A, antenna_point_B):
-    latitude_A = np.radians(antenna_point_A[0])
-    latitude_B = np.radians(antenna_point_B[0])
-    longitude_A = np.radians(antenna_point_A[1])
-    longitude_B = np.radians(antenna_point_B[1])
+# def get_heading(antenna_point_A, antenna_point_B):
+#     latitude_A = np.radians(antenna_point_A[0])
+#     latitude_B = np.radians(antenna_point_B[0])
+#     longitude_A = np.radians(antenna_point_A[1])
+#     longitude_B = np.radians(antenna_point_B[1])
 
-    x = np.cos(latitude_B) * np.sin(longitude_B - longitude_A)
-    y = np.cos(latitude_A) * np.sin(latitude_B) - np.sin(latitude_A) * np.cos(latitude_B) * np.cos(
-        longitude_B - longitude_A
-    )
+#     x = np.cos(latitude_B) * np.sin(longitude_B - longitude_A)
+#     y = np.cos(latitude_A) * np.sin(latitude_B) - np.sin(latitude_A) * np.cos(latitude_B) * np.cos(
+#         longitude_B - longitude_A
+#     )
 
-    bearing = np.arctan2(x, y)
-    return bearing
+#     bearing = np.arctan2(x, y)
+#     return bearing
 
 
-test_bearing = get_heading(np.array([39.099912, -94.581213]), np.array([38.627089, -90.200203]))
-print(np.degrees(test_bearing))
+# test_bearing = get_heading(np.array([39.099912, -94.581213]), np.array([38.627089, -90.200203]))
+# print(np.degrees(test_bearing))
 
 
 # reference point should be close, needs to be specified based on location
@@ -29,6 +29,20 @@ def spherical_to_cartesian(spherical_coord: np.ndarray, reference_coord: np.ndar
     y = r * (np.radians(spherical_coord[0]) - np.radians(reference_coord[0]))
     z = 0
     return np.array([x, y, z])
+
+def get_heading_vector_from_angle(heading_angle):
+    x = np.sin(heading_angle)
+    y = np.cos(heading_angle)
+    return np.array([x, y])
+
+def get_heading_vector(point_L: np.array, point_R: np.array):
+    vector_connecting = np.array([point_R[0] - point_L[0], point_R[1] - point_L[1], 0])
+    vector_perp = np.zeros_like(vector_connecting)
+    vector_perp[0] = -vector_connecting[1]
+    vector_perp[1] = vector_connecting[0]
+    print(vector_perp)
+    return vector_perp / np.linalg.norm(vector_perp)
+
 
 
 def plot_vectors(P_1, P_2):
@@ -80,3 +94,7 @@ def calculate_angle_from_true_north(P_1, P_2):
 
 
 # print(calculate_angle_from_true_north(P_1, P_2))
+# print(get_heading_vector_from_angle((3 * np.pi) / 2))
+# print(get_heading_vector_from_angle(np.pi / 4))
+
+print(get_heading_vector(np.array([0, 0]), np.array([3, 3])))
