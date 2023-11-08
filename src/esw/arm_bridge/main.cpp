280d23e812de84db442056053ff26380700165f5
tabiosg <gutstabios@gmail.com>
2023-11-08 14:25:18 -0800

    Add Arm laser callback

--- src/esw/arm_bridge/main.cpp
+++ src/esw/arm_bridge/main.cpp
@@ -1,15 +1,29 @@
-#include <ros/ros.h>
-
+#include "messaging.hpp"
 #include <motors_manager.hpp>
+#include <ros/ros.h>
+#include <std_srvs/SetBool.h>
+
+std::unique_ptr<CanDevice> armLaserCanDevice;
 
 std::vector<std::string> armNames{"joint_a", "joint_b", "joint_c", "joint_de_0", "joint_de_1", "finger", "gripper"};
+
+bool armLaserCallback(std_srvs::SetBool::Request& req, std_srvs::SetBool::Response& res) {
+    armLaserCanDevice->publish_message(InBoundPDLBMessage{ArmLaserCommand{.enable = req.data}});
+    res.success = true;
+    res.message = "DONE";
+    return true;
+}
 
 int main(int argc, char** argv) {
     // Initialize the ROS node
     ros::init(argc, argv, "arm_bridge");
     ros::NodeHandle nh;
 
+    armLaserCanDevice = std::make_unique<CanDevice>(nh, "jetson", "pdlb");
+
     [[maybe_unused]] auto armManager = std::make_unique<mrover::MotorsManager>(nh, "arm", armNames);
+
+    nh.advertiseService("enable_arm_laser", armLaserCallback);
 
     // Enter the ROS event loop
     ros::spin();
