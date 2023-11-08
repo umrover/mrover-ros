79c1797709be91454adfe839323928c2728a2a6b
tabiosg <gutstabios@gmail.com>
2023-11-08 12:15:27 -0800

    Remove UV Bulb and update PDB Bridge

--- src/esw/pdb_bridge/main.cpp
+++ src/esw/pdb_bridge/main.cpp
@@ -1,3 +1,4 @@
+#include "messaging.hpp"
 #include <mrover/CAN.h>
 #include <mrover/PDLB.h>
 #include <ros/ros.h>
@@ -20,9 +21,34 @@ int main(int argc, char** argv) {
     return 0;
 }
 
+void processMessage(PDBData const& message) {
+    mrover::PDLB pdlb_data;
+    pdlb_data.temperatures = {
+            message.temperature_24v,
+            message.temperature_12v_jetson,
+            message.temperature_12v_rest,
+            message.temperature_12v_buck,
+            message.temperature_5v,
+            message.temperature_3v3};
+    pdlb_data.currents = {
+            message.current_24v,
+            message.current_12v_jetson,
+            message.temperature_12v_rest,
+            message.current_12v_buck,
+            message.current_5v,
+            message.current_3v3};
+    PDBPublisher.publish(pdlb_data);
+}
+
 void processCANData(const mrover::CAN::ConstPtr& msg) {
-    mrover::PDLB PDBData;
-    PDBData.temperatures = {0, 0, 0, 0, 0}; // TODO
-    PDBData.currents = {0, 0, 0, 0, 0};     // TODO
-    PDBPublisher.publish(PDBData);
+
+    // TODO - there is a lot of code that needs to be fixed here
+    assert(msg->source == "pdlb");
+    assert(msg->destination == "jetson");
+
+
+    OutBoundPDLBMessage const& message = *reinterpret_cast<OutBoundPDLBMessage const*>(msg->data.data());
+
+    // This calls the correct process function based on the current value of the alternative
+    std::visit([&](auto const& messageAlternative) { processMessage(messageAlternative); }, message);
 }
--- src/esw/sa_bridge/main.cpp
+++ src/esw/sa_bridge/main.cpp
@@ -3,17 +3,7 @@
 #include <ros/ros.h>
 #include <std_srvs/SetBool.h>
 
-std::vector<std::string> SANames{"sa_x", "sa_y", "sa_z", "scoop", "drill"};
-std::unique_ptr<mrover::CanDevice> uv_bulb_can_manager;
-
-bool uvBulbCallback(std_srvs::SetBool::Request& req, std_srvs::SetBool::Response& res) {
-    // TODO - make sure send_data works
-    // TODO - this needs to be a variant
-    // uv_bulb_can_manager->send_data("pdlb", req.data);
-    res.success = true;
-    res.message = "DONE";
-    return true;
-}
+std::vector<std::string> SANames{"sa_x", "sa_y", "sa_z", "scoop", "sensor_actuator"};
 
 int main(int argc, char** argv) {
     // Initialize the ROS node
@@ -21,9 +11,7 @@ int main(int argc, char** argv) {
     ros::NodeHandle nh;
 
     // Load motor controllers configuration from the ROS parameter server
-    uv_bulb_can_manager = std::make_unique<mrover::CanDevice>(nh, "jetson", "uv_bulb");
     [[maybe_unused]] auto SAManager = std::make_unique<mrover::MotorsManager>(nh, "sa", SANames);
-    nh.advertiseService("sa_enable_uv_bulb", uvBulbCallback);
     // Enter the ROS event loop
     ros::spin();
 
