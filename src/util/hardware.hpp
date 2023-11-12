be5332a8ffeb083623c85be07ce39463c9f2c0a9
WinsonChen114 <14winsonchen@gmail.com>
2023-11-12 13:28:21 -0500

    Split hardware.hpp file actually this time

--- src/util/hardware.hpp
+++ src/util/hardware.hpp
@@ -48,35 +48,7 @@ namespace mrover {
         GPIO_TypeDef* m_port{};
         std::uint16_t m_pin{};
     };
-
-#ifdef ADC_ON
-
-    class ADCSensor {
-	public:
-		ADCSensor() = default;
-
-		ADCSensor(ADC_HandleTypeDef *hadc, uint8_t channels)
-			: m_hadc(hadc), m_channels(channels) {
-			m_values.resize(channels);
-		}
-
-		uint32_t get_raw_channel_value(uint8_t channel) {
-			return m_values.at(channel);
-		}
-
-		void update() {
-
-			HAL_ADC_Start_DMA(m_hadc, m_values.data(), m_channels);
-		}
-
-	private:
-		ADC_HandleTypeDef* m_hadc;
-		uint8_t m_channels;
-		std::vector<uint32_t> m_values;
-	};
-
-#endif
-
+    
     class LimitSwitch {
     public:
         LimitSwitch() = default;
