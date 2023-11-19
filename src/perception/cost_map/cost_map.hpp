2ff15052e82c42f21369c55c3c23188584cc1805
Mackenzie <murphymt@umich.edu>
2023-11-19 14:04:02 -0500

    changed costmap removed redundant code

--- src/perception/cost_map/cost_map.hpp
+++ src/perception/cost_map/cost_map.hpp
@@ -13,8 +13,9 @@ namespace mrover {
         bool publishCostMaps = false; // Do we publish our global cost maps?
         bool verbose = false;         // Do we print extra information for debugging?
         float resolution = 1;         // In m/cell
-        int map_width = 1;            // Number of cells wide
-        int map_height = 1;           // Number of cells high
+        int cell_width = 1;            // Number of cells wide
+        int cell_height = 1;           // Number of cells high
+        int global_dim = 15;
         const tf2_ros::Buffer tf_buffer = tf2_ros::Buffer();
 
         nav_msgs::OccupancyGrid globalGridMsg;
--- src/perception/cost_map/pch.hpp
+++ src/perception/cost_map/pch.hpp
@@ -3,3 +3,7 @@
 #include <ros/node_handle.h>
 #include <ros/publisher.h>
 #include <sensor_msgs/PointCloud2.h>
+#include <nodelet/loader.h>
+#include <nodelet/nodelet.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2_ros/buffer.h>
