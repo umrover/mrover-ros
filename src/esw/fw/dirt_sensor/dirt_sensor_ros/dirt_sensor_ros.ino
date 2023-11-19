80ac690365af5ae42578cefc352703f74506d0d1
Nate <nbaskins@umich.edu>
2023-11-19 14:19:27 -0500

    guthrie made me

--- src/esw/fw/dirt_sensor/dirt_sensor_ros/dirt_sensor_ros.ino
+++ src/esw/fw/dirt_sensor/dirt_sensor_ros/dirt_sensor_ros.ino
@@ -1,14 +1,14 @@
-#include "DFRobot_SHT20.h"
+#include <DFRobot_SHT20.h>
 #include <ros.h>
 #include <sensor_msgs/Temperature.h>
-#include <sensor_msgs/Humidity.h>
+#include <sensor_msgs/RelativeHumidity.h>
 
 ros::NodeHandle nh;
 
 sensor_msgs::Temperature temperature_data;
 ros::Publisher temperature_pub("sa_temp_data", &temperature_data);
 
-sensor_msgs::Humidity humidity_data;
+sensor_msgs::RelativeHumidity humidity_data;
 ros::Publisher humidity_pub("sa_humidity_data", &humidity_data);
 
 DFRobot_SHT20 sht20(&Wire, SHT20_I2C_ADDR);
@@ -29,10 +29,10 @@ void loop(){
   float humidity = sht20.readHumidity();
 
   temperature_data.temperature = temp;
-  temperature_pub.publish( &temperature_data );
+  temperature_pub.publish(&temperature_data);
 
-  humidity_data.humidity = humidity;
-  thumidity_pub.publish( &humidity_data );
+  humidity_data.relative_humidity = humidity;
+  humidity_pub.publish(&humidity_data);
 
   nh.spinOnce();
   delay(1000);
