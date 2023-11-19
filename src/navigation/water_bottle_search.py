0e6360df4e432b0bbeb2e2dc2e62ce27fc175fe7
Prabhleen Pawar <pawarp@umich.edu>
2023-11-19 17:25:55 +0000

    Added rostopic and started callback function

--- src/navigation/water_bottle_search.py
+++ src/navigation/water_bottle_search.py
@@ -4,8 +4,9 @@ from dataclasses import dataclass
 from typing import Optional
 
 import numpy as np
+import rospy
 from mrover.msg import GPSPointList
-
+from nav_msgs.msg import OccupancyGrid
 from util.ros_utils import get_rosparam
 from util.state_lib.state import State
 
@@ -15,24 +16,44 @@ from navigation.trajectory import Trajectory
 from navigation.search import SearchTrajectory
 
 # TODO: Right now this is a copy of search state, we will need to change this 
+# REFERENCE: https://docs.google.com/document/d/18GjDWxIu5f5-N5t5UgbrZGdEyaDj9ZMEUuXex8-NKrA/edit
 
 class WaterBottleSearchState(State):
-    traj: SearchTrajectory
+    #Spiral 
+    traj: SearchTrajectory 
+    #when we are moving along the spiral 
     prev_target: Optional[np.ndarray] = None
     is_recovering: bool = False
 
-    # TODO: add rosparam names to waterbottlesearch/... in the navigation.yaml file
-    STOP_THRESH = get_rosparam("search/stop_thresh", 0.2)
+    # TODO: add rosparam names to water_bottle_search/... in the navigation.yaml file
+    STOP_THRESH = get_rosparam("search/stop_thresh", 0.5)
     DRIVE_FWD_THRESH = get_rosparam("search/drive_fwd_thresh", 0.34)
     SPIRAL_COVERAGE_RADIUS = get_rosparam("search/coverage_radius", 10)
     SEGMENTS_PER_ROTATION = get_rosparam("search/segments_per_rotation", 8) # TODO: after testing, might need to change
     DISTANCE_BETWEEN_SPIRALS = get_rosparam("search/distance_between_spirals", 1.25) # TODO: after testing, might need to change
-
     
+    
+
+    # TODO: Data Structure to store the information given from the map (Look at navigation)
+
+    #2D list 
+    costMap = []
+
     # TODO: Make call_back function to push into data structure
 
+    def costmap_callback(self, msg:OccupancyGrid):
+        #update data structure
+        costMap = OccupancyGrid.data
+
+        #Call A-STAR 
+        pass
+
+    #TODO: A-STAR Algorithm: f(n) = g(n) + h(n)
+    #def a_star():
+    
 
     def on_enter(self, context) -> None:
+        self.listener = rospy.Subscriber("costmap", OccupancyGrid, self.costmap_callback)
         search_center = context.course.current_waypoint()
         if not self.is_recovering:
             self.traj = SearchTrajectory.spiral_traj(
