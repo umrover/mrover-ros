67b6a1f63dd43fa3f473e482037b13f824a94fc5
tabiosg <gutstabios@gmail.com>
2023-11-23 09:11:11 -0800

    Add code to change positions

--- scripts/test_joint_de.py
+++ scripts/test_joint_de.py
@@ -7,7 +7,14 @@ import math
 
 DEBUG_MODE_ONLY = True
 g_velocity_mode = True  # As opposed to looping position control mode
-pitch_roll_ofset = None
+pitch_roll_offset = None
+
+POSITION_FOR_VELOCITY_CONTROL = math.nan
+MAX_TORQUE = 0.3
+ROVER_NODE_TO_MOTEUS_WATCHDOG_TIMEOUT_S = 0.15
+
+g_prev_pitch, g_prev_roll = math.nan, math.nan
+g_prev_pitch_pos, g_prev_roll_pos = math.nan, math.nan
 
 TRANSFORMATION_MATRIX = np.array([[40, 40], [40, -40]])  # m1/m2 = TRANSFORMATION_MATRIX * pitch/roll
 INVERSE_TRANS_MATRIX = np.linalg.inv(TRANSFORMATION_MATRIX)  # pitch/roll = INVERSE_TRANS_MATRIX * m1/m2
@@ -15,8 +22,44 @@ INVERSE_TRANS_MATRIX = np.linalg.inv(TRANSFORMATION_MATRIX)  # pitch/roll = INVE
 controller_1 = moteus.Controller(id=0x20)
 controller_2 = moteus.Controller(id=0x21)
 
-g_controller_1_rev = 0
-g_controller_2_rev = 0
+g_controller_1_rev = None
+g_controller_2_rev = None
+
+if DEBUG_MODE_ONLY:
+    g_controller_1_rev = 0
+    g_controller_2_rev = 0
+
+MAX_REV_PER_SEC = 8 / 60  # 8 RPM to RPS
+
+current_pos_state = ""
+time_since_last_changed = time.time()
+
+positions_by_key = {
+    "i": (0.5, 0),  # pitch 90
+    "j": (0, -0.5),  # roll -90
+    "k": (-0.5, 0),  # pitch - 90
+    "l": (0, 0.5),  # roll 90
+}
+
+
+async def fix_controller_if_error_and_return_pos(controller, pos_val_to_update):
+    global g_controller_1_rev, g_controller_2_rev
+
+    def moteus_not_found(state):
+        return (
+            state is None
+            or not hasattr(state, "values")
+            or moteus.Register.FAULT not in state.values
+            or moteus.Register.MODE not in state.values
+        )
+
+    state = await controller.query()
+    controller_not_found = moteus_not_found(state)
+    if controller_not_found:
+        await controller.set_stop()
+        return None
+    else:
+        return state.values[moteus.Register.POSITION]
 
 
 def adjust_pitch_roll(believed_pitch, believed_roll):
@@ -24,19 +67,83 @@ def adjust_pitch_roll(believed_pitch, believed_roll):
     print(f"Adjusting pitch and roll to be {believed_pitch} and {believed_roll}")
 
     # TODO - get what the moteus thinks its revolution is
+    if g_controller_1_rev is None or g_controller_2_rev is None:
+        print("Failure to adjust because no current moteus data")
+        return
+
     moteus_believed_rev = np.array([g_controller_1_rev, g_controller_2_rev])
     moteus_believed_pitch_roll = np.dot(INVERSE_TRANS_MATRIX, moteus_believed_rev)
 
     user_believed_pitch_roll = np.array([believed_pitch, believed_roll])
 
-    pitch_roll_ofset = user_believed_pitch_roll - moteus_believed_pitch_roll
+    pitch_roll_offset = moteus_believed_pitch_roll - user_believed_pitch_roll
+
+
+async def move_position(pitch, roll):
+    global g_controller_1_rev, g_controller_2_rev
+    global g_prev_pitch_pos, g_prev_roll_pos
+
+    user_desired_pitch_roll = np.array([pitch, roll])
+    moteus_desired_pitch_roll = pitch_roll_offset + user_desired_pitch_roll
+
+    # Perform matrix multiplication
+    motor_position_vector = np.dot(TRANSFORMATION_MATRIX, moteus_desired_pitch_roll)
+
+    # Extract y1 and y2 from the result vector
+    m1pos, m2pos = motor_position_vector
+
+    if g_prev_pitch_pos != pitch or g_prev_roll_pos != roll:
+        print(f"pitch: {pitch}, roll: {roll}, m1pos: {m1pos}, m2pos: {m2pos}")
+        g_prev_pitch_pos = pitch
+        g_prev_roll_pos = roll
+
+    if DEBUG_MODE_ONLY:
+        return
+
+    await controller_1.set_position(
+        position=m1pos,
+        velocity=MAX_REV_PER_SEC / 4,
+        velocity_limit=MAX_REV_PER_SEC,
+        maximum_torque=MAX_TORQUE,
+        watchdog_timeout=ROVER_NODE_TO_MOTEUS_WATCHDOG_TIMEOUT_S,
+        query=True,
+    )
+    await controller_2.set_position(
+        position=m2pos,
+        velocity=MAX_REV_PER_SEC / 4,
+        velocity_limit=MAX_REV_PER_SEC,
+        maximum_torque=MAX_TORQUE,
+        watchdog_timeout=ROVER_NODE_TO_MOTEUS_WATCHDOG_TIMEOUT_S,
+        query=True,
+    )
+
+    return_pos_1 = await fix_controller_if_error_and_return_pos(controller_1)
+    if return_pos_1 is not None:
+        g_controller_1_rev = return_pos_1
+    return_pos_2 = await fix_controller_if_error_and_return_pos(controller_2)
+    if return_pos_2 is not None:
+        g_controller_2_rev = return_pos_2
+
+
+sequence_positions = ["i", "k", "j", "l"]
+
+
+async def run_position_state_machine():
+    global current_pos_state, time_since_last_changed
+    if time.time() - time_since_last_changed > 5:
+        current_index = sequence_positions.index(current_pos_state)
+        next_index = (current_index + 1) % len(sequence_positions)
+        next_pos_state = sequence_positions[next_index]
+        print(f"Changing position!")
+        current_pos_state = next_pos_state
+        time_since_last_changed = time.time()
+
+    pitch, roll = positions_by_key[current_pos_state]
+    await move_position(pitch, roll)
 
 
 def print_key(key):
     print(f"Key pressed: {key}")
-
-
-MAX_REV_PER_SEC = 8 / 60  # 8 RPM to RPS
 
 
 def transform_coordinates_and_clamp(pitch, roll):
@@ -59,16 +166,9 @@ def transform_coordinates_and_clamp(pitch, roll):
     return m1, m2
 
 
-POSITION_FOR_VELOCITY_CONTROL = math.nan
-MAX_TORQUE = 0.3
-ROVER_NODE_TO_MOTEUS_WATCHDOG_TIMEOUT_S = 0.15
-
-
-g_prev_pitch, g_prev_roll = math.nan, math.nan
-
-
 async def move_velocity(pitch, roll):
     global g_prev_pitch, g_prev_roll
+    global g_controller_1_rev, g_controller_2_rev
     m1rps, m2rps = transform_coordinates_and_clamp(pitch, roll)
     if g_prev_pitch != pitch or g_prev_roll != roll:
         print(f"pitch: {pitch}, roll: {roll}, m1rps: {m1rps}, m2rps: {m2rps}")
@@ -95,25 +195,6 @@ async def move_velocity(pitch, roll):
         query=True,
     )
 
-    async def fix_controller_if_error_and_return_pos(controller, pos_val_to_update):
-        global g_controller_1_rev, g_controller_2_rev
-
-        def moteus_not_found(state):
-            return (
-                state is None
-                or not hasattr(state, "values")
-                or moteus.Register.FAULT not in state.values
-                or moteus.Register.MODE not in state.values
-            )
-
-        state = await controller.query()
-        controller_not_found = moteus_not_found(state)
-        if controller_not_found:
-            await controller.set_stop()
-            return None
-        else:
-            return state.values[moteus.Register.POSITION]
-
     return_pos_1 = await fix_controller_if_error_and_return_pos(controller_1)
     if return_pos_1 is not None:
         g_controller_1_rev = return_pos_1
@@ -132,13 +213,6 @@ async def main():
         "d": (0, 1),
     }
 
-    adjust_by_key = {
-        "i": (0.5, 0),  # pitch 90
-        "j": (0, -0.5),  # roll -90
-        "k": (-0.5, 0),  # pitch - 90
-        "l": (0, 0.5),  # roll 90
-    }
-
     print("Controls are the following:")
     print("X to exit.")
     print("WASD to control in velocity control")
@@ -152,10 +226,12 @@ async def main():
             return
 
         current_adjust_state = {key: keyboard.is_pressed(key) for key in ["i", "j", "k", "l"]}
-        for key in adjust_by_key:
+        for key in positions_by_key:
             if current_adjust_state[key]:
-                believed_pitch = adjust_by_key[key][0]
-                believed_roll = adjust_by_key[key][1]
+                global current_pos_state
+                current_pos_state = key
+                believed_pitch = positions_by_key[key][0]
+                believed_roll = positions_by_key[key][1]
                 adjust_pitch_roll(believed_pitch, believed_roll)
 
                 g_velocity_mode = True
@@ -164,7 +240,7 @@ async def main():
 
         if g_velocity_mode:
             if keyboard.is_pressed("P"):
-                if pitch_roll_ofset == None:
+                if pitch_roll_offset is None:
                     print("Failure to go into Looping Position mode. Please calibrate with i/j/k/l first.")
                 else:
                     print("Entering the Looping Position Mode")
@@ -184,6 +260,7 @@ async def main():
                 print("Entering Velocity Mode")
                 g_velocity_mode = True
                 continue
+            await run_position_state_machine()
 
         # Add a delay to reduce CPU usage
         time.sleep(0.1)
