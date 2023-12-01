f3c96d98da25d785aa6aef04b40fe562e801a975
wongau <wongau@umich.edu>
2023-11-30 19:43:28 -0500

    planning

--- src/localization/tip_detection.py
+++ src/localization/tip_detection.py
@@ -23,41 +23,52 @@ class tip_detection:
     def __init__(self):
         rospy.Subscriber("imu/imu_only", Imu, self.detect_tip)
         #read the orientation data from the given Imu message, store that value in `self.pose`, then publish that pose to the TF tree.
+
     def imu_callback(self, msg: Imu):
         self.pose = SE3.from_pos_quat(self.pose.position, np.array([msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w]))
         self.pose.publish_to_tf_tree(self.tf_broadcaster, "map", "base_link")
         odometry_sub = message_filters.Subscriber("global_ekf/odometry", Odometry)
         
-
         #subscribe to IMU
+        
     def detect_tip(self, odometry):
-
-        # get rover position
-        cur_row = {}
-        cur_row["x"] = odometry.pose.pose.position.x
-        cur_row["y"] = odometry.pose.pose.position.y
-        cur_row["z"] = odometry.pose.pose.position.z
-
         # get rover orientation
 
-        cur_row["rot_x"] = odometry.pose.pose.orientation.x
-        cur_row["rot_y"] = odometry.pose.pose.orientation.y
-        cur_row["rot_z"] = odometry.pose.pose.orientation.z
-        cur_row["rot_w"] = odometry.pose.pose.orientation.w
+        x = odometry.pose.pose.orientation.x
+        y = odometry.pose.pose.orientation.y
+        z = odometry.pose.pose.orientation.z
+        w = odometry.pose.pose.orientation.w
 
         # get rover linear acceleration and velocity
         linear_velocity_norm = np.linalg.norm(
             np.array([odometry.twist.twist.linear.x, odometry.twist.twist.linear.y, odometry.twist.twist.linear.z])
         )
 
-        cur_row["linear_velocity"] = linear_velocity_norm  # type: ignore
-        cur_row["angular_velocity"] = odometry.twist.twist.angular.z
-        
-
+        linear_velocity = linear_velocity_norm 
+        angular_velocity = odometry.twist.twist.angular.z
 
 
         
         """"
+        check if rover is tipped over over a certain period of time
+        if there are a lot of fluctuations over an arbitrary time period,
+        then the rover is tipped
+
+        ex. if there are 50 over-tipped measurements over 10 secs, then tipped
+        made up numbers ^^
+        
+
+        
+        ways we can check the cases for tipping:
+        - measure magnitudes of angular velocity and see if there is one that is above a threshold
+        - velocity is = or near 0 for a set amount of time
+            - check orientation
+        
+
+
+
+
+
         quartonions:
         basically weird way to store the way the rotations are rotating
         qx = ax * sin(angle/2)
@@ -71,6 +82,4 @@ class tip_detection:
 
         when talking ab multiplying quartonions, it's just adding them
         one 90 deg rotate + one 90 deg rotate = 180 deg rotate
-
-        
         """"
