db998ea9fb4ba23e01bd0669b53dbf6dae5b6a7e
dllliu <dlliu@umich.edu>
2023-12-03 13:18:12 -0500

    tests for both gps distance

--- src/localization/Test-Subscriber.py
+++ src/localization/Test-Subscriber.py
@@ -5,6 +5,7 @@ import numpy as np
 
 # library for interacting with ROS and TF tree
 import rospy
+import csv
 import tf2_ros
 import math
 
@@ -12,17 +13,23 @@ import math
 from sensor_msgs.msg import NavSatFix, Imu
 
 # SE3 library for handling poses and TF tree
-from util.SE3 import SE3
-from util.SO3 import SO3
+# from util.SE3 import SE3
+# from util.SO3 import SO3
 from matplotlib import pyplot as plt
 
 
 lat_arr = []
 long_arr = []
 
+coord_arr_left_latitude = []
+coord_arr_left_longitude = []
+coord_arr_right_latitude = []
+coord_arr_right_longitude = []
+distances = []
+
 
 class Localization:
-    pose: SE3
+    # pose: SE3
 
     def __init__(self):
         # create subscribers for GPS data, linking them to our callback functions
@@ -30,6 +37,9 @@ class Localization:
         rospy.Subscriber("gps/fix/rover_gps_left", NavSatFix, self.gps_left_callback)
         rospy.Subscriber("gps/fix/rover_gps_right", NavSatFix, self.gps_right_callback)
 
+        # csvfile = open("/home/daniel/catkin_ws/src/mrover/src/localization/test_gps_sim.csv", "w")
+        # csvwriter = csv.writer(csvfile)
+        # csvwriter.writerow([9, 10])
         # create a transform broadcaster for publishing to the TF tree
         # self.tf_broadcaster = tf2_ros.TransformBroadcaster()
 
@@ -37,13 +47,16 @@ class Localization:
         # self.pose = SE3()
 
     def gps_left_callback(self, msg: NavSatFix):
-        print(msg)
-        lat_arr.append(msg.latitude)
-        long_arr.append(msg.longitude)
+        # print(msg)
+        coord_arr_left_latitude.append(msg.latitude)
+        coord_arr_left_longitude.append(msg.longitude)
+        # lat_arr.append(msg.latitude)
+        # long_arr.append(msg.longitude)
 
     def gps_right_callback(self, msg: NavSatFix):
-        print(msg)
-        pass
+        # print(msg)
+        coord_arr_right_latitude.append(msg.latitude)
+        coord_arr_right_longitude.append(msg.longitude)
 
 
 def main():
@@ -59,5 +72,15 @@ def main():
 
 if __name__ == "__main__":
     main()
-    plt.scatter(lat_arr, long_arr)
+
+    # array_left = np.array(coord_arr_left)
+    # array_right = np.array(coord_arr_right)
+    # for i in range(min(len(coord_arr_left_latitude), len(coord_arr_right_latitude))):
+    # plt.plot(coord_arr_left_latitude[i], coord_arr_left_longitude[i], color="red")
+    # plt.plot(coord_arr_right_latitude[i], coord_arr_right_longitude[i], color="blue")
+    # plt.show()
+    # print("here")
+    # distances[i] = math.dist([coord_arr_left_latitude])
+    plt.scatter(coord_arr_left_latitude, coord_arr_left_longitude, color="red")
+    plt.scatter(coord_arr_right_latitude, coord_arr_right_longitude, color="blue")
     plt.show()
