4602dabe4de56d8cfa28ca8c082130ddcaff5afe
Rahul <rdalvi2014@gmail.com>
2023-12-27 23:17:18 -0500

    update_gps draft

--- src/localization/invariant_ekf/invariant_ekf.cpp
+++ src/localization/invariant_ekf/invariant_ekf.cpp
@@ -2,6 +2,7 @@
 #include <Eigen/src/Core/Matrix.h>
 #include <manif/impl/se_2_3/SE_2_3.h>
 #include <manif/impl/se_2_3/SE_2_3Tangent.h>
+#include <manif/impl/so3/SO3.h>
 
 InvariantEKF::InvariantEKF(const SE_2_3d& x0, const Matrix9d& P0, const Matrix9d& Q, const Matrix3d& R_gps_default, const Matrix3d& R_accel_default, const Matrix3d& R_mag_default)
     : mX(x0), mP(P0), mQ(Q), mR_gps(R_gps_default), mR_accel(R_accel_default), mR_mag(R_mag_default) {}
@@ -30,8 +31,6 @@ void InvariantEKF::predict(SE_2_3Tangentd const& u, double dt) {
     manif::SE_2_3d::Jacobian J_x_u;
     manif::SE_2_3d::Jacobian J_u_x;
 
-
-    //include Jacobians?
     mX = mX.plus(u_est, J_x_x, J_x_u);
 
     //don't understand J_u_x
@@ -40,7 +39,41 @@ void InvariantEKF::predict(SE_2_3Tangentd const& u, double dt) {
     mP = F * mP * F.transpose() + mQ;
 }
 
-void InvariantEKF::update_gps(Eigen::Vector3d const& z, Matrix3d const& R_gps) {}
+void InvariantEKF::update_gps(Eigen::Vector3d const& z, Matrix3d const& R_gps) {
+    //Yk = h_x + noise
+    Vector3d Yk; //gps reading
+    Vector3d h_x = mX.translation(); //what is the jacobian of this
+
+    auto test = mX.rotation(); //rotation matrix
+    test = test.inverse(); //inverse of matrix
+
+    Eigen::Vector3d b(0, 0, 0);
+    b = b * test; //rotate vector by the inverse
+
+    manif::SE_2_3d::Jacobian J_h_x;
+
+    //this gives correct jacobian? does the velocity mess it up? rotation gets cancelled out so left with translation
+    //this seems like a stupid way to get the jacobian
+    Vector3d e = mX.act(b, J_h_x); 
+
+    auto H = J_h_x;
+
+    //innovation cov
+    auto S_n = (H * mP * H.transpose()) + R_gps;
+
+    //kalman gain
+    auto K_n = mP * H.transpose() * S_n.inverse();
+
+    //observed error
+    manif::SE_2_3Tangentd dx = K_n * z;
+
+    //state update
+    mX = mX.plus(dx);
+
+    //cov update
+    mP = mP - K_n * S_n * K_n.transpose();
+
+}
 
 void InvariantEKF::update_gps(Eigen::Vector3d const& z) {
     update_gps(z, mR_gps);
