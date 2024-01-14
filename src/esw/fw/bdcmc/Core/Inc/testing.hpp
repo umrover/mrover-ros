9713a9b9df8825649dca471389e58bdc589a6e3b
JWu0126 <jesswu0126@gmail.com>
2024-01-14 16:13:26 -0500

    Added tests with placeholder values. Check Syntax

--- src/esw/fw/bdcmc/Core/Inc/testing.hpp
+++ src/esw/fw/bdcmc/Core/Inc/testing.hpp
@@ -18,7 +18,26 @@ namespace mrover {
          * 6. PositionCommand (If PID Configured)
          * 7. IdleCommand
          */
+        ConfigCommand configCommand = {
+                1.0,
+                ConfigLimitSwitchInfo,
+                ConfigEncoderInfo,
+                1.0,
+                1.0,
+                1.0,
+                1.0,
+                1.0
+        };
+        tests.push_back({configCommand, 2});
 
+        EnableLimitSwitchesCommand limitSwitchesCommand = { true };
+        tests.push_back({limitSwitchesCommand, 2});
+
+        AdjustCommand adjustCommand = { 1.0 };
+        tests.push_back({adjustCommand, 2});
+
+        ThrottleCommand throttleCommand = { 0.2 };
+        tests.push_back({throttleCommand, 2});
 
         return tests;
     }
