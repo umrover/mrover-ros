9cfc1be0fef84cf9d4a268b42eb895095dc67bf0
Eric D'Urso <edurso862@gmail.com>
2024-01-14 14:23:17 -0500

    add bdcmc testing framework

---
+++ src/esw/fw/bdcmc/Core/Inc/testing.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace mrover {
+
+    auto get_test_msgs() -> std::vector<std::pair<InBoundMessage, uint32_t>> {
+        std::vector<std::pair<InBoundMessage, uint32_t>> tests;
+
+        // TODO add tests here
+
+        return tests;
+    }
+
+} // namespace mrover
