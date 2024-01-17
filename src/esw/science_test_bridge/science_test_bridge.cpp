30042033ee3a0d0398a07bdfdb56edf761f6b335
umroverPerception <umroverPerception@gmail.com>
2024-01-16 21:20:31 -0500

    Fix science test bugs

--- src/esw/science_test_bridge/science_test_bridge.cpp
+++ src/esw/science_test_bridge/science_test_bridge.cpp
@@ -1,35 +1,28 @@
-#include "ros/ros.h"
 #include "messaging.hpp"
-#include <mrover/HeaterData.h>
+#include "ros/ros.h"
 #include "std_srvs/SetBool.h"
+#include <mrover/HeaterData.h>
 
-int main(int argc, char **argv)
-{
+int main(int argc, char** argv) {
     ros::init(argc, argv, "science_test_bridge");
     ros::NodeHandle nh;
 
     ros::service::waitForService("/science_change_heater_auto_shutoff_state");
 
-    try
-    {
-        ros::ServiceClient service_client = nh.serviceClient("/science_change_heater_auto_shutoff_state"); 
+    try {
+        ros::ServiceClient service_client = nh.serviceClient<std_srvs::SetBool::Request, std_srvs::SetBool::Response>("/science_change_heater_auto_shutoff_state");
 
         std_srvs::SetBool::Request request;
         request.data = false;
 
 
-        if (std_srvs::SetBool::Request resp; service_client.call(request, resp))
-        {
-            ROS_INFO("Response: %s", response.message.c_str());
-        }
-        else
-        {
+        if (std_srvs::SetBool::Response resp; service_client.call(request, resp)) {
+            ROS_INFO("Response: %s", resp.message.c_str());
+        } else {
             ROS_ERROR("Failed to call service");
             return 1;
         }
-    }
-    catch (const std::exception &e)
-    {
+    } catch (std::exception const& e) {
         ROS_ERROR("Service call failed: %s", e.what());
         return 1;
     }
