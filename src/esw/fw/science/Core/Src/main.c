f424ac8ef2015b3b97a671ca5c40722ffcadf844
jmaff <jmaff2003@gmail.com>
2024-01-18 16:33:04 -0500

    Enable thermistor task

--- src/esw/fw/science/Core/Src/main.c
+++ src/esw/fw/science/Core/Src/main.c
@@ -191,7 +191,7 @@ int main(void)
   // TODO - Using spectral causes a hardfault!!!
 //  SpectralTaskHandle = osThreadNew(SpectralTask, NULL, &SpectralTask_attributes);
 //  SpectralPollingTaskHandle = osThreadNew(SpectralPollingTask, NULL, &SpectralPollingTask_attributes);
-//  ThermistorAndAutoShutoffTaskHandle = osThreadNew(ThermistorAndAutoShutoffTask, NULL, &ThermistorAndAutoShutoffTask_attributes);
+  ThermistorAndAutoShutoffTaskHandle = osThreadNew(ThermistorAndAutoShutoffTask, NULL, &ThermistorAndAutoShutoffTask_attributes);
   HeaterUpdatesTaskHandle = osThreadNew(HeaterUpdatesTask, NULL, &HeaterUpdatesTask_attributes);
 
   /* add threads, ... */
