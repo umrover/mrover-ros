df540ccfd4925243894e46d35b42989174fe04f2
jaccob <jaccob@umich.edu>
2024-01-18 19:57:42 -0500

    ioc update

--- src/esw/fw/bdcmc/Core/Src/main.c
+++ src/esw/fw/bdcmc/Core/Src/main.c
@@ -596,7 +596,7 @@ static void MX_TIM15_Init(void)
   htim15.Instance = TIM15;
   htim15.Init.Prescaler = 63;
   htim15.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim15.Init.Period = 100;
+  htim15.Init.Period = 99;
   htim15.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim15.Init.RepetitionCounter = 0;
   htim15.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
