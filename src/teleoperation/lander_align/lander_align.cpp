54a0b7040e959fdcd38ee99780d2690187e93285
Joshua Huang <joshhua@umich.edu>
2024-01-25 18:27:40 -0500

    Redefine filtering/ransac problem

--- src/teleoperation/lander_align/lander_align.cpp
+++ src/teleoperation/lander_align/lander_align.cpp
@@ -1,4 +1,6 @@
 #include "lander_align.hpp"
+#include <random>
+#include <vector>
 
 namespace mrover {
 
@@ -7,16 +9,29 @@ namespace mrover {
         mPnh = getMTPrivateNodeHandle();
     }
 
-    auto LanderAlignNodelet::downsample(sensor_msgs::PointCloud2Ptr const& cloud) -> sensor_msgs::PointCloud2Ptr {
+    // deprecated/not needed anymore
+    // auto LanderAlignNodelet::downsample(sensor_msgs::PointCloud2Ptr const& cloud) -> sensor_msgs::PointCloud2Ptr {
         // TODO: make a new point cloud with half the resolution, check the zed wrapper for how to make one
-    }
+        // sensor_msgs::PointCloud2 cloudSample;
+        // cloudSample.header = cloud->header
+    //     cloudSample.height = cloud->height / 2;
+    //     cloudSample.width = cloud->width / 2;
+    //     cloudSample.fields
+    // }
 
-    auto LanderAlignNodelet::filterNormals(sensor_msgs::PointCloud2Ptr const& cloud) -> void {
-        // TODO: modify "cloud" in-place, this should be input from the downsample function
+    auto LanderAlignNodelet::filterNormals(sensor_msgs::PointCloud2Ptr const& cloud, const int threshold) -> std::vector<Point*> {
+        // TODO: return a vector of Point pointers that correspond to all points in the point cloud that are not the ground
+        // filter based on whether z-normal valus exceed specified threshold
+
+        return {};
     }
 
     auto LanderAlignNodelet::ransac(sensor_msgs::PointCloud2Ptr const& cloud) -> Eigen::Vector3d {
         // TODO: use RANSAC to find the lander face, should be the closest, we may need to modify this to output more information, currently the output is the normal
+        auto* cloudData = reinterpret_cast<Point const*>(cloud->fields.data());
+
+        std::default_random_engine generator;
+        std::uniform_int_distribution<int> distribution (0, )
     }
 
 } // namespace mrover
--- src/teleoperation/lander_align/lander_align.hpp
+++ src/teleoperation/lander_align/lander_align.hpp
@@ -9,11 +9,12 @@ namespace mrover {
 
         auto onInit() -> void override;
 
-        auto downsample(sensor_msgs::PointCloud2Ptr const& cloud) -> sensor_msgs::PointCloud2Ptr;
+        // deprecated/not needed anymore
+        // auto downsample(sensor_msgs::PointCloud2Ptr const& cloud) -> sensor_msgs::PointCloud2Ptr;
 
-        auto filterNormals(sensor_msgs::PointCloud2Ptr const& cloud) -> void;
+        auto filterNormals(sensor_msgs::PointCloud2Ptr const& cloud, const int threshold) -> std::vector<Point*>;
 
-        auto ransac(sensor_msgs::PointCloud2Ptr const& cloud) -> Eigen::Vector3d;
+        auto ransac(const std::vector<Point*>& filteredPoints) -> Eigen::Vector3d;
     };
 
 } // namespace mrover
