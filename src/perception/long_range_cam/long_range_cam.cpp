b9d69574904cb1957bb343208993a5294e9826a9
Alison Ryckman <alison.ryckman@gmail.com>
2024-01-29 21:28:22 -0500

    remove mains from percep nodelets

--- src/perception/long_range_cam/long_range_cam.cpp
+++ src/perception/long_range_cam/long_range_cam.cpp
@@ -82,19 +82,6 @@ namespace mrover {
 
 } // namespace mrover
 
-int main(int argc, char** argv) {
-    ros::init(argc, argv, "usb_camera");
-
-    // Start the ZED Nodelet
-    nodelet::Loader nodelet;
-    std::cout << ros::this_node::getName() << std::endl;
-    nodelet.load(ros::this_node::getName(), "mrover/LongRangeCamNodelet", ros::names::getRemappings(), {});
-
-    ros::spin();
-
-    return EXIT_SUCCESS;
-}
-
 #ifdef MROVER_IS_NODELET
 #include <pluginlib/class_list_macros.h>
 PLUGINLIB_EXPORT_CLASS(mrover::LongRangeCamNodelet, nodelet::Nodelet)
--- src/perception/tag_detector/long_range_cam/long_range_tag_detector.cpp
+++ src/perception/tag_detector/long_range_cam/long_range_tag_detector.cpp
@@ -143,17 +143,5 @@ namespace mrover {
 
 } // namespace mrover
 
-int main(int argc, char** argv) {
-    ros::init(argc, argv, "long_range_tag_detector");
-
-    // Start the long range cam Nodelet
-    nodelet::Loader nodelet;
-    nodelet.load(ros::this_node::getName(), "mrover/LongRangeTagDetectorNodelet", ros::names::getRemappings(), {});
-
-    ros::spin();
-
-    return EXIT_SUCCESS;
-}
-
 #include <pluginlib/class_list_macros.h>
 PLUGINLIB_EXPORT_CLASS(mrover::LongRangeTagDetectorNodelet, nodelet::Nodelet)
