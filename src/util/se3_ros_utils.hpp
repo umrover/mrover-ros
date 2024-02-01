95e019852b4060a37e2f46d20c36b61f95ea58e4
Riley Bridges <rlybrdgs@gmail.com>
2024-01-31 23:11:15 -0500

    debugging

--- src/util/se3_ros_utils.hpp
+++ src/util/se3_ros_utils.hpp
@@ -16,18 +16,20 @@
 using manif::SE3d;
 
 
-SE3d SE3fromTf(geometry_msgs::Transform const& transform);
+namespace mrover {
+    SE3d SE3fromTf(geometry_msgs::Transform const& transform);
 
-SE3d SE3fromPose(geometry_msgs::Pose const& pose);
+    SE3d SE3fromPose(geometry_msgs::Pose const& pose);
 
-[[nodiscard]] geometry_msgs::Pose SE3toPose(SE3d const& tf);
+    [[nodiscard]] geometry_msgs::Pose SE3toPose(SE3d const& tf);
 
-[[nodiscard]] geometry_msgs::Transform SE3toTransform(SE3d const& tf);
+    [[nodiscard]] geometry_msgs::Transform SE3toTransform(SE3d const& tf);
 
-[[nodiscard]] geometry_msgs::PoseStamped SE3toPoseStamped(SE3d const& tf, std::string const& frameId);
+    [[nodiscard]] geometry_msgs::PoseStamped SE3toPoseStamped(SE3d const& tf, std::string const& frameId);
 
-[[nodiscard]] geometry_msgs::TransformStamped SE3toTransformStamped(SE3d const& tf, std::string const& parentFrameId, std::string const& childFrameId);
+    [[nodiscard]] geometry_msgs::TransformStamped SE3toTransformStamped(SE3d const& tf, std::string const& parentFrameId, std::string const& childFrameId);
 
-[[nodiscard]] SE3d SE3fromTfTree(tf2_ros::Buffer const& buffer, std::string const& fromFrameId, std::string const& toFrameId);
+    [[nodiscard]] SE3d SE3fromTfTree(tf2_ros::Buffer const& buffer, std::string const& fromFrameId, std::string const& toFrameId);
 
-void pushSE3ToTfTree(tf2_ros::TransformBroadcaster& broadcaster, std::string const& childFrameId, std::string const& parentFrameId, SE3d const& tf);
+    void pushSE3ToTfTree(tf2_ros::TransformBroadcaster& broadcaster, std::string const& childFrameId, std::string const& parentFrameId, SE3d const& tf);
+} // namespace mrover
