ab3dfb425c2ba0d0f87d761bb9a4854f5d46c4e1
RyanI70I <ryankers@umich.edu>
2024-02-01 19:36:44 -0500

    Use right pointcloud fields "rgb not x,y,z" Remove cvMat->imagemsg fnx Save response to desktop

--- src/perception/panorama.py
+++ src/perception/panorama.py
@@ -18,39 +18,38 @@ from sensor_msgs import point_cloud2
 
 def capture_panorama(request: CapturePanoramaRequest) -> CapturePanoramaResponse:
 
-    opencv_matrix = []
+    image_list = []
     def received_point_cloud(point: PointCloud2):
         rospy.loginfo("Received point cloud data")
         #Extract RGB field
-
         pc=ros_numpy.numpify(point)
         pc = ros_numpy.point_cloud2.split_rgb_field(pc)
         shape = pc.shape + (3, )
-        points = np.zeros(shape)
-        points[..., 0] = pc['x']
-        points[..., 1] = pc['y']
-        points[..., 2] = pc['z']
         rgb = np.zeros(shape)
         rgb[..., 0] = pc['r']
         rgb[..., 1] = pc['g']
         rgb[..., 2] = pc['b']
-        opencv_matrix.append(cv2.cvtColor((points * 255).astype(np.uint8), cv2.COLOR_RGB2BGR))
+        image_list.append(cv2.cvtColor((rgb * 255).astype(np.uint8), cv2.COLOR_RGB2BGR))
         rospy.loginfo("Converted and Saved Image")
-        rospy.loginfo("Total Images Saved" + str(len(opencv_matrix)))
+        rospy.loginfo("Total Images Saved" + str(len(image_list)))
 
     image_subscriber = rospy.Subscriber("/camera/left/points", PointCloud2,received_point_cloud, queue_size=1)
-
     #TODO, make motors spin around
-    if(len(opencv_matrix) < 2):
+    if(len(image_list) < 2):
         rospy.sleep(1)
-    rospy.loginfo("Creating Panorama..." + str(len(opencv_matrix)))
+
+    rospy.loginfo("Creating Panorama..." + str(len(image_list)))
     stitcher = cv2.Stitcher.create()
-    (code,outPic) = stitcher.stitch(opencv_matrix)
-    rospy.loginfo("Panorama Created" + str(len(opencv_matrix)))
-    bridge = CvBridge()
-    image_msg = bridge.cv2_to_imgmsg(outPic, encoding="bgr8")
-    rospy.loginfo("Sending Response" + str(len(opencv_matrix)))
-    return CapturePanoramaResponse(image_msg)
+    (code,outPic) = stitcher.stitch(image_list)
+    rospy.loginfo("Panorama Created" + str(len(image_list)))
+
+    desktop_path = '/Users/ryankersten/Desktop/'
+    cv2.imwrite(desktop_path, image_list[0])
+
+
+    rospy.loginfo("Sending Response" + str(len(image_list)))
+    return CapturePanoramaResponse(...)
+
 
 def main() -> int:
     rospy.init_node("panorama")
