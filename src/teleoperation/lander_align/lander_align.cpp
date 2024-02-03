275c4c16b6223a7da4f34d3afc4455f70495be60
Joshua Huang <joshhua@umich.edu>
2024-02-03 13:30:03 -0500

    Add retry zero-vector case

--- src/teleoperation/lander_align/lander_align.cpp
+++ src/teleoperation/lander_align/lander_align.cpp
@@ -39,46 +39,47 @@ namespace mrover {
     auto LanderAlignNodelet::ransac(const std::vector<Point*>& points, const float distanceThreshold, int minInliers, const int epochs) -> Eigen::Vector3f {
         // TODO: use RANSAC to find the lander face, should be the closest, we may need to modify this to output more information, currently the output is the normal
         // takes 3 samples for every epoch and terminates after specified number of epochs
-        Eigen::Vector3f bestPlane; // normal vector representing plane (initialize as zero vector?? default ctor??)
+        Eigen::Vector3f bestPlane(0, 0, 0); // normal vector representing plane (initialize as zero vector?? default ctor??)
 
         // define randomizer
         std::default_random_engine generator;
         std::uniform_int_distribution<int> distribution(0, (int) points.size() - 1);
 
-        for (int i = 0; i < epochs; ++i) {
-            // sample 3 random points (potential inliers)
-            Point* point1 = points[distribution(generator)];
-            Point* point2 = points[distribution(generator)];
-            Point* point3 = points[distribution(generator)];
+        do {
+            for (int i = 0; i < epochs; ++i) {
+                // sample 3 random points (potential inliers)
+                Point* point1 = points[distribution(generator)];
+                Point* point2 = points[distribution(generator)];
+                Point* point3 = points[distribution(generator)];
 
-            Eigen::Vector3f vec1{point1->x, point1->y, point1->z};
-            Eigen::Vector3f vec2{point2->x, point2->y, point2->z};
-            Eigen::Vector3f vec3{point3->x, point3->y, point3->z};
+                Eigen::Vector3f vec1{point1->x, point1->y, point1->z};
+                Eigen::Vector3f vec2{point2->x, point2->y, point2->z};
+                Eigen::Vector3f vec3{point3->x, point3->y, point3->z};
 
-            // fit a plane to these points
-            Eigen::Vector3f normal = (vec1 - vec2).cross(vec1 - vec3).normalized();
-            float offset = -normal.dot(vec1); // calculate offset (D value) using one of the points
+                // fit a plane to these points
+                Eigen::Vector3f normal = (vec1 - vec2).cross(vec1 - vec3).normalized();
+                float offset = -normal.dot(vec1); // calculate offset (D value) using one of the points
 
-            int numInliers = 0;
+                int numInliers = 0;
 
-            for (auto p : points) { 
-                // calculate distance of each point from potential plane
-                float distance = std::abs(normal.x() * p->x + normal.y() * p->y + normal.z() * p->z + offset);
+                for (auto p : points) { 
+                    // calculate distance of each point from potential plane
+                    float distance = std::abs(normal.x() * p->x + normal.y() * p->y + normal.z() * p->z + offset);
 
-                if (distance < distanceThreshold) {
-                    ++numInliers; // count num of inliers that pass the "good enough fit" threshold
+                    if (distance < distanceThreshold) {
+                        ++numInliers; // count num of inliers that pass the "good enough fit" threshold
+                    }
+                }
+
+                // update best plane if better inlier count
+                if (numInliers > minInliers) { 
+                    minInliers = numInliers;
+                    bestPlane = normal;
                 }
             }
+        } while (bestPlane.isZero()); // keep trying until we get valid result TODO break case after X attempts??
 
-            // update best plane if better inlier count
-            if (numInliers > minInliers) { 
-                minInliers = numInliers;
-                bestPlane = normal;
-            }
-        }
-
-        return bestPlane; // TODO address zero vector null case or bug
-
+        return bestPlane;
     }
 
 } // namespace mrover
