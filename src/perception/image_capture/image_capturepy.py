9f418653012d5076b35c253e682abf3a2bb8f8f4
John <jrabraham1904@gmail.com>
2024-02-05 20:52:42 -0500

    Cleaned up Image Capture

--- src/perception/image_capture/image_capturepy.py
+++ src/perception/image_capture/image_capturepy.py
@@ -2,13 +2,11 @@
 
 from pynput.keyboard import Key, Controller
 
-
 # python linear algebra library
 import numpy as np
 
 # library for interacting with ROS and TF tree
 import rospy
-import tf2_ros
 
 import cv2
 
@@ -18,42 +16,39 @@ import datetime
 # ROS message types we need to use
 from sensor_msgs.msg import Image
 
-# SE3 library for handling poses and TF tree
-from util.SE3 import SE3
-
-import math
-
 import threading
+
 
 class KeyboardThread(threading.Thread):
 
-    def __init__(self, input_cbk = None, name='keyboard-input-thread'):
+    def __init__(self, input_cbk=None, name="keyboard-input-thread"):
         self.input_cbk = input_cbk
         super(KeyboardThread, self).__init__(name=name)
         self.start()
         self.run = True
-        
 
     def run(self):
         while True:
-            self.input_cbk(input()) #waits to get input + Return
-            if(not self.run):
+            self.input_cbk(input())  # waits to get input + Return
+            if not self.run:
                 break
 
     def stop(self):
         self.run = False
 
-showcounter = 0 #something to demonstrate the change
+
+showcounter = 0  # something to demonstrate the change
+
 
 def my_callback(inp):
-    #evaluate the keyboard input
+    # evaluate the keyboard input
     image_capturepy.showcounter += 1
 
-#start the Keyboard thread
+
+# start the Keyboard thread
+
 
 class image_capturepy:
-    pose: SE3
-
     showcounter = 0
 
     def __init__(self):
@@ -67,17 +62,16 @@ class image_capturepy:
         self.keyboard.release(Key.enter)
 
     def img_callback(self, msg):
-        data = np.empty(msg.height * msg.width * 4, dtype = np.uint8)
+        data = np.empty(msg.height * msg.width * 4, dtype=np.uint8)
         for x in range(msg.height * msg.width * 4):
             data[x] = msg.data[x]
-        
+
         image = np.reshape(data, [msg.height, msg.width, 4])
         print(image_capturepy.showcounter)
-        if(image_capturepy.showcounter != 0):
+        if image_capturepy.showcounter != 0:
             unique_id = "{date:%Y-%m-%d_%H:%M:%S}".format(date=datetime.datetime.now())
-            print(cv2.imwrite(f'//home//john//Desktop//Rover//Images//image_{unique_id}.jpg', image))
+            print(cv2.imwrite(f"//home//john//Desktop//Rover//Images//image_{unique_id}.jpg", image))
             image_capturepy.showcounter = 0
-        
 
 
 def main():
