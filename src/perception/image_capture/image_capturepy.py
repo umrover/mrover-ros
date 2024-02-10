94d656a3aa7fb1300321e47be5d9b44d11efcc39
John <jrabraham1904@gmail.com>
2024-02-10 13:55:34 -0500

    WIP Working on directories

--- src/perception/image_capture/image_capturepy.py
+++ src/perception/image_capture/image_capturepy.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
-from pynput.keyboard import Controller, Key
+from pynput import keyboard
 
+import rospkg
+
+import os
 
 # python linear algebra library
 import numpy as np
@@ -17,35 +20,44 @@ import datetime
 # ROS message types we need to use
 from sensor_msgs.msg import Image
 
-# start the Keyboard thread
+
+def on_press(key):
+    if key == keyboard.Key.enter:
+
+        msg = rospy.wait_for_message("/camera/left/image", Image, timeout=5)
+        data = np.empty(msg.height * msg.width * 4, dtype=np.uint8)
+        for x in range(msg.height * msg.width * 4):
+            data[x] = msg.data[x]
+
+        image = np.reshape(data, [msg.height, msg.width, 4])
+        unique_id = "{date:%Y-%m-%d_%H:%M:%S}".format(date=datetime.datetime.now())
+        rospack = rospkg.RosPack()
+        pkgPath = rospack.get_path("mrover")
+        imagePath = pkgPath + f"/data/Images"
+        if not os.path.exists(imagePath):
+            os.mkdir(imagePath)
+
+        print(cv2.imwrite(imagePath + f"image_{unique_id}.jpg", image))
+
+
+def on_release(key):
+    while False:
+        continue
 
 
 class image_capturepy:
 
     def __init__(self):
-        self.keyboard = Controller()
-        while True:
-            input()
-            msg = rospy.wait_for_message("/camera/left/image", Image, timeout=5)
-            data = np.empty(msg.height * msg.width * 4, dtype=np.uint8)
-            for x in range(msg.height * msg.width * 4):
-                data[x] = msg.data[x]
-
-            image = np.reshape(data, [msg.height, msg.width, 4])
-            unique_id = "{date:%Y-%m-%d_%H:%M:%S}".format(date=datetime.datetime.now())
-            print(cv2.imwrite(f"//home//john//Desktop//Rover//Images//image_{unique_id}.jpg", image))
-
-    def __del__(self):
-        self.keyboard.press(Key.enter)
-        self.keyboard.release(Key.enter)
+        while False:
+            continue
 
 
 def main():
     # initialize the node
     rospy.init_node("image_capturepy")
 
-    # create and start our localization system
-    image_capturepy1 = image_capturepy()
+    listener = keyboard.Listener(on_press=on_press, on_release=on_release)
+    listener.start()
 
     # let the callback functions run asynchronously in the background
     rospy.spin()
