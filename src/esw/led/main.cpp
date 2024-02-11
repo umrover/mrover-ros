4a5f089355f5066e26322e11290e4e491a5fa4be
qhdwight <qhdwight@gmail.com>
2024-02-11 16:47:28 -0500

    Lint

--- src/esw/led/main.cpp
+++ src/esw/led/main.cpp
@@ -1,13 +1,9 @@
+#include <ros/ros.h>
+#include <std_srvs/SetBool.h>
+
 #include <mrover/CAN.h>
 #include <mrover/LED.h>
 #include <mrover/StateMachineStateUpdate.h>
-#include <ros/ros.h>
-#include <std_msgs/Bool.h>
-#include <std_srvs/SetBool.h>
-
-static std::string const DONE_STATE = "DoneState";
-
-ros::Publisher LEDPublisher;
 
 enum class LEDMode {
     Unknown = 0,
@@ -17,14 +13,21 @@ enum class LEDMode {
     Blue = 4
 };
 
-bool navDone = false;
-bool teleop_enabled = false;
+static constexpr std::string DONE_STATE = "DoneState";
+
+ros::Publisher led_publisher;
+
+// When navigation reaches a waypoint it will publish "DoneState" to the "nav_state" topic
+
+bool is_navigation_done = false;
+bool is_teleop_enabled = false;
 LEDMode led_mode = LEDMode::Unknown;
 
 auto update_led() -> void {
-    if (teleop_enabled) {
+    // Teleoperation takes precedence over autonomous control
+    if (is_teleop_enabled) {
         led_mode = LEDMode::Blue;
-    } else if (navDone) {
+    } else if (is_navigation_done) {
         led_mode = LEDMode::BlinkingGreen;
     } else {
         led_mode = LEDMode::Red;
@@ -34,17 +37,17 @@ auto update_led() -> void {
     led_msg.red = led_mode == LEDMode::Red;
     led_msg.green = led_mode == LEDMode::BlinkingGreen;
     led_msg.blue = led_mode == LEDMode::Blue;
-    LEDPublisher.publish(led_msg);
+    led_publisher.publish(led_msg);
 }
 
 auto state_machine_state_update(mrover::StateMachineStateUpdate::ConstPtr const& msg) -> void {
-    navDone = msg->state == DONE_STATE;
+    is_navigation_done = msg->state == DONE_STATE;
 
     update_led();
 }
 
 auto teleop_enabled_update(std_srvs::SetBoolRequest& request, std_srvs::SetBoolResponse& response) -> bool {
-    teleop_enabled = request.data;
+    is_teleop_enabled = request.data;
 
     update_led();
 
@@ -52,15 +55,13 @@ auto teleop_enabled_update(std_srvs::SetBoolRequest& request, std_srvs::SetBoolR
 }
 
 auto main(int argc, char** argv) -> int {
-    // Initialize the ROS node
     ros::init(argc, argv, "led");
     ros::NodeHandle nh;
 
-    LEDPublisher = nh.advertise<mrover::CAN>("led", 1);
-    ros::ServiceServer teleopClient = nh.advertiseService("teleop_enabled", teleop_enabled_update);
-    ros::Subscriber stateMachineStateSubscriber = nh.subscribe<mrover::StateMachineStateUpdate>("nav_state", 1, state_machine_state_update);
+    led_publisher = nh.advertise<mrover::CAN>("led", 1);
+    ros::ServiceServer teleop_enabled_client = nh.advertiseService("teleop_enabled", teleop_enabled_update);
+    ros::Subscriber state_machine_update_sub = nh.subscribe<mrover::StateMachineStateUpdate>("nav_state", 1, state_machine_state_update);
 
-    // Enter the ROS event loop
     ros::spin();
 
     return 0;
