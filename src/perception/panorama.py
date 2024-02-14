cb4d94a8265559550cf7a62a5af0c89037e45489
RyanI70I <ryankers@umich.edu>
2024-02-13 19:16:02 -0500

    Spins around camera while taking pano, need to be more sophisticated and factor in position

--- src/perception/panorama.py
+++ src/perception/panorama.py
@@ -5,9 +5,9 @@ import struct
 
 import cv2
 import numpy as np
-from scipy import stats
 import collections
 from sensor_msgs.msg import PointCloud2, PointField
+from mrover.msg import MotorsStatus, Throttle, Position
 
 import rospy
 import sys
@@ -18,13 +18,18 @@ from cv_bridge import CvBridge
 from mrover.srv import CapturePanorama, CapturePanoramaRequest, CapturePanoramaResponse
 from sensor_msgs.point_cloud2 import PointCloud2
 from sensor_msgs import point_cloud2
-type_mappings = [(PointField.INT8, np.dtype('int8')), (PointField.UINT8, np.dtype('uint8')), (PointField.INT16, np.dtype('int16')),
-                 (PointField.UINT16, np.dtype('uint16')), (PointField.INT32, np.dtype('int32')), (PointField.UINT32, np.dtype('uint32')),
+
+type_mappings = [(PointField.INT8, np.dtype('int8')), (PointField.UINT8, np.dtype('uint8')),
+                 (PointField.INT16, np.dtype('int16')),
+                 (PointField.UINT16, np.dtype('uint16')), (PointField.INT32, np.dtype('int32')),
+                 (PointField.UINT32, np.dtype('uint32')),
                  (PointField.FLOAT32, np.dtype('float32')), (PointField.FLOAT64, np.dtype('float64'))]
 pftype_to_nptype = dict(type_mappings)
 # sizes (in bytes) of PointField types
 pftype_sizes = {PointField.INT8: 1, PointField.UINT8: 1, PointField.INT16: 2, PointField.UINT16: 2,
                 PointField.INT32: 4, PointField.UINT32: 4, PointField.FLOAT32: 4, PointField.FLOAT64: 8}
+
+
 def fields_to_dtype(fields, point_step):
     '''Convert a list of PointFields to a numpy record datatype.
     '''
@@ -49,6 +54,8 @@ def fields_to_dtype(fields, point_step):
         offset += 1
 
     return np_dtype_list
+
+
 def pointcloud2_to_array(cloud_msg, squeeze=True):
     ''' Converts a rospy PointCloud2 message to a numpy recordarray
 
@@ -109,17 +116,15 @@ def split_rgb_field(cloud_arr):
     return new_cloud_arr
 
 
-
-
-
 def capture_panorama(request: CapturePanoramaRequest) -> CapturePanoramaResponse:
     image_list = []
+
     def received_point_cloud(point: PointCloud2):
         rospy.loginfo("Received point cloud data")
-        #Extract RGB field
-        pc=pointcloud2_to_array(point)
+        # Extract RGB field
+        pc = pointcloud2_to_array(point)
         pc = split_rgb_field(pc)
-        shape = pc.shape + (3, )
+        shape = pc.shape + (3,)
         rgb = np.zeros(shape)
         rgb[..., 0] = pc['r']
         rgb[..., 1] = pc['g']
@@ -128,14 +133,21 @@ def capture_panorama(request: CapturePanoramaRequest) -> CapturePanoramaResponse
         rospy.loginfo("Converted and Saved Image")
         rospy.loginfo("Total Images Saved" + str(len(image_list)))
 
-    image_subscriber = rospy.Subscriber("/camera/left/points", PointCloud2,received_point_cloud, queue_size=1)
-    #TODO, make motors spin around
-    if(len(image_list) < 2):
+    def received_motor_position(status: MotorsStatus):
+        rospy.loginfo("Received motor position")
+        mast_throttle.publish(Position(["mast_joint"], [2]))
+        rospy.loginfo(status.joint_states.position)
+
+    position_subscriber = rospy.Subscriber("/mast_status", MotorsStatus, received_motor_position, queue_size=1)
+    image_subscriber = rospy.Subscriber("/camera/left/points", PointCloud2, received_point_cloud, queue_size=1)
+    mast_throttle = rospy.Publisher("/mast_position_cmd", Position, queue_size=1)
+    # TODO, make motors spin around
+    if (len(image_list) < 2):
         rospy.sleep(1)
 
     rospy.loginfo("Creating Panorama..." + str(len(image_list)))
     stitcher = cv2.Stitcher.create()
-    (code,outPic) = stitcher.stitch(image_list)
+    (code, outPic) = stitcher.stitch(image_list)
     rospy.loginfo("Panorama Created" + str(len(image_list)))
     desktop_path = '/Users/ryankersten/Desktop/test.png'
 
@@ -156,7 +168,4 @@ def main() -> int:
 
 
 if __name__ == "__main__":
-
     sys.exit(main())
-
-
