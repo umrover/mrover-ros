3de52c4b6c6ae6a771498a3540d860e7e024148e
tabiosg <gutstabios@gmail.com>
2024-02-14 02:07:55 -0500

    Science

--- src/esw/fw/science/Core/Src/science.cpp
+++ src/esw/fw/science/Core/Src/science.cpp
@@ -10,8 +10,7 @@ extern uint8_t spectral_status_buffer[1];
 
 namespace mrover {
 
-    // NOTE: Change this for the PDLB controller
-    constexpr static std::uint8_t DEVICE_ID = 0x33;
+    constexpr static std::uint8_t DEVICE_ID = 0x51;
 
     // Usually this is the Jetson
     constexpr static std::uint8_t DESTINATION_DEVICE_ID = 0x10;
@@ -20,12 +19,6 @@ namespace mrover {
     Science science;
 
     void init() {
-        check(HAL_FDCAN_ActivateNotification(
-                      &hfdcan1,
-                      FDCAN_IT_RX_FIFO0_NEW_MESSAGE | FDCAN_IT_ERROR_PASSIVE | FDCAN_IT_ERROR_WARNING | FDCAN_IT_ARB_PROTOCOL_ERROR | FDCAN_IT_DATA_PROTOCOL_ERROR | FDCAN_IT_ERROR_LOGGING_OVERFLOW,
-                      0) == HAL_OK,
-              Error_Handler);
-
         std::shared_ptr<SMBus<uint8_t, uint16_t>> i2c_bus = std::make_shared<SMBus<uint8_t, uint16_t>>(&hi2c1);
 
         std::shared_ptr<I2CMux> i2c_mux = std::make_shared<I2CMux>(
@@ -96,11 +89,6 @@ namespace mrover {
 		science.update_and_send_heater();
 	}
 
-	// USED FOR TESTING WHILE CAN IS DOWN
-	void receive_message_debug(InBoundScienceMessage message) {
-		science.receive(message);
-	}
-
     void receive_message() {
 		if (std::optional received = fdcan_bus.receive()) {
 			auto const& [header, message] = received.value();
@@ -132,16 +120,17 @@ void update_and_send_heater() {
 	mrover::update_and_send_heater();
 }
 
-// Used for debugging while CAN isn't working
-void receive_message_debug(int device, int enable) {
-	mrover::EnableScienceDeviceCommand message;
-	message.science_device = static_cast<mrover::ScienceDevice>(device);
-	message.enable = enable;
-	mrover::receive_message_debug(message);	
-}
-
 void receive_message() {
 	mrover::receive_message();
+}
+
+void HAL_FDCAN_RxFifo0Callback(FDCAN_HandleTypeDef* hfdcan, uint32_t RxFifo0ITs) {
+    if (RxFifo0ITs & FDCAN_IT_RX_FIFO0_NEW_MESSAGE) {
+        receive_message();
+    } else {
+        // Mailbox is full OR we lost a frame
+        Error_Handler();
+    }
 }
 
 void HAL_I2C_MasterTXCpltCallback(I2C_HandleTypeDef *hi2c) {
