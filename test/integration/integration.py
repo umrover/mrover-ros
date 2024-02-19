eb97a9a2827a1fc670505d8a1efc5f7f940716b1
qhdwight <qhdwight@gmail.com>
2024-02-18 20:29:34 -0500

    Update

--- test/integration/integration.py
+++ test/integration/integration.py
@@ -47,12 +47,23 @@ class TestIntegration(unittest.TestCase):
 
         rate = rospy.Rate(20)
         for waypoint in waypoints:
+            waypoint_data, pose = waypoint
             rospy.loginfo(f"Moving to waypoint: {waypoint}")
 
             def distance_to_current_waypoint():
                 try:
                     rover_in_world = SE3.from_tf_tree(tf_buffer, parent_frame="map", child_frame="base_link")
-                    waypoint_in_world = waypoint[1]
+                    if waypoint_data.type == WaypointType(val=WaypointType.NO_SEARCH):
+                        waypoint_in_world = pose
+                    elif waypoint_data.type == WaypointType(val=WaypointType.POST):
+                        waypoint_in_world = SE3.from_tf_tree(tf_buffer, parent_frame="map",
+                                                             child_frame=f"tag_{waypoint_data.tag_id}_truth")
+                    elif waypoint_data.type == WaypointType(val=WaypointType.MALLET):
+                        waypoint_in_world = SE3.from_tf_tree(tf_buffer, parent_frame="map", child_frame="mallet_truth")
+                    elif waypoint_data.type == WaypointType(val=WaypointType.WATER_BOTTLE):
+                        waypoint_in_world = SE3.from_tf_tree(tf_buffer, parent_frame="map", child_frame="bottle_truth")
+                    else:
+                        raise ValueError(f"Unknown waypoint type: {waypoint_data.type}")
                     distance_to_target = waypoint_in_world.pos_distance_to(rover_in_world)
                     rospy.logdebug(distance_to_target)
                     return distance_to_target
