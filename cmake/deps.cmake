cbf0fe49907bba7c0c737a492ddcc5b0f321e44c
qhdwight <qhdwight@gmail.com>
2024-02-19 13:41:13 -0500

    Look for dawn first

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -1,3 +1,27 @@
+find_package(dawn QUIET)
+if (dawn_FOUND)
+    message(STATUS "Using Dawn system install")
+else ()
+    message(STATUS "Using Dawn from source")
+    add_library(webgpu SHARED IMPORTED)
+
+    set(WEBGPU_BUILD_DIR ${CMAKE_CURRENT_LIST_DIR}/../deps/dawn/out/Release)
+    if (APPLE)
+        set(WEBGPU_SHARED_LIB ${WEBGPU_BUILD_DIR}/src/dawn/native/libwebgpu_dawn.dylib)
+    else ()
+        set(WEBGPU_SHARED_LIB ${WEBGPU_BUILD_DIR}/src/dawn/native/libwebgpu_dawn.so)
+    endif ()
+    if (NOT EXISTS ${WEBGPU_SHARED_LIB})
+        message(FATAL_ERROR "Dawn not found. If on Ubuntu install with 'sudo apt install -f ./pkg/dawn.deb'. Or build from source with ./scripts/build_dawn.sh")
+    endif ()
+    target_include_directories(webgpu INTERFACE ${CMAKE_CURRENT_LIST_DIR}/../deps/dawn/include ${WEBGPU_BUILD_DIR}/gen/include)
+    set_property(TARGET webgpu PROPERTY IMPORTED_LOCATION ${WEBGPU_SHARED_LIB})
+
+    set(dawn_FOUND TRUE)
+endif ()
+
+option(MROVER_BUILD_SIM "Build the simulator" ${dawn_FOUND})
+
 if (MROVER_BUILD_SIM)
     # Apparently Assimp has different names on different systems
     # find_package is case-sensitive so try both
@@ -9,28 +33,6 @@ if (MROVER_BUILD_SIM)
 
     find_package(Bullet REQUIRED)
     find_package(glfw3 REQUIRED)
-
-    find_package(dawn QUIET)
-    if (dawn_FOUND)
-        message(STATUS "Using Dawn system install")
-    else ()
-        message(STATUS "Using Dawn from source")
-        add_library(webgpu SHARED IMPORTED)
-
-        set(WEBGPU_BUILD_DIR ${CMAKE_CURRENT_LIST_DIR}/../deps/dawn/out/Release)
-        if (APPLE)
-            set(WEBGPU_SHARED_LIB ${WEBGPU_BUILD_DIR}/src/dawn/native/libwebgpu_dawn.dylib)
-        else ()
-            set(WEBGPU_SHARED_LIB ${WEBGPU_BUILD_DIR}/src/dawn/native/libwebgpu_dawn.so)
-        endif ()
-        if (NOT EXISTS ${WEBGPU_SHARED_LIB})
-            message(FATAL_ERROR "Dawn not found. If on Ubuntu install with 'sudo apt install -f ./pkg/dawn.deb'. Or build from source with ./scripts/build_dawn.sh")
-        endif ()
-        target_include_directories(webgpu INTERFACE ${CMAKE_CURRENT_LIST_DIR}/../deps/dawn/include ${WEBGPU_BUILD_DIR}/gen/include)
-        set_property(TARGET webgpu PROPERTY IMPORTED_LOCATION ${WEBGPU_SHARED_LIB})
-
-        set(dawn_FOUND TRUE)
-    endif ()
 
     add_subdirectory(deps/glfw3webgpu SYSTEM)
 endif ()
