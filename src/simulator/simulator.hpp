42b556b472916cba0fe307d3195637d00feaddcc
qhdwight <qhdwight@gmail.com>
2024-02-20 15:56:19 -0500

    Lower noise for now

--- src/simulator/simulator.hpp
+++ src/simulator/simulator.hpp
@@ -227,13 +227,13 @@ namespace mrover {
 
         // TODO: make variances configurable
         std::default_random_engine mRNG;
-        std::normal_distribution<double> mGPSDist{0, 0.2},
-                mAccelDist{0, 0.05},
-                mGyroDist{0, 0.02},
+        std::normal_distribution<> mGPSDist{0, 0.02},
+                mAccelDist{0, 0.01},
+                mGyroDist{0, 0.01},
                 mMagDist{0, 0.1},
-                mRollDist{0, 0.05},
-                mPitchDist{0, 0.05},
-                mYawDist{0, 0.1};
+                mRollDist{0, 0.01},
+                mPitchDist{0, 0.01},
+                mYawDist{0, 0.01};
 
         PeriodicTask mGpsTask;
         PeriodicTask mImuTask;
