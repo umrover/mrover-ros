fixed offset calculation

src/localization/bag.py
@@ -91,7 +91,7 @@ with open(csv_imu_heading, "w", newline="") as csv_file:
     csv_writer = csv.writer(csv_file)
     csv_writer.writerow(["time", "imu_heading"])
 
-    for _, msg, _ in bag.read_messages("/imu/data"):
+    for _, msg, _ in bag.read_messages("/imu/drift"):
         heading = euler_from_quaternion(np.array([msg.imu.orientation.x, msg.imu.orientation.y, msg.imu.orientation.z, msg.imu.orientation.w]))[2]
         time = msg.header.stamp.secs + msg.header.stamp.nsecs * (0.000000001)
         csv_writer.writerow([time, heading])

src/localization/gps_linearization.py
@@ -140,25 +140,24 @@ class GPSLinearization:
             return
         
         imu_orientation = numpify(msg.imu.orientation)
-
-        # how can we make sure these 2 headings are comparable??
+        
+        # if calculate_offset flag is true
         if self.calculate_offset:
-            imu_heading = euler_from_quaternion(numpify(msg.imu.orientation))[2]
+            imu_heading = euler_from_quaternion(imu_orientation)[2]
             imu_heading_matrix = euler_matrix(0, 0, imu_heading, axes="sxyz")
 
             gps_heading = euler_from_quaternion(self.last_gps_pose_fixed.rotation.quaternion)[2]
             gps_heading_matrix = euler_matrix(0, 0, gps_heading, axes="sxyz")
-            self.rtk_offset = np.matmul(inverse_matrix(gps_heading_matrix), imu_heading_matrix)
+            self.rtk_offset = np.matmul(inverse_matrix(imu_heading_matrix), gps_heading_matrix)
             self.calculate_offset = False
 
-        # if rtk_offset is set, apply offset
+        # if rtk_offset is set, apply offset to imu orientation
         if self.rtk_offset is not None:
-            imu_rotation = euler_from_quaternion(numpify(msg.imu.orientation))
+            imu_rotation = euler_from_quaternion(imu_orientation)
             imu_rotation_matrix = euler_matrix(ai=imu_rotation[0], aj=imu_rotation[1], ak=imu_rotation[2], axes="sxyz")
 
             offsetted_rotation_matrix = np.matmul(imu_rotation_matrix, self.rtk_offset)
             offsetted_euler = euler_from_matrix(offsetted_rotation_matrix)
-
             imu_orientation = quaternion_from_euler(
                 offsetted_euler[0], offsetted_euler[1], offsetted_euler[2], axes="sxyz"
             )