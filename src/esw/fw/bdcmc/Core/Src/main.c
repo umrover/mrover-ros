cc7f2f54d846e52ed20d6436ce296a6ca3b64a10
owenpark <owenpark@umich.edu>
2024-03-12 20:59:46 -0400

    Fix filters in ioc

--- src/esw/fw/bdcmc/Core/Src/main.c
+++ src/esw/fw/bdcmc/Core/Src/main.c
@@ -247,7 +247,7 @@ static void MX_FDCAN1_Init(void)
   hfdcan1.Init.DataTimeSeg1 = 14;
   hfdcan1.Init.DataTimeSeg2 = 13;
   hfdcan1.Init.StdFiltersNbr = 0;
-  hfdcan1.Init.ExtFiltersNbr = 0;
+  hfdcan1.Init.ExtFiltersNbr = 2;
   hfdcan1.Init.TxFifoQueueMode = FDCAN_TX_FIFO_OPERATION;
   if (HAL_FDCAN_Init(&hfdcan1) != HAL_OK)
   {
