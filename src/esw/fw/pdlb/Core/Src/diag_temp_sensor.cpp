9d6ab08302e39f660be6bf5623c8758047ab03ca
tabiosg <gutstabios@gmail.com>
2024-03-14 10:45:40 -0400

    Make assumption that it is 47k ohm resistor

--- src/esw/fw/pdlb/Core/Src/diag_temp_sensor.cpp
+++ src/esw/fw/pdlb/Core/Src/diag_temp_sensor.cpp
@@ -11,11 +11,11 @@ namespace mrover {
 
     constexpr static float DIAG_TEMP_COEFFICIENT = 0.0064f;
     constexpr static float DIAG_TEMP_25_DEGREE_RESISTANCE = 47000.0;
-    constexpr static float THRM_A0 = -2.808429E+02;
-    constexpr static float THRM_A1 = 1.186908E-02;
-    constexpr static float THRM_A2 = -1.731036E-07;
-	constexpr static float THRM_A3 = 1.517359E-12;
-	constexpr static float THRM_A4 = -5.398440E-18;
+    constexpr static float THRM_A0 = -5.160732E+02;
+    constexpr static float THRM_A1 = 6.831122E+02;
+    constexpr static float THRM_A2 = -3.774928E+02;
+	constexpr static float THRM_A3 = 1.159826E+02;
+	constexpr static float THRM_A4 = -1.060407E+01;
 
     DiagTempSensor::DiagTempSensor(std::shared_ptr<ADCSensor> adc_sensor, uint8_t channel)
     	: m_adc_sensor(adc_sensor), m_channel(channel) {
@@ -25,10 +25,8 @@ namespace mrover {
     void DiagTempSensor::update_temp() {
     	uint16_t adc_val = m_adc_sensor->get_raw_channel_value(m_channel);
     	float measured_volts = (adc_val * 3.3f) / 4096.0f;
-    	float calculated_current = (3.3f - measured_volts) / (10000.0f);
-    	float thermistor_resistance = measured_volts / calculated_current;
 
-    	m_temp = (THRM_A4 * powf(thermistor_resistance,4)) + (THRM_A3 * powf(thermistor_resistance,3)) + (THRM_A2 * powf(thermistor_resistance,2)) + (THRM_A1 *  thermistor_resistance) + THRM_A0;
+    	m_temp = (THRM_A4 * powf(measured_volts,4)) + (THRM_A3 * powf(measured_volts,3)) + (THRM_A2 * powf(measured_volts,2)) + (THRM_A1 *  measured_volts) + THRM_A0;
     }
 
     float DiagTempSensor::get_temp() {
