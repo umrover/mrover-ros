37409fb7be095104ed4c03d5b7800a6fdb066874
Pearl Lin <pearllin@umich.edu>
2024-03-20 15:48:24 -0400

    added plotting and testing files

--- src/localization/bag.py
+++ src/localization/bag.py
@@ -7,10 +7,11 @@ bag_file_path = "../../offset_testing.bag"
 
 bag = rosbag.Bag(bag_file_path)
 
-topics = ["/linearized_pose", "/imu/data"]
+topics = ["/linearized_pose", "/imu/data", "/gps_pose"]
 
 csv_linearized_heading = "linearized_heading.csv"
 csv_imu_heading = "imu_heading.csv"
+csv_gps_heading = "gps_heading.csv"
 
 with open(csv_linearized_heading, "w", newline="") as csv_file:
     csv_writer = csv.writer(csv_file)
@@ -31,6 +32,17 @@ with open(csv_imu_heading, "w", newline="") as csv_file:
         roll = euler_from_quaternion(np.array([msg.imu.orientation.x, msg.imu.orientation.y, msg.imu.orientation.z, msg.imu.orientation.w]))[0]
         pitch = euler_from_quaternion(np.array([msg.imu.orientation.x, msg.imu.orientation.y, msg.imu.orientation.z, msg.imu.orientation.w]))[1]
         heading = euler_from_quaternion(np.array([msg.imu.orientation.x, msg.imu.orientation.y, msg.imu.orientation.z, msg.imu.orientation.w]))[2]
+        time = msg.header.stamp.secs + msg.header.stamp.nsecs * (0.000000001)
+        csv_writer.writerow([time, roll, pitch, heading])
+
+with open(csv_gps_heading, "w", newline="") as csv_file:
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(["time", "gps_roll", "gps_pitch", "gps_heading"])
+
+    for _, msg, _ in bag.read_messages(topics="/gps_pose"):
+        roll = euler_from_quaternion(np.array([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w]))[0]
+        pitch = euler_from_quaternion(np.array([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w]))[1]
+        heading = euler_from_quaternion(np.array([msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w]))[2]
         time = msg.header.stamp.secs + msg.header.stamp.nsecs * (0.000000001)
         csv_writer.writerow([time, roll, pitch, heading])
 
