1e3fb0b8fc099a87d4981373d83c66593783e7b6
John <jrabraham1904@gmail.com>
2024-03-29 09:08:47 -0400

    name update

--- deps/glfw3webgpu/cmake/Packing.cmake
+++ deps/glfw3webgpu/cmake/Packing.cmake
@@ -32,7 +32,7 @@ set(CPACK_RESOURCE_FILE_README "${CMAKE_CURRENT_SOURCE_DIR}/README.md")
 
 # package name for deb. If set, then instead of some-application-0.9.2-Linux.deb
 # you'll get some-application_0.9.2_amd64.deb (note the underscores too)
-set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT)
+set(CPACK_DEBIAN_FILE_NAME "libglfw3-web-gpu-dev.deb")
 # that is if you want every group to have its own package,
 # although the same will happen if this is not set (so it defaults to ONE_PER_GROUP)
 # and CPACK_DEB_COMPONENT_INSTALL is set to YES
