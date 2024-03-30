9a52b643ca389955da6a52f602def0a2321f0f43
qhdwight <umroverPerception@gmail.com>
2024-03-29 20:07:22 -0400

    Add latest teleop control arm changes

--- src/teleoperation/frontend/src/components/ArmControls.vue
+++ src/teleoperation/frontend/src/components/ArmControls.vue
@@ -98,7 +98,7 @@ import MotorAdjust from './MotorAdjust.vue'
 import LimitSwitch from './LimitSwitch.vue'
 
 // In seconds
-const updateRate = 0.01
+const updateRate = 0.05
 let interval: number | undefined
 
 export default defineComponent({
