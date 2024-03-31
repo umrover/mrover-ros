56cc1bca7fa7cbdb5e3a78fdf5a0870a933cc25c
John <jrabraham1904@gmail.com>
2024-03-31 11:39:14 -0400

    I think I may need to build imgui first

---
+++ deps/webgpuhpp/build/CMakeFiles/webgpu_hpp.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for webgpu_hpp.
+# This may be replaced when dependencies are built.
---
+++ deps/webgpuhpp/build/imgui/CMakeFiles/imgui.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/john/catkin_ws/src/mrover/deps/webgpuhpp/imgui/backends/imgui_impl_glfw.cpp" "imgui/CMakeFiles/imgui.dir/backends/imgui_impl_glfw.cpp.o" "gcc" "imgui/CMakeFiles/imgui.dir/backends/imgui_impl_glfw.cpp.o.d"
+  "/home/john/catkin_ws/src/mrover/deps/webgpuhpp/imgui/backends/imgui_impl_wgpu.cpp" "imgui/CMakeFiles/imgui.dir/backends/imgui_impl_wgpu.cpp.o" "gcc" "imgui/CMakeFiles/imgui.dir/backends/imgui_impl_wgpu.cpp.o.d"
+  "/home/john/catkin_ws/src/mrover/deps/webgpuhpp/imgui/imgui.cpp" "imgui/CMakeFiles/imgui.dir/imgui.cpp.o" "gcc" "imgui/CMakeFiles/imgui.dir/imgui.cpp.o.d"
+  "/home/john/catkin_ws/src/mrover/deps/webgpuhpp/imgui/imgui_draw.cpp" "imgui/CMakeFiles/imgui.dir/imgui_draw.cpp.o" "gcc" "imgui/CMakeFiles/imgui.dir/imgui_draw.cpp.o.d"
+  "/home/john/catkin_ws/src/mrover/deps/webgpuhpp/imgui/imgui_tables.cpp" "imgui/CMakeFiles/imgui.dir/imgui_tables.cpp.o" "gcc" "imgui/CMakeFiles/imgui.dir/imgui_tables.cpp.o.d"
+  "/home/john/catkin_ws/src/mrover/deps/webgpuhpp/imgui/imgui_widgets.cpp" "imgui/CMakeFiles/imgui.dir/imgui_widgets.cpp.o" "gcc" "imgui/CMakeFiles/imgui.dir/imgui_widgets.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
