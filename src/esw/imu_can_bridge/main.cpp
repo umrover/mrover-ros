4f8dd856d08c89a51dd60a30e4d71c323312a317
nbaskins <nbaskins@umich.edu>
2024-04-06 19:10:48 -0400

    started writing imu_can_bridge code, does not work yet

---
+++ src/esw/imu_can_bridge/main.cpp
@@ -0,0 +1,27 @@
+#include "messaging.hpp"
+#include <mrover/CAN.h>
+#include <ros/ros.h>
+
+void processCANData(const mrover::CAN::ConstPtr& msg);
+
+int main(int argc, char** argv) {
+    // Initialize the ROS node
+    ros::init(argc, argv, "imu_arduino_bridge");
+    ros::NodeHandle nh;
+
+    ros::Subscriber CANSubscriber = nh.subscribe<mrover::CAN>("can/imu_arduino/in", 1, processCANData);
+
+    // Enter the ROS event loop
+    ros::spin();
+
+    return 0;
+}
+
+void processCANData(const mrover::CAN::ConstPtr& msg) {
+
+    assert(msg->source == "imu_arduino");
+    assert(msg->destination == "jetson");
+
+    // msg->data.data());
+
+}
