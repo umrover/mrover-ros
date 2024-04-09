51b6ec74d0b2ce8d36c7187f8122f508cb3367d6
qhdwight <qhdwight@gmail.com>
2024-04-09 15:58:44 -0400

    Make compile on mac lol, not working tho

--- src/esw/motor_library/brushless.hpp
+++ src/esw/motor_library/brushless.hpp
@@ -5,8 +5,6 @@
 #include <optional>
 #include <unistd.h>
 
-// TODO(quintin) this is not defined in my system can header for some reason, but moteus needs it? Is this the correct value?
-#define CANFD_FDF 0x04
 #include <moteus/moteus.h>
 
 #include <can_device.hpp>
