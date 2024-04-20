189b267db7c889df242d2ed786a894b030036008
qhdwight <qhdwight@gmail.com>
2024-04-20 17:29:43 -0400

    Update

--- src/teleoperation/arm_automation/arm_automation.py
+++ src/teleoperation/arm_automation/arm_automation.py
@@ -8,6 +8,8 @@ from mrover.msg import ArmActionAction, ArmActionGoal, ArmActionResult, Position
 
 from sensor_msgs.msg import JointState
 
+import numpy as np
+
 
 def arm_automation() -> None:
     rospy.init_node("arm_automation")
@@ -21,33 +23,51 @@ def arm_automation() -> None:
         nonlocal joint_state
         joint_state = msg
 
-    rospy.Subscriber("joint_states", JointState, joint_state_callback)
+    rospy.Subscriber("arm_joint_data", JointState, joint_state_callback)
 
     def execute_callback(goal: ArmActionGoal):
-        success = True
-
+        if joint_state is None:
+            rospy.logerr("No joint state data available")
+            server.set_aborted(ArmActionResult())
+            return
+        
+        if goal.name == "de_home":
+            target_names = ["joint_de_pitch", "joint_de_roll"]
+            target_positions = [
+                joint_state.position[joint_state.name.index('joint_de_pitch')],
+                np.pi / 8
+            ]
+            rospy.loginfo(f"Moving to {target_positions} for {target_names}")
+        else:
+            rospy.logerr("Invalid goal name")
+            server.set_aborted(ArmActionResult())
+            return
+        
         rate = rospy.Rate(20)
         while not rospy.is_shutdown():
             if server.is_preempt_requested():
                 server.set_preempted()
-                success = False
+                rospy.loginfo("Preempted")
+                server.set_aborted(ArmActionResult())
+                return
+
+
+            pos_pub.publish(Position(names=target_names, positions=target_positions))
+
+            feedback = [
+                joint_state.position[joint_state.name.index('joint_de_pitch')],
+                joint_state.position[joint_state.name.index('joint_de_roll')]
+            ]
+            
+            if np.allclose(target_positions, feedback, atol=0.1):
+                rospy.loginfo("Reached target")
                 break
-
-            if goal.name == "de_home":
-                pos_pub.publish(Position(names=["joint_de_pitch", "joint_de_roll"], position=[0, 0]))
-                if joint_state is None:
-                    success = False
-                    break
-
-                if abs(joint_state.position[0]) < 0.1 and abs(joint_state.position[1]) < 0.1:
-                    break
 
             rate.sleep()
 
-        if success:
-            server.set_succeeded(ArmActionResult())
-        else:
-            server.set_aborted(ArmActionResult())
+        server.set_succeeded(ArmActionResult())
+
+    rospy.sleep(1)
 
     server = actionlib.SimpleActionServer("arm_action", ArmActionAction, execute_cb=execute_callback, auto_start=False)
     server.start()
