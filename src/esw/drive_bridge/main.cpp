45a34794e5c9013e9f02396d606d13df3342512f
tabiosg <gutstabios@gmail.com>
2024-04-28 16:44:20 -0400

    Scale outer wheel speeds

--- src/esw/drive_bridge/main.cpp
+++ src/esw/drive_bridge/main.cpp
@@ -17,6 +17,7 @@ void moveDrive(geometry_msgs::Twist::ConstPtr const& msg);
 ros::Publisher leftVelocityPub, rightVelocityPub;
 
 Meters WHEEL_DISTANCE_INNER;
+Meters WHEEL_DISTANCE_OUTER;
 compound_unit<Radians, inverse<Meters>> WHEEL_LINEAR_TO_ANGULAR;
 
 auto main(int argc, char** argv) -> int {
@@ -24,7 +25,9 @@ auto main(int argc, char** argv) -> int {
     ros::NodeHandle nh;
 
     auto roverWidth = requireParamAsUnit<Meters>(nh, "rover/width");
+    auto roverLength = requireParamAsUnit<Meters>(nh, "rover/length");
     WHEEL_DISTANCE_INNER = roverWidth / 2;
+    WHEEL_DISTANCE_OUTER = sqrt((roverWidth / 2) * (roverWidth / 2) + (roverLength / 2) * (roverLength / 2));
 
     auto ratioMotorToWheel = requireParamAsUnit<Dimensionless>(nh, "wheel/gear_ratio");
     auto wheelRadius = requireParamAsUnit<Meters>(nh, "wheel/radius");
@@ -50,26 +53,33 @@ void moveDrive(geometry_msgs::Twist::ConstPtr const& msg) {
     auto turn = RadiansPerSecond{msg->angular.z};
     // TODO(quintin)    Don't ask me to explain perfectly why we need to cancel out a meters unit in the numerator
     //                  I think it comes from the fact that there is a unit vector in the denominator of the equation
-    auto delta = turn / Radians{1} * WHEEL_DISTANCE_INNER; // should be in m/s
-    RadiansPerSecond left = (forward - delta) * WHEEL_LINEAR_TO_ANGULAR;
-    RadiansPerSecond right = (forward + delta) * WHEEL_LINEAR_TO_ANGULAR;
-    RadiansPerSecond maximal = std::max(abs(right), abs(left));
-    if (maximal > RadiansPerSecond{15 * 2 * M_PI}) {
-        Dimensionless changeRatio = RadiansPerSecond{15 * 2 * M_PI} / maximal;
-        left = left * changeRatio;
-        right = right * changeRatio;
+    auto delta_inner = turn / Radians{1} * WHEEL_DISTANCE_INNER; // should be in m/s
+    auto delta_outer = turn / Radians{1} * WHEEL_DISTANCE_OUTER;
+    RadiansPerSecond left_inner = (forward - delta_inner) * WHEEL_LINEAR_TO_ANGULAR;
+    RadiansPerSecond right_inner = (forward + delta_inner) * WHEEL_LINEAR_TO_ANGULAR;
+    RadiansPerSecond left_outer = (forward - delta_outer) * WHEEL_LINEAR_TO_ANGULAR;
+    RadiansPerSecond right_outer = (forward + delta_outer) * WHEEL_LINEAR_TO_ANGULAR;
+
+    constexpr auto MAX_SPEED = RevolutionsPerSecond{15};
+    RadiansPerSecond maximal = std::max(abs(left_outer), abs(right_outer));
+    if (maximal > MAX_SPEED) {
+        Dimensionless changeRatio = MAX_SPEED / maximal;
+        left_inner = left_inner * changeRatio;
+        right_inner = right_inner * changeRatio;
+        left_outer = left_outer * changeRatio;
+        right_outer = right_outer * changeRatio;
     }
+
     {
         Velocity leftVelocity;
         leftVelocity.names = {"front_left", "middle_left", "back_left"};
-        leftVelocity.velocities.resize(leftVelocity.names.size(), left.get());
-        leftVelocity.velocities.resize(leftVelocity.names.size(), left.get());
+        leftVelocity.velocities = {left_outer.get(), left_inner.get(), left_outer.get()};
         leftVelocityPub.publish(leftVelocity);
     }
     {
         Velocity rightVelocity;
         rightVelocity.names = {"front_right", "middle_right", "back_right"};
-        rightVelocity.velocities.resize(rightVelocity.names.size(), right.get());
+        rightVelocity.velocities = {right_outer.get(), right_inner.get(), right_outer.get()};
         rightVelocityPub.publish(rightVelocity);
     }
 }
