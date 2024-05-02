ab1d21e0ff79e05860598d2167b95d9272d79634
Alison Ryckman <alison.ryckman@gmail.com>
2024-05-02 11:45:02 -0400

    oops merged wrong

--- src/simulator/simulator.sensors.cpp
+++ src/simulator/simulator.sensors.cpp
@@ -218,60 +218,36 @@ namespace mrover {
         if (auto lookup = getUrdf("rover"); lookup) {
             URDF const& rover = *lookup;
 
-            {
-                MotorsStatus status;
-                status.joint_states.header.stamp = ros::Time::now();
-                status.joint_states.header.frame_id = "map";
-                ControllerState driveControllerState;
-                for (auto& position: {"front", "center", "back"}) {
-                    for (auto& side: {"left", "right"}) {
-                        std::string linkName = std::format("{}_{}_wheel_link", position, side);
-                        int index = rover.linkNameToMeta.at(linkName).index;
-                        double pos = rover.physics->getJointPos(index);
-                        double vel = rover.physics->getJointVel(index);
-                        double torque = rover.physics->getJointTorque(index);
-
-                        status.name.push_back(linkName);
-                        status.joint_states.name.push_back(linkName);
-                        status.joint_states.position.push_back(pos);
-                        status.joint_states.velocity.push_back(vel);
-                        status.joint_states.effort.push_back(torque);
-
-                        status.moteus_states.name.push_back(linkName);
-                        status.moteus_states.state.emplace_back("Armed");
-                        status.moteus_states.error.emplace_back("None");
-
-                        driveControllerState.name.push_back(linkName);
-                        driveControllerState.state.emplace_back("Armed");
-                        driveControllerState.error.emplace_back("None");
-                        driveControllerState.limit_hit.push_back(0b000);
-                    }
-                }
-                mMotorStatusPub.publish(status);
-                mDriveControllerStatePub.publish(driveControllerState);
-            }
-            {
-                MotorsStatus status;
-                for (auto& linkName : {"zed_mini_camera"}) {
+            MotorsStatus status;
+            status.joint_states.header.stamp = ros::Time::now();
+            status.joint_states.header.frame_id = "map";
+            ControllerState driveControllerState;
+            for (auto& position: {"front", "center", "back"}) {
+                for (auto& side: {"left", "right"}) {
+                    std::string linkName = std::format("{}_{}_wheel_link", position, side);
                     int index = rover.linkNameToMeta.at(linkName).index;
                     double pos = rover.physics->getJointPos(index);
                     double vel = rover.physics->getJointVel(index);
                     double torque = rover.physics->getJointTorque(index);
 
-                    status.name.emplace_back(linkName);
-                    status.joint_states.name.emplace_back(linkName);
+                    status.name.push_back(linkName);
+                    status.joint_states.name.push_back(linkName);
                     status.joint_states.position.push_back(pos);
                     status.joint_states.velocity.push_back(vel);
                     status.joint_states.effort.push_back(torque);
+
+                    status.moteus_states.name.push_back(linkName);
+                    status.moteus_states.state.emplace_back("Armed");
+                    status.moteus_states.error.emplace_back("None");
+
+                    driveControllerState.name.push_back(linkName);
+                    driveControllerState.state.emplace_back("Armed");
+                    driveControllerState.error.emplace_back("None");
+                    driveControllerState.limit_hit.push_back(0b000);
                 }
-                mMastStatusPub.publish(status);
             }
-            {
-                ControllerState armControllerState;
-                for (auto& linkName: {"arm_a_link", "arm_b_link", "arm_c_link", "arm_d_link", "arm_e_link"}) {
-                    armControllerState.name.emplace_back(armMsgToUrdf.backward(linkName).value());
-                    armControllerState.state.emplace_back("Armed");
-                    armControllerState.error.emplace_back("None");
+            mMotorStatusPub.publish(status);
+            mDriveControllerStatePub.publish(driveControllerState);
 
             ControllerState armControllerState;
             sensor_msgs::JointState armJointState;
@@ -294,7 +270,7 @@ namespace mrover {
                     if (joinPosition < limits->lower + OFFSET) limitSwitches |= 0b001;
                     if (joinPosition > limits->upper - OFFSET) limitSwitches |= 0b010;
                 }
-                mArmControllerStatePub.publish(armControllerState);
+                armControllerState.limit_hit.push_back(limitSwitches);
             }
             mArmControllerStatePub.publish(armControllerState);
             mArmJointStatePub.publish(armJointState);
