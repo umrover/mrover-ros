8995d0e1a50ec0a08e4308da06fa596f48c05bbf
qhdwight <qhdwight@gmail.com>
2024-05-07 20:13:55 -0400

    Correct with quaternion

--- src/esw/imu_driver/imu_driver.cpp
+++ src/esw/imu_driver/imu_driver.cpp
@@ -13,29 +13,41 @@
 
 #include <array>
 #include <cstdlib>
+#include <numbers>
 #include <numeric>
 #include <sstream>
 
 #include <mrover/CalibrationStatus.h>
 #include <mrover/ImuAndMag.h>
 
-#include <boost/circular_buffer.hpp>
+#include <Eigen/Eigenvalues>
 
 constexpr static std::size_t BUFFER_SIZE = 256;
 constexpr static std::size_t LINE_ABORT_SIZE = 4096;
 
+static ros::Duration const STEP{0.1}, WINDOW{1};
+
+constexpr static float CORRECTION_THRESHOLD = 1;
+
+static std::string const BASE_LINK_FRAME = "base_link", MAP_FRAME = "map";
+
 struct ImuData {
-    float qx, qy, qz, qw;
-    float ax, ay, az;
-    float gx, gy, gz;
-    float mx, my, mz;
-    float temperature;
-    unsigned int calibration;
+    S3 q = S3::Identity();
+    R3 a, g, m;
+    float temperature{};
+    unsigned int calibration{};
 };
 
 auto operator>>(std::istream& iss, ImuData& data) -> std::istream& {
-    return iss >> data.qx >> data.qy >> data.qz >> data.qw >> data.ax >> data.ay >> data.az >> data.gx >> data.gy >> data.gz >> data.mx >> data.my >> data.mz >> data.temperature >> data.calibration;
+    return iss >> data.q.x() >> data.q.y() >> data.q.z() >> data.q.w() >>
+           data.a.x() >> data.a.y() >> data.a.z() >>
+           data.g.x() >> data.g.y() >> data.g.z() >>
+           data.m.x() >> data.m.y() >> data.m.z() >>
+           data.temperature >>
+           data.calibration;
 }
+
+auto squared(auto const& x) { return x * x; }
 
 auto main(int argc, char** argv) -> int {
     ros::init(argc, argv, "imu_driver");
@@ -52,29 +64,38 @@ auto main(int argc, char** argv) -> int {
     auto frame = nh.param<std::string>("/imu_driver/frame_id", "imu_link");
     auto correct = nh.param<bool>("/imu_driver/correct", true);
 
-    float correctionOffset = 0;
+    S3 orientationCorrection = S3::Identity();
 
-    constexpr ros::Duration step{0.1};
-    constexpr ros::Duration window{1};
-    ros::Timer correctTimer = nh.createTimer(window, [&](ros::TimerEvent const&) {
-        std::vector<float> angles;
-        ros::Time end = ros::Time::now(), start = end - window;
-        for (ros::Time t = start; t < end; t += step) {
-            auto t1 = SE3Conversions::fromTfTree(tfBuffer, "base_link", "imu_link", t);
-            auto t2 = SE3Conversions::fromTfTree(tfBuffer, "base_link", "imu_link", t + step);
-            auto v = (t2.translation() - t1.translation()) / step.toSec();
-            angles.push_back(std::atan2(v.y(), v.x()));
+    ImuData data;
+
+    ros::Timer correctTimer;
+    if (correct) correctTimer = nh.createTimer(WINDOW, [&](ros::TimerEvent const&) {
+        Eigen::Matrix3Xd roverVelocitiesInMap; // Each column vector is a velocity
+
+        ros::Time end = ros::Time::now(), start = end - WINDOW;
+        for (ros::Time t = start; t < end; t += STEP) {
+            auto roverInMapOld = SE3Conversions::fromTfTree(tfBuffer, BASE_LINK_FRAME, MAP_FRAME, t);
+            auto roverInMapNew = SE3Conversions::fromTfTree(tfBuffer, BASE_LINK_FRAME, MAP_FRAME, t + STEP);
+            R3 roverVelocityInMap = (roverInMapNew.translation() - roverInMapOld.translation()) / STEP.toSec();
+            roverVelocitiesInMap.resize(Eigen::NoChange, roverVelocitiesInMap.cols() + 1);
+            roverVelocitiesInMap.col(roverVelocitiesInMap.cols() - 1) = roverVelocityInMap;
         }
 
-        float mean = std::accumulate(angles.begin(), angles.end(), 0.0f) / static_cast<float>(angles.size());
-        float variance = std::accumulate(angles.begin(), angles.end(), 0.0f, [mean](float acc, float angle) {
-                             return acc + (angle - mean) * (angle - mean);
-                         }) /
-                         static_cast<float>(angles.size());
-        if (float stddev = std::sqrt(variance); stddev < 1) {
-            correctionOffset = mean;
+        R3 mean = roverVelocitiesInMap.rowwise().mean();
+        R3 centered = roverVelocitiesInMap.colwise() - mean;
+        Eigen::Matrix3d covariance = centered * centered.adjoint() / static_cast<double>(roverVelocitiesInMap.cols());
+
+        // If the velocity has not changed too much in a local timeframe, assume we are driving straight
+        if (covariance.norm() < CORRECTION_THRESHOLD) {
+            R3 forward = mean.normalized();
+            R3 right = R3::UnitZ().cross(forward).normalized();
+            R3 up = forward.cross(right);
+            SO3d::Rotation orientation;
+            orientation << forward, right, up;
+            orientationCorrection = orientation * data.q.inverse();
         }
     });
+    (void) correctTimer;
 
     int fd = open(port.c_str(), O_RDWR);
     if (fd < 0) {
@@ -117,40 +138,40 @@ auto main(int argc, char** argv) -> int {
         remaining.append(buffer.data(), actually_read);
 
         while (ros::ok()) {
-            auto pos = remaining.find('\n');
-            if (pos == std::string::npos) break;
+            std::size_t newLineIndex = remaining.find('\n');
+            if (newLineIndex == std::string::npos) break;
 
-            std::string line = remaining.substr(0, pos);
-            remaining = remaining.substr(pos + 1);
+            std::string line = remaining.substr(0, newLineIndex);
+            remaining = remaining.substr(newLineIndex + 1);
 
             // Means we have not seen a new line in a while, IMU firmware may be giving wrong data
             if (remaining.size() > LINE_ABORT_SIZE) throw std::runtime_error("Too much data");
-
-            ImuData data{};
 
             if (std::istringstream iss{line}; !(iss >> data)) {
                 ROS_ERROR_STREAM("Failed to parse IMU data: " << line);
                 continue;
             }
 
+            S3 correctedOrientation = data.q * orientationCorrection;
+
             mrover::ImuAndMag msg{};
             msg.header.stamp = ros::Time::now();
             msg.header.frame_id = frame;
-            msg.imu.orientation.x = data.qx;
-            msg.imu.orientation.y = data.qy;
-            msg.imu.orientation.z = data.qz;
-            msg.imu.orientation.w = data.qw;
-            msg.imu.angular_velocity.x = data.gx;
-            msg.imu.angular_velocity.y = data.gy;
-            msg.imu.angular_velocity.z = data.gz;
-            msg.imu.linear_acceleration.x = data.ax;
-            msg.imu.linear_acceleration.y = data.ay;
-            msg.imu.linear_acceleration.z = data.az;
+            msg.imu.orientation.x = correctedOrientation.x();
+            msg.imu.orientation.y = correctedOrientation.y();
+            msg.imu.orientation.z = correctedOrientation.z();
+            msg.imu.orientation.w = correctedOrientation.w();
+            msg.imu.angular_velocity.x = data.g.x();
+            msg.imu.angular_velocity.y = data.g.y();
+            msg.imu.angular_velocity.z = data.g.z();
+            msg.imu.linear_acceleration.x = data.a.x();
+            msg.imu.linear_acceleration.y = data.a.y();
+            msg.imu.linear_acceleration.z = data.a.z();
             msg.mag.header.stamp = ros::Time::now();
             msg.mag.header.frame_id = frame;
-            msg.mag.magnetic_field.x = data.mx;
-            msg.mag.magnetic_field.y = data.my;
-            msg.mag.magnetic_field.z = data.mz;
+            msg.mag.magnetic_field.x = data.m.x();
+            msg.mag.magnetic_field.y = data.m.y();
+            msg.mag.magnetic_field.z = data.m.z();
             dataPublisher.publish(msg);
 
             mrover::CalibrationStatus calibration{};
