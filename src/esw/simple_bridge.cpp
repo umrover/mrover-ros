6d4721248dfa8f218ceebf4e0d5ca3e562bc0016
umroverPerception <umroverPerception@gmail.com>
2024-05-13 13:32:59 -0400

    Update

--- src/esw/simple_bridge.cpp
+++ src/esw/simple_bridge.cpp
@@ -8,7 +8,7 @@ auto main(int argc, char** argv) -> int {
     ros::NodeHandle nh;
     ros::NodeHandle pnh{"~"};
 
-    auto groupName = pnh.param<std::string>("~group_name", {});
+    auto groupName = pnh.param<std::string>("group_name", {});
     if (groupName.empty()) throw std::runtime_error{"Group name is reuqired for a simple bridge!"};
 
     mrover::MotorsGroup group{nh, groupName};
