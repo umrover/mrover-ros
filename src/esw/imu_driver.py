7a6f28e4eb8266c82acce871837793be5be6be5b
umroverPerception <umroverPerception@gmail.com>
2024-05-14 21:04:19 -0400

    working imu driver with adafruit

--- src/esw/imu_driver.py
+++ src/esw/imu_driver.py
@@ -1,43 +1,65 @@
-import time
+#!/usr/bin/env python3
+
 import board
 import busio
 from adafruit_bno08x import (
-    BNO_REPORT_ACCELEROMETER,
+    BNO_REPORT_LINEAR_ACCELERATION,
     BNO_REPORT_GYROSCOPE,
     BNO_REPORT_MAGNETOMETER,
     BNO_REPORT_ROTATION_VECTOR,
 )
 from adafruit_bno08x.i2c import BNO08X_I2C
 
-i2c = busio.I2C(board.SCL, board.SDA, frequency=800000)
-bno = BNO08X_I2C(i2c,address=0x4a) # BNO080 (0x4b) BNO085 (0x4a)
+import rospy
+from geometry_msgs.msg import Quaternion, Vector3
+from mrover.msg import ImuAndMag, CalibrationStatus
+from sensor_msgs.msg import Imu, MagneticField
+from std_msgs.msg import Header
 
-bno.enable_feature(BNO_REPORT_ACCELEROMETER)
-bno.enable_feature(BNO_REPORT_GYROSCOPE)
-bno.enable_feature(BNO_REPORT_MAGNETOMETER)
-bno.enable_feature(BNO_REPORT_ROTATION_VECTOR)
+BN0085_I2C_ADDRESS = 0x4A
 
-while True:
 
-    time.sleep(0.5)
-    print("Acceleration:")
-    accel_x, accel_y, accel_z = bno.acceleration  # pylint:disable=no-member
-    print("X: %0.6f  Y: %0.6f Z: %0.6f  m/s^2" % (accel_x, accel_y, accel_z))
-    print("")
+def main() -> None:
+    rospy.init_node("imu_driver")
 
-    print("Gyro:")
-    gyro_x, gyro_y, gyro_z = bno.gyro  # pylint:disable=no-member
-    print("X: %0.6f  Y: %0.6f Z: %0.6f rads/s" % (gyro_x, gyro_y, gyro_z))
-    print("")
+    imu_pub = rospy.Publisher("/imu/data", ImuAndMag, queue_size=1)
+    calib_pub = rospy.Publisher("/imu/calibration", CalibrationStatus, queue_size=1)
 
-    print("Magnetometer:")
-    mag_x, mag_y, mag_z = bno.magnetic  # pylint:disable=no-member
-    print("X: %0.6f  Y: %0.6f Z: %0.6f uT" % (mag_x, mag_y, mag_z))
-    print("")
+    i2c = busio.I2C(board.SCL, board.SDA)
+    bno = BNO08X_I2C(i2c, address=BN0085_I2C_ADDRESS)
 
-    print("Rotation Vector Quaternion:")
-    quat_i, quat_j, quat_k, quat_real = bno.quaternion  # pylint:disable=no-member
-    print(
-        "I: %0.6f  J: %0.6f K: %0.6f  Real: %0.6f" % (quat_i, quat_j, quat_k, quat_real)
-    )
-    print("")
+    all_done = False
+
+    while not all_done and not rospy.is_shutdown():
+        try:
+            bno.enable_feature(BNO_REPORT_LINEAR_ACCELERATION)
+            bno.enable_feature(BNO_REPORT_GYROSCOPE)
+            bno.enable_feature(BNO_REPORT_MAGNETOMETER)
+            bno.enable_feature(BNO_REPORT_ROTATION_VECTOR)
+            all_done = True
+        except:
+            rospy.logwarn("Failed to enable all features, retrying...")
+
+    rate = rospy.Rate(50)
+    while not rospy.is_shutdown():
+        header = Header(stamp=rospy.Time.now(), frame_id="imu_link")
+        imu_pub.publish(
+            ImuAndMag(
+                header,
+                Imu(
+                    header=header,
+                    orientation=Quaternion(*bno.quaternion),
+                    angular_velocity=Vector3(*bno.gyro),
+                    linear_acceleration=Vector3(*bno.linear_acceleration),
+                ),
+                MagneticField(header=header, magnetic_field=Vector3(*bno.magnetic)),
+            )
+        )
+
+        calib_pub.publish(CalibrationStatus(header, bno.calibration_status))
+
+        rate.sleep()
+
+
+if __name__ == "__main__":
+    main()
