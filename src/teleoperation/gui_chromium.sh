641a55db68b8e8baa6b93c5848b183a1096ef433
MRover Laptop <basestation@mrover.com>
2024-05-19 15:11:39 -0400

    Fix some things

--- src/teleoperation/gui_chromium.sh
+++ src/teleoperation/gui_chromium.sh
@@ -4,6 +4,6 @@
 # Chromium currently only supports this when using VA-API hardware acceleration
 # It uses the WebCodecs API to decode
 # You can easily test if your setup works with this URL: https://w3c.github.io/webcodecs/samples/video-decode-display/
-readonly FLAGS="--enable-features=VaapiVideoDecodeLinuxGL"
+readonly FLAGS="--silent-launch --enable-features=VaapiVideoDecodeLinuxGL"
 readonly ADDRESS="http://localhost:8080"
 chromium ${FLAGS} --app=${ADDRESS}/$1
