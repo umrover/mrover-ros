2ad0bfb5eacd03345c5f9144c3010875fd66405f
Sarah Shapin <sshapin@umich.edu>
2024-08-28 11:28:15 +0000

    bun vs. yarn on .sh

--- starter_project/teleop/gui_starter.sh
+++ starter_project/teleop/gui_starter.sh
@@ -1,32 +1,46 @@
 #!/bin/bash
 
-# Ensure proper NODE_PATH
-export NODE_PATH=/usr/lib/nodejs:/usr/share/nodejs
+bun run serve
+
+# # Ensure proper NODE_PATH
+# export NODE_PATH=/usr/lib/nodejs:/usr/share/nodejs
 
 
-node_executable=node
-node_executable_path=$(which "$node_executable")
-yarn_executable=yarn
-yarn_executable_path=$(which "$yarn_executable")
+# node_executable=node
+# node_executable_path=$(which "$node_executable")
+# yarn_executable=yarn
+# yarn_executable_path=$(which "$yarn_executable")
 
-if [ -z "$node_executable_path" ]
-then
-    echo "Node installation not found, please run src/teleop/gui/gui_install.sh"
-    exit 1
-fi
-if [ -z "$yarn_executable_path" ]
-then
-    echo "Yarn installation not found, please run src/teleop/gui/gui_install.sh"
-    exit 1
-fi
+# if [ -z "$node_executable_path" ]
+# then
+#     echo "Node installation not found, please run src/teleop/gui/gui_install.sh"
+#     exit 1
+# fi
+# if [ -z "$yarn_executable_path" ]
+# then
+#     echo "Yarn installation not found, please run src/teleop/gui/gui_install.sh"
+#     exit 1
+# fi
 
-# Check if node_modules up to date
-yarn check --verify-tree
+# # Check if node_modules up to date
+# yarn check --verify-tree
 
-# If not up to date, install
-if [ $? == 1 ]
-then
-    yarn install --check-files
-fi
+# # If not up to date, install
+# if [ $? == 1 ]
+# then
+#     yarn install --check-files
+# fi
 
-yarn run serve
+# yarn run serve # hard-coding url (yarn run serve + '[8080]')
+
+# # Check if node_modules up to date
+# yarn check --verify-tree
+
+# # If not up to date, install
+# if [ $? == 1 ]
+# then
+#     yarn install --check-files
+# fi
+
+# yarn run serve # hard-coding url (yarn run serve + '[8080]')
+# # may want to switch to bun
